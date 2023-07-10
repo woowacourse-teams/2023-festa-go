ea54d30b6a0378227630e902e286594a4eac3a04
hyunseo <ohseo8813@gmail.com>
2023-07-10 18:00:46 +0900

    feat: 개발환경 구축

---
+++ backend/src/main/java/com/festago/FestaGoApplication.java
@@ -0,0 +1,13 @@
+package com.festago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestaGoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/festago/FestaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
