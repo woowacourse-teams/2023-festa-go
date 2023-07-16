15f92e45e2d8ea8aa93eb648c0865ba0731a866e
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-17 00:28:34 +0900

    feat: 메인 페이지에서 티켓 제시 화면으로 이동할 수 있도록 기능 추가

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/MainActivity.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.festago.festago.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.festago.festago.R
+import com.festago.festago.databinding.ActivityMainBinding
+import com.festago.festago.presentation.ui.ticketentry.TicketEntryActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initBinding()
+        initView()
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initView() {
+        binding.btnEnterTicket.setOnClickListener {
+            navigateToTicketEntryActivity()
+        }
+    }
+
+    private fun navigateToTicketEntryActivity() {
+        startActivity(TicketEntryActivity.getIntent(this, 0L))
     }
 }
