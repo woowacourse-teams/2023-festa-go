ee7573dd5bf0eabbe95de0ba26dc4652a54771f8
EmilyCh0 <emily9803@ewhain.net>
2023-07-16 13:57:42 +0900

    init project

---
+++ android/festago-staff/app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    kotlin("plugin.serialization") version "1.8.22"
+}
+
+android {
+    namespace = "com.festago.festagostaff"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.festago.festagostaff"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    dataBinding {
+        enable = true
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+dependencies {
+    // android
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // kotlin-serialization
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    // junit4
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test:runner:1.5.2")
+
+    // android-test
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    // espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // mock
+    testImplementation("io.mockk:mockk-android:1.13.5")
+
+    // espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
+    // viewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+
+    // zxing
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+}
