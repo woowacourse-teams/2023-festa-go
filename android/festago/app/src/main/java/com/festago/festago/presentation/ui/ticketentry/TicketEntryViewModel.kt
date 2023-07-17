325e8288ff9f89c37a2485292391054464e89f49
vrexpert <vrexpert2302@gmail.com>
2023-07-17 23:01:35 +0900

    feat: 티켓 정보를 화면에 보여주는 기능 구현

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketentry/TicketEntryViewModel.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketentry/TicketEntryViewModel.kt
@@ -9,19 +9,26 @@ import com.festago.festago.domain.model.timer.Timer
 import com.festago.festago.domain.model.timer.TimerListener
 import com.festago.festago.domain.repository.TicketRepository
 import com.festago.festago.presentation.mapper.toPresentation
+import com.festago.festago.presentation.model.StageUiModel
 import com.festago.festago.presentation.model.TicketCodeUiModel
+import com.festago.festago.presentation.model.TicketStateUiModel
+import com.festago.festago.presentation.model.TicketUiModel
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class TicketEntryViewModel(
     private val ticketRepository: TicketRepository,
 ) : ViewModel() {
-    private val _ticketRemainTime: MutableLiveData<Int> = MutableLiveData(0)
+    private val _ticketRemainTime: MutableLiveData<Int> = MutableLiveData(DEFAULT_REMAIN_TIME)
     val ticketRemainTime: LiveData<Int> = _ticketRemainTime
 
     private val _ticketCode: MutableLiveData<TicketCodeUiModel> = MutableLiveData(
-        TicketCodeUiModel("festago.com", 0),
+        DEFAULT_TICKET_CODE,
     )
     val ticketCode: LiveData<TicketCodeUiModel> = _ticketCode
+
+    private val _ticket: MutableLiveData<TicketUiModel> = MutableLiveData(DEFAULT_TICKET)
+    val ticket: LiveData<TicketUiModel> = _ticket
 
     private val timer: Timer = Timer()
 
@@ -35,6 +42,13 @@ class TicketEntryViewModel(
                 period = ticketCode.period,
             )
             timer.start(ticketCode.period)
+        }
+    }
+
+    fun loadTicket(ticketId: Long) {
+        viewModelScope.launch {
+            val ticket = ticketRepository.loadTicket(ticketId)
+            _ticket.value = ticket.toPresentation()
         }
     }
 
@@ -63,5 +77,19 @@ class TicketEntryViewModel(
             }
             throw IllegalArgumentException("Unknown ViewModel Class")
         }
+    }
+
+    companion object {
+        private const val DEFAULT_REMAIN_TIME = 0
+
+        private val DEFAULT_TICKET_CODE = TicketCodeUiModel("festago.com", 0)
+
+        private val DEFAULT_TICKET = TicketUiModel(
+            id = 1,
+            number = 123,
+            entryTime = LocalDateTime.of(2023, 7, 17, 16, 0),
+            ticketState = TicketStateUiModel.BEFORE_ENTRY,
+            stage = StageUiModel(1, "페스타고", LocalDateTime.of(2023, 7, 17, 18, 0)),
+        )
     }
 }
