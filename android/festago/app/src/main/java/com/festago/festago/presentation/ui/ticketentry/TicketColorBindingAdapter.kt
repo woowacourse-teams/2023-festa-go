7ee3b4ecd7d7846ff8ff03d6e3886eb5908620c2
vrexpert <vrexpert2302@gmail.com>
2023-07-17 13:45:16 +0900

    feat: 티켓 제시 화면 구현

---
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketentry/TicketColorBindingAdapter.kt
@@ -0,0 +1,15 @@
+package com.festago.festago.presentation.ui.ticketentry
+
+import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import com.festago.festago.R
+import com.festago.festago.presentation.model.TicketStateUiModel
+
+@BindingAdapter("ticketBackground")
+fun setTicketBackgroundByState(viewGroup: ViewGroup, state: TicketStateUiModel) {
+    val background = when (state) {
+        TicketStateUiModel.BEFORE_ENTRY -> R.drawable.bg_ticket_gradient_primary
+        TicketStateUiModel.AFTER_ENTRY -> R.drawable.bg_ticket_gradient_secondary
+    }
+    viewGroup.setBackgroundResource(background)
+}
