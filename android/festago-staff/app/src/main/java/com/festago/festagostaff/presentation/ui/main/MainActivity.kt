c90115cf67a7bd3720fc324461fe5d6d81289c26
해시 <67777523+EmilyCh0@users.noreply.github.com>
2023-07-17 14:13:08 +0900

    [AN/STAFF] feat: 페스타고 스태프 어플 프로젝트 초기 세팅(#15) (#46)
    * init project
    
    * style: colors, dimens, themes 설정
    
    * feat: 스태프 메인 화면 레이아웃
    
    * refactor: 버튼 id 수정
    
    * refactor: 중복 라이브러리 제거

---
+++ android/festago-staff/app/src/main/java/com/festago/festagostaff/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.festago.festagostaff.presentation.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.festago.festagostaff.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
