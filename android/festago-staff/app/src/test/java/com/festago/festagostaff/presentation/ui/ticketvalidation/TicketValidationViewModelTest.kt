965cdbd68f7d5511ae4ba52a57520ed44214eaca
EmilyCh0 <emily9803@ewhain.net>
2023-07-18 16:25:19 +0900

    test: TicketValidationViewModel 티켓 검증 기능

---
+++ android/festago-staff/app/src/test/java/com/festago/festagostaff/presentation/ui/ticketvalidation/TicketValidationViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.festago.festagostaff.presentation.ui.ticketvalidation
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.festago.festagostaff.domain.model.TicketState
+import com.festago.festagostaff.domain.repository.TicketRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class TicketValidationViewModelTest {
+    private lateinit var vm: TicketValidationViewModel
+    private lateinit var ticketRepository: TicketRepository
+
+    private val fakeTicketState = TicketState.AFTER_ENTRY
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        ticketRepository = mockk()
+        vm = TicketValidationViewModel(ticketRepository)
+    }
+
+    @Test
+    fun `티켓 코드 검증을 하면 새로운 티켓 상태를 불러올 수 있다`() {
+        // given
+        coEvery { ticketRepository.validateTicket(any()) } returns fakeTicketState
+
+        // when
+        val code = "xxx"
+        vm.validateTicketCode(code)
+
+        // then
+        assertThat(vm.ticketState.value).isEqualTo(fakeTicketState.state)
+    }
+}
