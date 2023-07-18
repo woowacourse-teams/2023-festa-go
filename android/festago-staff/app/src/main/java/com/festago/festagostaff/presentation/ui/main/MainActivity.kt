4a66bf96f8464325f3c9eed12e9570ad3a80ccc5
EmilyCh0 <emily9803@ewhain.net>
2023-07-18 16:08:10 +0900

    feat: Main에서 TicketValidation으로 이동

--- android/festago-staff/app/src/main/java/com/festago/festagostaff/presentation/ui/main/MainActivity.kt
+++ android/festago-staff/app/src/main/java/com/festago/festagostaff/presentation/ui/main/MainActivity.kt
@@ -2,20 +2,37 @@ package com.festago.festagostaff.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.festago.festagostaff.databinding.ActivityMainBinding
-import com.festago.festagostaff.presentation.ui.ticketscan.TicketScanActivity
+import com.festago.festagostaff.presentation.ui.ticketvalidation.TicketValidationActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val vm: MainViewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initBinding()
+        initObserve()
+    }
+
+    private fun initBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCheckTicket.setOnClickListener {
-            startActivity(TicketScanActivity.getIntent(this))
+        binding.lifecycleOwner = this
+        binding.vm = vm
+    }
+
+    private fun initObserve() {
+        vm.event.observe(this) {
+            handleEvent(it)
         }
+    }
+
+    private fun handleEvent(event: MainEvent) = when (event) {
+        is MainEvent.OpenTicketValidation -> startActivity(TicketValidationActivity.getIntent(this))
     }
 }
