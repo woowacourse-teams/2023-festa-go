a4f14ddd37cd1bb3b1e2afb856c6b373d37d886e
vrexpert <vrexpert2302@gmail.com>
2023-07-18 21:03:24 +0900

    refactor: 바인딩 어댑터를 한 파일로 관리

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketentry/TicketEntryBindingAdapter.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketentry/TicketEntryBindingAdapter.kt
@@ -1,9 +1,24 @@
 package com.festago.festago.presentation.ui.ticketentry
 
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.festago.festago.R
 import com.festago.festago.presentation.model.TicketStateUiModel
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+@BindingAdapter("qrContent")
+fun setQrCodeImage(imageView: ImageView, content: String) {
+    val barcodeEncoder = BarcodeEncoder()
+    val bitmap = barcodeEncoder.encodeBitmap(
+        content,
+        BarcodeFormat.QR_CODE,
+        200,
+        200,
+    )
+    imageView.setImageBitmap(bitmap)
+}
 
 @BindingAdapter("ticketBackground")
 fun setTicketBackgroundByState(viewGroup: ViewGroup, state: TicketStateUiModel) {
