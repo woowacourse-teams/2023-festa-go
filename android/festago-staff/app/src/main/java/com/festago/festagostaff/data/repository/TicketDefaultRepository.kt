72fc78d3571758e35435a39f9786e9fea2d1694c
EmilyCh0 <emily9803@ewhain.net>
2023-07-19 16:36:35 +0900

    fix: 티켓 검증 요청 시 body 수정

--- android/festago-staff/app/src/main/java/com/festago/festagostaff/data/repository/TicketDefaultRepository.kt
+++ android/festago-staff/app/src/main/java/com/festago/festagostaff/data/repository/TicketDefaultRepository.kt
@@ -1,5 +1,6 @@
 package com.festago.festagostaff.data.repository
 
+import com.festago.festagostaff.data.dto.TicketValidationRequestDto
 import com.festago.festagostaff.data.service.TicketRetrofitService
 import com.festago.festagostaff.domain.model.TicketState
 import com.festago.festagostaff.domain.repository.TicketRepository
@@ -8,6 +9,9 @@ class TicketDefaultRepository(
     private val ticketRetrofitService: TicketRetrofitService,
 ) : TicketRepository {
     override suspend fun validateTicket(code: String): TicketState {
-        return ticketRetrofitService.validateTicket(code).body()!!.toTicketState()
+        return ticketRetrofitService
+            .validateTicket(TicketValidationRequestDto(code))
+            .body()!!
+            .toTicketState()
     }
 }
--- android/festago-staff/app/src/main/java/com/festago/festagostaff/data/service/TicketRetrofitService.kt
+++ android/festago-staff/app/src/main/java/com/festago/festagostaff/data/service/TicketRetrofitService.kt
@@ -1,11 +1,12 @@
 package com.festago.festagostaff.data.service
 
 import com.festago.festagostaff.data.dto.TicketValidationDto
+import com.festago.festagostaff.data.dto.TicketValidationRequestDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TicketRetrofitService {
     @POST("/staff/tickets/validation")
-    suspend fun validateTicket(@Body code: String): Response<TicketValidationDto>
+    suspend fun validateTicket(@Body code: TicketValidationRequestDto): Response<TicketValidationDto>
 }
