16a9b99fe1b4191690bb57934e36ac474313b6a3
해시 <67777523+EmilyCh0@users.noreply.github.com>
2023-07-19 17:46:42 +0900

    fix: 문자열 id 변경

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/MainBinding.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/MainBinding.kt
@@ -13,13 +13,13 @@ object MainBinding {
         when (LocalDateTime.now().isAfter(ticket.entryTime)) {
             false -> {
                 binding.btnEnterTicket.text =
-                    DateTimeFormatter.ofPattern(context.getString(R.string.main_ticket_disable_time))
+                    DateTimeFormatter.ofPattern(context.getString(R.string.main_btn_ticket_disable_time))
                         .format(ticket.entryTime)
                 binding.btnEnterTicket.isEnabled = false
             }
 
             else -> {
-                binding.btnEnterTicket.text = context.getString(R.string.main_ticket_enable_time)
+                binding.btnEnterTicket.text = context.getString(R.string.main_btn_ticket_enable_time)
                 binding.btnEnterTicket.isEnabled = true
             }
         }
@@ -28,9 +28,9 @@ object MainBinding {
     fun setTvTicketState(binding: ActivityMainBinding, ticket: TicketUiModel) {
         val context = binding.root.context
         binding.tvTicketState.text = when (ticket.ticketState) {
-            TicketStateUiModel.BEFORE_ENTRY -> context.getString(R.string.main_ticket_before_entry)
-            TicketStateUiModel.AFTER_ENTRY -> context.getString(R.string.main_ticket_after_entry)
-            TicketStateUiModel.AWAY -> context.getString(R.string.main_ticket_away)
+            TicketStateUiModel.BEFORE_ENTRY -> context.getString(R.string.main_tv_ticket_before_entry)
+            TicketStateUiModel.AFTER_ENTRY -> context.getString(R.string.main_tv_ticket_after_entry)
+            TicketStateUiModel.AWAY -> context.getString(R.string.main_tv_ticket_away)
             else -> ""
         }
     }
