4af9ec100f4c264a089907e5444d6f49a1ce0d62
EmilyCh0 <emily9803@ewhain.net>
2023-07-19 14:39:50 +0900

    feat: 연속 스캔 및 화면 커스텀

--- android/festago-staff/app/src/main/java/com/festago/festagostaff/presentation/ui/ticketvalidation/TicketValidationActivity.kt
+++ android/festago-staff/app/src/main/java/com/festago/festagostaff/presentation/ui/ticketvalidation/TicketValidationActivity.kt
@@ -3,14 +3,16 @@ package com.festago.festagostaff.presentation.ui.ticketvalidation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.festago.festagostaff.data.RetrofitClient
 import com.festago.festagostaff.data.repository.TicketDefaultRepository
 import com.festago.festagostaff.databinding.ActivityTicketValidationBinding
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanOptions
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
 
 class TicketValidationActivity : AppCompatActivity() {
 
@@ -24,19 +26,20 @@ class TicketValidationActivity : AppCompatActivity() {
         )
     }
 
-    private val barcodeLauncher = registerForActivityResult(ScanContract()) { result ->
-        if (result.contents == null) {
+    private val barcodeCallback = BarcodeCallback { result ->
+        if (result.text == null) {
             Toast.makeText(this, "No content", Toast.LENGTH_SHORT).show()
-            return@registerForActivityResult
+            return@BarcodeCallback
         }
-        vm.validateTicketCode(result.contents.toString())
+        if (!vm.isLatestCode(result.text)) {
+            vm.validateTicketCode(result.text)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initBinding()
-        initBarcodeLauncher()
     }
 
     private fun initBinding() {
@@ -45,13 +48,25 @@ class TicketValidationActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         binding.vm = vm
+        binding.dbvScanner.apply {
+            decoderFactory = DefaultDecoderFactory(listOf(BarcodeFormat.QR_CODE))
+            initializeFromIntent(intent)
+            decodeContinuous(barcodeCallback)
+        }
     }
 
-    private fun initBarcodeLauncher() {
-        val scanOptions = ScanOptions().apply {
-            setDesiredBarcodeFormats(ScanOptions.QR_CODE)
-        }
-        barcodeLauncher.launch(scanOptions)
+    override fun onResume() {
+        super.onResume()
+        binding.dbvScanner.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.dbvScanner.pause()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return binding.dbvScanner.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event)
     }
 
     companion object {
