7b44d9215c5e41b5b13dac38d30766154f65264d
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-23 14:06:42 +0900

    feat: 티켓 예매 화면 추가

---
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketreservation/TicketReservationActivity.kt
@@ -0,0 +1,12 @@
+package com.festago.festago.presentation.ui.ticketreservation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.festago.festago.R
+
+class TicketReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_reservation)
+    }
+}
