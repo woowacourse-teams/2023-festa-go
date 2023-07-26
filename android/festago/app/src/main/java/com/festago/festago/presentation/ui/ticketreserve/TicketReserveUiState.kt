671e2f942309540598b868878f699e696f0f6526
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-26 15:15:14 +0900

    refactor: 티켓예매 uiState를 인터페이스로 변경

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketreserve/TicketReserveUiState.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketreserve/TicketReserveUiState.kt
@@ -2,8 +2,8 @@ package com.festago.festago.presentation.ui.ticketreserve
 
 import com.festago.festago.presentation.model.ReservationUiModel
 
-sealed class TicketReserveUiState {
-    object Loading : TicketReserveUiState()
-    class Success(val reservation: ReservationUiModel) : TicketReserveUiState()
-    object Error : TicketReserveUiState()
+sealed interface TicketReserveUiState {
+    object Loading : TicketReserveUiState
+    class Success(val reservation: ReservationUiModel) : TicketReserveUiState
+    object Error : TicketReserveUiState
 }
