59afc9eb2bf0f5005ed392d89565c1b1594e6658
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-27 03:16:50 +0900

    feat: 버전에 호환되는 ParcelableCompat 추가

--- android/festago/app/src/main/java/com/festago/festago/presentation/model/ReservationTicketUiModel.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/model/ReservationTicketUiModel.kt
@@ -1,7 +1,11 @@
 package com.festago.festago.presentation.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ReservationTicketUiModel(
     val remainAmount: Int,
     val ticketType: String,
     val totalAmount: Int,
-)
+) : Parcelable
