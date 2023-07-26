d19fa172bb75fa215795eb3d2cfb132a77c7389d
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-26 16:12:24 +0900

    refactor: 바인딩 어댑터 확장함수로 변경

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/BindingAdapter.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/BindingAdapter.kt
@@ -4,6 +4,6 @@ import android.view.View
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("visibility")
-fun setVisibility(view: View, isVisible: Boolean) {
-    view.visibility = if (isVisible) View.VISIBLE else View.GONE
+fun View.setVisibility(isVisible: Boolean) {
+    this.visibility = if (isVisible) View.VISIBLE else View.GONE
 }
