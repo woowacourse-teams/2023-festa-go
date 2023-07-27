e15d28366e1ce409e4a032d66516e20cf39a6d45
xxeol2 <jhj01177@naver.com>
2023-07-27 22:06:41 +0900

    refactor: StaffTicketControllerTest 클래스 네이밍 수정

--- backend/src/test/java/com/festago/presentation/StaffTicketControllerTest.java
+++ backend/src/test/java/com/festago/presentation/StaffTicketControllerTest.java
@@ -26,7 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(StaffTicketController.class)
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class StaffMemberTicketControllerTest {
+class StaffTicketControllerTest {
 
     @Autowired
     MockMvc mockMvc;
