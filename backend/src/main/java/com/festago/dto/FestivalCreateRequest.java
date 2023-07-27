db6b2e35c5152bb78c65351c4c1baba5f5cfbfa6
BGuga <tmdwns9913@gmail.com>
2023-07-27 14:06:05 +0900

    feat: 축제 요청에 섬네일 요청이 비어있을 경우 디폴트 이미지 저장

--- backend/src/main/java/com/festago/dto/FestivalCreateRequest.java
+++ backend/src/main/java/com/festago/dto/FestivalCreateRequest.java
@@ -2,12 +2,12 @@ package com.festago.dto;
 
 import com.festago.domain.Festival;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record FestivalCreateRequest(String name, LocalDate startDate, LocalDate endDate, String thumbnail) {
 
-
     public Festival toEntity() {
-        if (thumbnail == null) {
+        if (Objects.isNull(thumbnail) || thumbnail.isBlank()) {
             return new Festival(name, startDate, endDate);
         }
         return new Festival(name, startDate, endDate, thumbnail);
