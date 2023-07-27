7f3990ec439b296d4e1415301ca2e72c7d7cc041
hyunseo <ohseo8813@gmail.com>
2023-07-27 14:21:25 +0900

    feat: 공연 티켓 조회 기능 추가

--- backend/src/main/java/com/festago/application/TicketService.java
+++ backend/src/main/java/com/festago/application/TicketService.java
@@ -1,7 +1,18 @@
 package com.festago.application;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+
+import com.festago.domain.MemberTicketRepository;
+import com.festago.domain.Ticket;
 import com.festago.domain.TicketRepository;
+import com.festago.domain.TicketType;
+import com.festago.dto.StageTicketResponse;
 import com.festago.dto.StageTicketsResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +21,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class TicketService {
 
     private final TicketRepository ticketRepository;
+    private final MemberTicketRepository memberTicketRepository;
 
-    public TicketService(TicketRepository ticketRepository) {
+    public TicketService(TicketRepository ticketRepository, MemberTicketRepository memberTicketRepository) {
         this.ticketRepository = ticketRepository;
+        this.memberTicketRepository = memberTicketRepository;
     }
 
     public StageTicketsResponse findStageTickets(Long stageId) {
-        return null;
+        List<Ticket> tickets = ticketRepository.findAllByStageId(stageId);
+        Map<TicketType, Integer> collect = tickets.stream()
+            .collect(groupingBy(Ticket::getTicketType, summingInt(Ticket::getTotalAmount)));
+
+        List<StageTicketResponse> stageTicketResponses = new ArrayList<>();
+        for (Entry<TicketType, Integer> entry : collect.entrySet()) {
+            Integer reserveCount = memberTicketRepository.countByTicketTypeAndStageId(entry.getKey(), stageId);
+            stageTicketResponses.add(
+                new StageTicketResponse(entry.getKey(), entry.getValue(), entry.getValue() - reserveCount));
+        }
+
+        return new StageTicketsResponse(stageTicketResponses);
     }
 }
