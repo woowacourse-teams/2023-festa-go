05e19c35d5bff952fd87ec2786a664bab1603808
xxeol2 <jhj01177@naver.com>
2023-07-28 16:27:06 +0900

    [BE] feat: 축제 전체 조회 API 구현 (#123) (#140)
    * feat: 축제 전체 조회 API 구현
    
    * test: 축제 전체 조회 API 컨트롤러 테스트
    
    * test: FestivalService 단위 테스트 추가
    
    ---------
    
    Co-authored-by: seokjin8678 <seokjin8678@gmail.com>

--- backend/src/main/java/com/festago/application/FestivalService.java
+++ backend/src/main/java/com/festago/application/FestivalService.java
@@ -4,6 +4,8 @@ import com.festago.domain.Festival;
 import com.festago.domain.FestivalRepository;
 import com.festago.dto.FestivalCreateRequest;
 import com.festago.dto.FestivalResponse;
+import com.festago.dto.FestivalsResponse;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +22,11 @@ public class FestivalService {
     public FestivalResponse create(FestivalCreateRequest request) {
         Festival newFestival = festivalRepository.save(request.toEntity());
         return FestivalResponse.from(newFestival);
+    }
+
+    @Transactional(readOnly = true)
+    public FestivalsResponse findAll() {
+        List<Festival> festivals = festivalRepository.findAll();
+        return FestivalsResponse.from(festivals);
     }
 }
