fb02c62da47bcbaa9a4a80abc021425d3c77dde2
BGuga <tmdwns9913@gmail.com>
2023-07-28 11:39:29 +0900

    feat: Ticket 검증 로직 추가

--- backend/src/main/java/com/festago/domain/Ticket.java
+++ backend/src/main/java/com/festago/domain/Ticket.java
@@ -1,5 +1,7 @@
 package com.festago.domain;
 
+import com.festago.exception.BadRequestException;
+import com.festago.exception.ErrorCode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
 public class Ticket {
 
     private static final int ENTRY_LIMIT_DAY = 1;
+    private static final int MIN_TOTAL_AMOUNT = 1;
+    private static final long EARLY_ENTRY_LIMIT = 12;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,11 +41,27 @@ public class Ticket {
     }
 
     public Ticket(Long id, Stage stage, TicketType ticketType, Integer totalAmount, LocalDateTime entryTime) {
+        validate(totalAmount, stage, entryTime);
         this.id = id;
         this.stage = stage;
         this.ticketType = ticketType;
         this.totalAmount = totalAmount;
         this.entryTime = entryTime;
+    }
+
+    private void validate(Integer totalAmount, Stage stage, LocalDateTime entryTime) {
+        if (totalAmount < MIN_TOTAL_AMOUNT) {
+            throw new BadRequestException(ErrorCode.INVALID_MIN_TICKET_AMOUNT);
+        }
+        LocalDateTime stageStartTime = stage.getStartTime();
+        if (validateEntryTime(entryTime, stageStartTime)) {
+            throw new BadRequestException(ErrorCode.INVALID_TICKET_ENTRY_TIME);
+        }
+    }
+
+    private boolean validateEntryTime(LocalDateTime entryTime, LocalDateTime stageStartTime) {
+        return (entryTime.isAfter(stageStartTime) || entryTime.isEqual(stageStartTime))
+            || entryTime.isBefore(stageStartTime.minusHours(EARLY_ENTRY_LIMIT));
     }
 
     public boolean canEntry(LocalDateTime time) {
