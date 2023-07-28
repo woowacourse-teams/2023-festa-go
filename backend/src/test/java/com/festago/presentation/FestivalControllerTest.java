0bfabbad7b469f181a8503f822bc9f4c44a77c08
xxeol2 <jhj01177@naver.com>
2023-07-28 11:28:28 +0900

    test: 축제 전체 조회 API 컨트롤러 테스트

---
+++ backend/src/test/java/com/festago/presentation/FestivalControllerTest.java
@@ -0,0 +1,61 @@
+package com.festago.presentation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.festago.application.FestivalService;
+import com.festago.dto.FestivalResponse;
+import com.festago.dto.FestivalsResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(FestivalController.class)
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class FestivalControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    FestivalService festivalService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void 모든_축제를_조회한다() throws Exception {
+        // given
+        FestivalResponse festivalResponse1 = new FestivalResponse(1L, "테코대학교", LocalDate.now(),
+            LocalDate.now().plusDays(3), "https://image1.png");
+        FestivalResponse festivalResponse2 = new FestivalResponse(2L, "우테대학교", LocalDate.now().minusDays(3),
+            LocalDate.now(), "https://image2.png");
+        FestivalsResponse expected = new FestivalsResponse(List.of(festivalResponse1, festivalResponse2));
+        given(festivalService.findAll())
+            .willReturn(expected);
+
+        // when & then
+        String content = mockMvc.perform(get("/festivals")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn()
+            .getResponse()
+            .getContentAsString(StandardCharsets.UTF_8);
+        FestivalsResponse actual = objectMapper.readValue(content, FestivalsResponse.class);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
