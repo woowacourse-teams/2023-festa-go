8ed0271cdbcf52713137348a3ee52c67350d47ef
seokjin8678 <seokjin8678@gmail.com>
2023-07-28 16:03:13 +0900

    style: 서식 수정

--- backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
+++ backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class TicketServiceIntegrationTest extends ApplicationIntegrationTest {
