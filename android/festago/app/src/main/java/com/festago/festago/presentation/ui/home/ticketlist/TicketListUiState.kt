f0d7576afc764ccd036bada82e075cb2fb7b21b7
vrexpert <vrexpert2302@gmail.com>
2023-07-29 17:37:33 +0900

    feat: state 에 티켓 개수 확인 추가

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/ticketlist/TicketListUiState.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/ticketlist/TicketListUiState.kt
@@ -9,7 +9,8 @@ sealed interface TicketListUiState {
 
     object Error : TicketListUiState
 
-    val shouldShowSuccess get() = this is Success
+    val shouldShowSuccessWithTickets get() = this is Success && tickets.isNotEmpty()
+    val shouldShowSuccessAndEmpty get() = this is Success && tickets.isEmpty()
     val shouldShowLoading get() = this is Loading
     val shouldShowError get() = this is Error
 }
