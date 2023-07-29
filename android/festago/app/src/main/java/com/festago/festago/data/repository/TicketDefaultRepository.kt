7c497b0fe628f4a4b212aaad9e2d9d9f7638c57f
vrexpert <vrexpert2302@gmail.com>
2023-07-29 10:17:03 +0900

    feat: 티켓들 불러오기 기능 임시 구현

--- android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
@@ -1,15 +1,34 @@
 package com.festago.festago.data.repository
 
 import com.festago.festago.data.service.TicketRetrofitService
+import com.festago.festago.domain.model.Stage
 import com.festago.festago.domain.model.Ticket
 import com.festago.festago.domain.model.TicketCode
+import com.festago.festago.domain.model.TicketState
 import com.festago.festago.domain.repository.TicketRepository
+import java.time.LocalDateTime
 
 class TicketDefaultRepository(
     private val ticketRetrofitService: TicketRetrofitService,
 ) : TicketRepository {
+
+    private val fakeTicket = List(10) {
+        Ticket(
+            id = it + 1L,
+            number = 100,
+            LocalDateTime.MAX,
+            TicketState.BEFORE_ENTRY,
+            Stage(1, "테코대학교 축제 DAY1", LocalDateTime.MAX),
+            "",
+        )
+    }
+
     override suspend fun loadTicket(ticketId: Long): Ticket {
         return ticketRetrofitService.getTicket(ticketId).body()!!.toDomain()
+    }
+
+    override suspend fun loadTickets(): Result<List<Ticket>> {
+        return Result.success(fakeTicket)
     }
 
     override suspend fun loadTicketCode(ticketId: Long): TicketCode {
