07b7ca9384730436ca63cbdd04642b9c5c74653a
vrexpert <vrexpert2302@gmail.com>
2023-07-30 18:54:21 +0900

    refactor: bindingAdapter 로직을 ViewHolder 로 이동

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/ticketlist/TicketItemViewHolder.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/ticketlist/TicketItemViewHolder.kt
@@ -2,9 +2,13 @@ package com.festago.festago.presentation.ui.home.ticketlist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
+import com.festago.festago.R
 import com.festago.festago.databinding.ItemTicketBinding
 import com.festago.festago.presentation.model.TicketUiModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class TicketItemViewHolder(
     val binding: ItemTicketBinding,
@@ -17,6 +21,31 @@ class TicketItemViewHolder(
 
     fun bind(item: TicketUiModel) {
         binding.ticket = item
+        setTicketEntryBtn(item)
+    }
+
+    private fun setTicketEntryBtn(item: TicketUiModel) {
+        val btn = binding.btnTicketEntry
+        val isAfterEntryTime = LocalDateTime.now().isAfter(item.entryTime)
+        btn.isEnabled = isAfterEntryTime
+
+        setTicketEntryBtnText(isAfterEntryTime = isAfterEntryTime, btn = btn, ticket = item)
+    }
+
+    private fun setTicketEntryBtnText(
+        isAfterEntryTime: Boolean,
+        btn: Button,
+        ticket: TicketUiModel,
+    ) {
+        if (isAfterEntryTime) {
+            btn.text = btn.context.getString(R.string.ticket_list_btn_ticket_entry)
+            return
+        }
+        btn.text = DateTimeFormatter.ofPattern(
+            btn.context.getString(
+                R.string.ticket_list_btn_ticket_disable_time,
+            ),
+        ).format(ticket.entryTime)
     }
 
     companion object {
