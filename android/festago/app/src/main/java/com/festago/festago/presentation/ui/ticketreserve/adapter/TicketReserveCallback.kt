0665d81383e731aa3425fda7493123e7051ac4e0
re4rk <37167652+re4rk@users.noreply.github.com>
2023-07-30 19:26:25 +0900

    refactor: vm 함수 출력하도록 변경

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/ticketreserve/adapter/TicketReserveCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.festago.festago.presentation.ui.ticketreserve.adapter
-
-import com.festago.festago.presentation.model.ReservationStageUiModel
-
-fun interface TicketReserveCallback {
-    fun onTicketReserveClick(reservationStage: ReservationStageUiModel)
-}
