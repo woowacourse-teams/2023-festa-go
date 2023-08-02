3ed02ca46f1ec999a791e7b2da63d4c6b369e831
xxeol2 <jhj01177@naver.com>
2023-08-02 10:35:10 +0900

    refactor: ticketAmount의 calculateRemainAmount 사용하도록 리팩토링

--- backend/src/main/java/com/festago/dto/StageTicketResponse.java
+++ backend/src/main/java/com/festago/dto/StageTicketResponse.java
@@ -11,12 +11,11 @@ public record StageTicketResponse(Long id,
 
     public static StageTicketResponse from(Ticket ticket) {
         TicketAmount ticketAmount = ticket.getTicketAmount();
-        int remainAmount = ticketAmount.getTotalAmount() - ticketAmount.getReservedAmount();
         return new StageTicketResponse(
             ticket.getId(),
             ticket.getTicketType(),
             ticketAmount.getTotalAmount(),
-            remainAmount // TODO : 169 pr 머지 후 ticketAmount.calculateRemainAmount 사용
+            ticketAmount.calculateRemainAmount()
         );
     }
 }
