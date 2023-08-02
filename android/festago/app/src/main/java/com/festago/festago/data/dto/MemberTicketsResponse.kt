882bb2846051dc9c3fe61977943ab450937d34ce
vrexpert <vrexpert2302@gmail.com>
2023-08-02 14:37:03 +0900

    refactor: tickets API 명세 변경

--- android/festago/app/src/main/java/com/festago/festago/data/dto/MemberTicketsResponse.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/dto/MemberTicketsResponse.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MemberTicketsResponse(
-    @SerialName("tickets") val memberTickets: List<MemberTicketResponse>,
+    @SerialName("memberTickets") val memberTickets: List<MemberTicketResponse>,
 ) {
     fun toDomain(): List<Ticket> = memberTickets.map { it.toDomain() }
 }
