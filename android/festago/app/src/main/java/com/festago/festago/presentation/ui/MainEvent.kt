7e73aafa89c86299b1a1011eb681c39f683d40bf
vrexpert <vrexpert2302@gmail.com>
2023-08-03 10:59:39 +0900

    delete: 사용하지 않는 main 화면 삭제

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/MainEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.festago.festago.presentation.ui
-
-import com.festago.festago.presentation.model.TicketUiModel
-
-sealed class MainEvent {
-    class OpenTicketEntry(val ticketUiModel: TicketUiModel) : MainEvent()
-    object FailToOpenTicketEntry : MainEvent()
-    object FailToLoadTicket : MainEvent()
-}
