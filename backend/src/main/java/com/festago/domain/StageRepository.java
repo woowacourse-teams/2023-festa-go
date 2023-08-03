822e210a50dcaedb16756e10961f41fb4e6157de
xxeol2 <jhj01177@naver.com>
2023-08-03 15:39:49 +0900

    refactor: JOIN 방식 LEFT JOIN으로 수정

--- backend/src/main/java/com/festago/domain/StageRepository.java
+++ backend/src/main/java/com/festago/domain/StageRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface StageRepository extends JpaRepository<Stage, Long> {
-    
+
     @Query("""
         SELECT s FROM Stage s
-        JOIN FETCH s.tickets t
-        JOIN FETCH t.ticketAmount
+        LEFT JOIN FETCH s.tickets t
+        LEFT JOIN FETCH t.ticketAmount
         WHERE s.festival.id = :festivalId
         """)
     List<Stage> findAllDetailByFestivalId(@Param("festivalId") Long festivalId);
