36b70ce85d1b6ac3bd8566a693704ac49ccc57e6
seokjin8678 <seokjin8678@gmail.com>
2023-08-07 23:21:17 +0900

    chore: 오타 수정

--- backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
+++ backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
@@ -3,6 +3,8 @@ package com.festago.application.integration;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.festago.application.FestivalService;
+import com.festago.application.StageService;
 import com.festago.application.TicketService;
 import com.festago.domain.Member;
 import com.festago.domain.MemberRepository;
@@ -37,16 +39,22 @@ class TicketServiceIntegrationTest {
     MemberRepository memberRepository;
 
     @Autowired
+    FestivalService festivalService;
+
+    @Autowired
+    StageService stageService;
+
+    @Autowired
     MemberTicketRepository memberTicketRepository;
 
     @Test
     void 공연이_없으면_예외() {
         // given
         String entryTime = "2023-07-26T18:00:00";
-        long invliadStageId = 0L;
+        long invalidStageId = 0L;
         int totalAmount = 100;
 
-        TicketCreateRequest request = new TicketCreateRequest(invliadStageId, TicketType.VISITOR,
+        TicketCreateRequest request = new TicketCreateRequest(invalidStageId, TicketType.VISITOR,
             totalAmount, LocalDateTime.parse(entryTime));
 
         // when && then
