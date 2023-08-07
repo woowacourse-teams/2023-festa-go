02163ac51f90cf79914f530c02ff08a869d62702
xxeol2 <jhj01177@naver.com>
2023-08-07 12:14:14 +0900

    test: 티켓 최초 생성시 입장시간이 유효하지 않으면 티켓도 생성되지 않는다

--- backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
+++ backend/src/test/java/com/festago/application/integration/TicketServiceIntegrationTest.java
@@ -2,15 +2,24 @@ package com.festago.application.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.festago.application.TicketService;
+import com.festago.domain.Festival;
+import com.festago.domain.FestivalRepository;
 import com.festago.domain.Member;
 import com.festago.domain.MemberRepository;
 import com.festago.domain.MemberTicketRepository;
+import com.festago.domain.Stage;
+import com.festago.domain.StageRepository;
+import com.festago.domain.TicketRepository;
 import com.festago.domain.TicketType;
 import com.festago.dto.TicketCreateRequest;
 import com.festago.dto.TicketingRequest;
+import com.festago.exception.BadRequestException;
 import com.festago.exception.NotFoundException;
+import com.festago.support.FestivalFixture;
+import com.festago.support.StageFixture;
 import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -35,6 +44,15 @@ class TicketServiceIntegrationTest {
 
     @Autowired
     MemberTicketRepository memberTicketRepository;
+
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @Autowired
+    StageRepository stageRepository;
+
+    @Autowired
+    FestivalRepository festivalRepository;
 
     @Test
     void 공연이_없으면_예외() {
@@ -76,4 +94,22 @@ class TicketServiceIntegrationTest {
         assertThat(memberTicketRepository.count()).isEqualTo(100);
     }
 
+    @Test
+    void 티켓_최초_생성시_입장시간이_유효하지_않으면_티켓도_생성되지않는다() {
+        // given
+        Festival festival = festivalRepository.save(FestivalFixture.festival().build());
+        Stage stage = stageRepository.save(StageFixture.stage().festival(festival).build());
+        LocalDateTime entryTime = stage.getStartTime().plusHours(1);
+        Long stageId = stage.getId();
+        TicketCreateRequest request = new TicketCreateRequest(stageId, TicketType.STUDENT, 100,
+            entryTime);
+
+        // when & then
+        assertSoftly(softly -> {
+            softly.assertThatThrownBy(() -> ticketService.create(request))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage("입장 시간은 공연 시간보다 빨라야합니다.");
+            softly.assertThat(ticketRepository.findAllByStageId(stageId).size()).isEqualTo(0);
+        });
+    }
 }
