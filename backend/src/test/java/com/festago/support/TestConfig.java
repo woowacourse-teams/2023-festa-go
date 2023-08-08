52230b6c1c106132658261dc417006c08253ee29
xxeol2 <jhj01177@naver.com>
2023-08-08 16:05:37 +0900

    refactor: test용 Configuration파일에 @TestConfiguration 어노테이션 적용
    - 같은 타입의 빈이 존재할시 TestConfiguration의 빈을 사용한다

--- backend/src/test/java/com/festago/support/TestConfig.java
+++ backend/src/test/java/com/festago/support/TestConfig.java
@@ -2,10 +2,10 @@ package com.festago.support;
 
 import com.festago.auth.domain.AuthExtractor;
 import com.festago.auth.infrastructure.JwtAuthExtractor;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@TestConfiguration
 public class TestConfig {
 
     private static final String SECRET_KEY = "1231231231231231223131231231231231231212312312";
