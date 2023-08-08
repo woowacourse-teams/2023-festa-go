7a01230954d852d7272688ad7e92eaf94630df9c
seokjin8678 <seokjin8678@gmail.com>
2023-08-09 01:37:34 +0900

    refactor: JdbcTemplate를 사용하는 코드로 변경

--- backend/src/test/java/com/festago/support/DatabaseClearExtension.java
+++ backend/src/test/java/com/festago/support/DatabaseClearExtension.java
@@ -1,7 +1,5 @@
 package com.festago.support;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,20 +8,21 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 import org.junit.jupiter.api.extension.TestInstanceFactoryContext;
 import org.junit.jupiter.api.extension.TestInstancePreConstructCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 public class DatabaseClearExtension implements TestInstancePreConstructCallback, AfterEachCallback {
 
     private static final Namespace CLEANER = Namespace.create("festago.database.cleaner");
     private static final Set<String> TABLES = new HashSet<>();
-    private static final String ENTITY_MANAGER = "entityManager";
+    private static final String JDBC_TEMPLATE = "jdbcTemplate";
 
     @Override
     public void preConstructTestInstance(TestInstanceFactoryContext factoryContext, ExtensionContext context)
         throws Exception {
-        EntityManagerFactory emf = SpringExtension.getApplicationContext(context)
-            .getBean(EntityManagerFactory.class);
-        context.getStore(CLEANER).put(ENTITY_MANAGER, emf.createEntityManager());
+        JdbcTemplate jdbcTemplate = SpringExtension.getApplicationContext(context)
+            .getBean(JdbcTemplate.class);
+        context.getStore(CLEANER).put(JDBC_TEMPLATE, jdbcTemplate);
 
         initialTable(context);
         truncate(context);
@@ -32,30 +31,24 @@ public class DatabaseClearExtension implements TestInstancePreConstructCallback,
     @SuppressWarnings("unchecked")
     private void initialTable(ExtensionContext context) {
         if (TABLES.isEmpty()) {
-            EntityManager em = context.getStore(CLEANER).get(ENTITY_MANAGER, EntityManager.class);
-            List<Object[]> tableInfos = em.createNativeQuery("SHOW TABLES").getResultList();
-            for (Object[] tableInfo : tableInfos) {
-                String tableName = (String) tableInfo[0];
-                TABLES.add(tableName);
-            }
-            em.clear();
+            JdbcTemplate jdbcTemplate = context.getStore(CLEANER).get(JDBC_TEMPLATE, JdbcTemplate.class);
+            List<String> tables = jdbcTemplate.query("SHOW TABLES", (rs, rowNum) -> rs.getString(1));
+            TABLES.addAll(tables);
         }
     }
 
     private void truncate(ExtensionContext context) {
-        EntityManager em = context.getStore(CLEANER).get(ENTITY_MANAGER, EntityManager.class);
-        em.getTransaction().begin();
-        em.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 0)).executeUpdate();
+        JdbcTemplate jdbcTemplate = context.getStore(CLEANER).get(JDBC_TEMPLATE, JdbcTemplate.class);
+        jdbcTemplate.update("SET FOREIGN_KEY_CHECKS = ?", 0);
         for (String table : TABLES) {
-            em.createNativeQuery(String.format("TRUNCATE TABLE %s", table)).executeUpdate();
+            jdbcTemplate.update("TRUNCATE TABLE " + table);
         }
-        em.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 1)).executeUpdate();
-        em.getTransaction().commit();
+        jdbcTemplate.update("SET FOREIGN_KEY_CHECKS = ?", 1);
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
         truncate(context);
-        context.getStore(CLEANER).remove(ENTITY_MANAGER);
+        context.getStore(CLEANER).remove(JDBC_TEMPLATE);
     }
 }
