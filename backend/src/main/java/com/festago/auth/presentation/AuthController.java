a375dc01b77e5401c9fa939e7641d7153fdc4f00
xxeol2 <jhj01177@naver.com>
2023-08-09 13:58:07 +0900

    refactor: redirect URI용 API 삭제

--- backend/src/main/java/com/festago/auth/presentation/AuthController.java
+++ backend/src/main/java/com/festago/auth/presentation/AuthController.java
@@ -1,15 +1,12 @@
 package com.festago.auth.presentation;
 
 import com.festago.auth.application.AuthService;
-import com.festago.auth.domain.SocialType;
 import com.festago.auth.dto.LoginRequest;
 import com.festago.auth.dto.LoginResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,13 +22,6 @@ public class AuthController {
     @PostMapping("/oauth2")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         LoginResponse response = authService.login(request);
-        return ResponseEntity.ok()
-            .body(response);
-    }
-
-    @GetMapping("/oauth2/kakao")
-    public ResponseEntity<LoginResponse> loginWithKakao(@RequestParam String accessToken) {
-        LoginResponse response = authService.login(new LoginRequest(SocialType.KAKAO, accessToken));
         return ResponseEntity.ok()
             .body(response);
     }
