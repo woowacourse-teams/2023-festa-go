c73d7e42011d77802519a22416297ba3c28f5364
vrexpert <vrexpert2302@gmail.com>
2023-08-09 22:08:03 +0900

    fix: RetrofitClient 오류 수정

--- android/festago/app/src/main/java/com/festago/festago/data/RetrofitClient.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/RetrofitClient.kt
@@ -43,7 +43,7 @@ object RetrofitClient {
     }
 
     private fun initNormalRetrofit(baseUrl: String) {
-        if (!::normalRetrofit.isInitialized) return
+        if (::normalRetrofit.isInitialized) return
         normalRetrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
@@ -51,7 +51,7 @@ object RetrofitClient {
     }
 
     private fun initAuthRetrofit(authDataSource: AuthDataSource, baseUrl: String) {
-        if (!::authRetrofit.isInitialized) return
+        if (::authRetrofit.isInitialized) return
         val okHttpClient: OkHttpClient = OkHttpClient
             .Builder()
             .addInterceptor(AuthInterceptor(authDataSource))
