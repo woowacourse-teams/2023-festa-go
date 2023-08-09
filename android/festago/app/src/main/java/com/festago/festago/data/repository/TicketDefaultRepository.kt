7cff9ce8c5ca670132caa1f6267d1429a13a11c2
EmilyCh0 <emily9803@ewhain.net>
2023-08-09 09:56:39 +0900

    feat: 과거 예매내역 불러오기 더미 데이터 제거 및 메서드 구현

--- android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
@@ -1,14 +1,9 @@
 package com.festago.festago.data.repository
 
 import com.festago.festago.data.service.TicketRetrofitService
-import com.festago.festago.domain.model.MemberTicketFestival
-import com.festago.festago.domain.model.Stage
 import com.festago.festago.domain.model.Ticket
 import com.festago.festago.domain.model.TicketCode
-import com.festago.festago.domain.model.TicketCondition
 import com.festago.festago.domain.repository.TicketRepository
-import kotlinx.coroutines.delay
-import java.time.LocalDateTime
 
 class TicketDefaultRepository(
     private val ticketRetrofitService: TicketRetrofitService,
@@ -51,23 +46,14 @@ class TicketDefaultRepository(
     }
 
     override suspend fun loadAllTickets(size: Int): Result<List<Ticket>> {
-        delay(2000)
-        return Result.success(
-            listOf(
-                Ticket(
-                    id = 1,
-                    number = 8934,
-                    entryTime = LocalDateTime.now(),
-                    condition = TicketCondition.BEFORE_ENTRY,
-                    reserveAt = LocalDateTime.now(),
-                    stage = Stage(id = 1, startTime = LocalDateTime.now()),
-                    festivalTicket = MemberTicketFestival(
-                        id = 4663,
-                        name = "테코대학교 무슨 축제 DAY1",
-                        thumbnail = "https://images.unsplash.com/photo-1592194996308-7b43878e84a6",
-                    ),
-                ),
-            ),
-        )
+        try {
+            val response = ticketRetrofitService.getHistoryTickets(size)
+            if (response.isSuccessful && response.body() != null) {
+                return Result.success(response.body()!!.toDomain())
+            }
+            return Result.failure(Throwable("code: ${response.code()} message: ${response.message()}"))
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
     }
 }
