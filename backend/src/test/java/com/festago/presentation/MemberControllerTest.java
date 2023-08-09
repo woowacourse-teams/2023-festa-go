66de07a638c110f0606bba5c585bcaeb6c4cbbc1
hyunseo <ohseo8813@gmail.com>
2023-08-09 18:13:35 +0900

    test: 테스트 깨지던 문제 수정
    - authExtractor 모킹 추가

--- backend/src/test/java/com/festago/presentation/MemberControllerTest.java
+++ backend/src/test/java/com/festago/presentation/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package com.festago.presentation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,6 +11,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.festago.application.MemberService;
 import com.festago.application.MemberTicketService;
+import com.festago.auth.domain.AuthExtractor;
+import com.festago.auth.domain.AuthPayload;
 import com.festago.dto.MemberProfileResponse;
 import com.festago.support.TestConfig;
 import java.nio.charset.StandardCharsets;
@@ -33,23 +36,29 @@ class MemberControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
-    
+
     @MockBean
     MemberService memberService;
 
     @MockBean
     MemberTicketService memberTicketService;
 
+    @MockBean
+    AuthExtractor authExtractor;
 
     @Test
     void 회원_정보_반환() throws Exception {
         // given
+        String token = "sampleToken";
         MemberProfileResponse expected = new MemberProfileResponse(1L, "닉네임", "www.profileImageUrl.com");
         given(memberService.findMemberProfile(anyLong()))
             .willReturn(expected);
+        given(authExtractor.extract(any()))
+            .willReturn(new AuthPayload(1L));
 
         // when & then
-        String content = mockMvc.perform(get("/members/profile"))
+        String content = mockMvc.perform(get("/members/profile")
+                .header("Authorization", "Bearer " + token))
             .andExpect(status().isOk())
             .andDo(print())
             .andReturn()
