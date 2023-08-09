4a7b6bc8d26c3cc539ba7ca66e0ba066c493bc9e
vrexpert <vrexpert2302@gmail.com>
2023-08-09 15:13:31 +0900

    feat: Retorfit Client 애플리케이션에서 초기화

--- android/festago/app/src/main/java/com/festago/festago/FestagoApplication.kt
+++ android/festago/app/src/main/java/com/festago/festago/FestagoApplication.kt
@@ -2,6 +2,8 @@ package com.festago.festago
 
 import android.app.Application
 import com.festago.festago.analytics.FirebaseAnalyticsHelper
+import com.festago.festago.data.RetrofitClient
+import com.festago.festago.data.datasource.SharedPrefAuthDataSource
 import com.kakao.sdk.common.KakaoSdk
 
 class FestagoApplication : Application() {
@@ -9,7 +11,8 @@ class FestagoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseAnalyticsHelper.create(applicationContext)
-
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        val authDataSource = SharedPrefAuthDataSource.getInstance(applicationContext)
+        RetrofitClient.create(authDataSource = authDataSource, baseUrl = BuildConfig.BASE_URL)
     }
 }
