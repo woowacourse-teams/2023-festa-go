8e5f358727a6b852fa5ff1b60b76dd570f77a34d
seokjin8678 <seokjin8678@gmail.com>
2023-08-09 15:33:35 +0900

    [BE] refactor: DatabaseCleaner의 스프링 빈 등록을 제외하고, ExtendWith에만 등록될 때 작동하게 변경 (#285) (#286)
    * refactor: DatabaseClearExtension 스프링 빈 없이 작동하도록 리팩터링
    
    * refactor: DatabaseCleaner 삭제
    
    * refactor: 불필요한 코드 삭제
    
    * refactor: JdbcTemplate를 사용하는 코드로 변경
    
    * refactor: SuppressWarnings 제거
    
    * refactor: BeforeAllCallback으로 변경
    
    * refactor: TestInstancePreConstructCallback 사용하게 변경

--- backend/src/test/java/com/festago/support/DatabaseClearExtension.java
+++ backend/src/test/java/com/festago/support/DatabaseClearExtension.java
@@ -1,31 +1,57 @@
 package com.festago.support;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 import org.junit.jupiter.api.extension.TestInstanceFactoryContext;
 import org.junit.jupiter.api.extension.TestInstancePreConstructCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class DatabaseClearExtension implements TestInstancePreConstructCallback, AfterEachCallback {
+public class DatabaseClearExtension implements TestInstancePreConstructCallback, AfterEachCallback, AfterAllCallback {
+
+    private static final Namespace CLEANER = Namespace.create("festago.database.cleaner");
+    private static final Set<String> TABLES = new HashSet<>();
+    private static final String JDBC_TEMPLATE = "jdbcTemplate";
 
     @Override
     public void preConstructTestInstance(TestInstanceFactoryContext factoryContext, ExtensionContext context)
         throws Exception {
-        clear(context);
+        JdbcTemplate jdbcTemplate = SpringExtension.getApplicationContext(context)
+            .getBean(JdbcTemplate.class);
+        context.getStore(CLEANER).put(JDBC_TEMPLATE, jdbcTemplate);
+        initialTable(context);
+        truncate(context);
     }
 
-    private void clear(ExtensionContext context) {
-        DatabaseCleaner databaseCleaner = getDataCleaner(context);
-        databaseCleaner.clear();
+    private void initialTable(ExtensionContext context) {
+        if (TABLES.isEmpty()) {
+            JdbcTemplate jdbcTemplate = context.getStore(CLEANER).get(JDBC_TEMPLATE, JdbcTemplate.class);
+            List<String> tables = jdbcTemplate.query("SHOW TABLES", (rs, rowNum) -> rs.getString(1));
+            TABLES.addAll(tables);
+        }
     }
 
-    private DatabaseCleaner getDataCleaner(ExtensionContext extensionContext) {
-        return SpringExtension.getApplicationContext(extensionContext)
-            .getBean(DatabaseCleaner.class);
+    private void truncate(ExtensionContext context) {
+        JdbcTemplate jdbcTemplate = context.getStore(CLEANER).get(JDBC_TEMPLATE, JdbcTemplate.class);
+        jdbcTemplate.update("SET FOREIGN_KEY_CHECKS = ?", 0);
+        for (String table : TABLES) {
+            jdbcTemplate.update("TRUNCATE TABLE " + table);
+        }
+        jdbcTemplate.update("SET FOREIGN_KEY_CHECKS = ?", 1);
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
-        clear(context);
+        truncate(context);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        context.getStore(CLEANER).remove(JDBC_TEMPLATE);
     }
 }
