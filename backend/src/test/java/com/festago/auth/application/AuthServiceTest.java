44be40d8d062d63c2a459cc69796730f352a820d
xxeol2 <jhj01177@naver.com>
2023-08-09 16:37:28 +0900

    feat: 회원 탈퇴 api 구현

--- backend/src/test/java/com/festago/auth/application/AuthServiceTest.java
+++ backend/src/test/java/com/festago/auth/application/AuthServiceTest.java
@@ -1,6 +1,8 @@
 package com.festago.auth.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -15,11 +17,13 @@ import com.festago.auth.dto.LoginResponse;
 import com.festago.auth.infrastructure.FestagoOAuth2Client;
 import com.festago.domain.Member;
 import com.festago.domain.MemberRepository;
+import com.festago.exception.NotFoundException;
 import com.festago.support.MemberFixture;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -85,5 +89,35 @@ class AuthServiceTest {
         // then
         assertThat(response.isNew())
             .isFalse();
+    }
+
+    @Nested
+    class 회원탈퇴 {
+
+        @Test
+        void 멤버가_없으면_예외() {
+            // given
+            Long memberId = 1L;
+            given(memberRepository.findById(memberId))
+                .willReturn(Optional.empty());
+
+            // then
+            assertThatThrownBy(() -> authService.deleteMember(memberId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("존재하지 않는 멤버입니다.");
+        }
+
+        @Test
+        void 성공() {
+            // given
+            Long memberId = 1L;
+            Member member = MemberFixture.member().id(memberId).build();
+            given(memberRepository.findById(memberId))
+                .willReturn(Optional.of(member));
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> authService.deleteMember(memberId));
+        }
     }
 }
