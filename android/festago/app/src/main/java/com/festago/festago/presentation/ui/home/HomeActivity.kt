ad160d7f39f86e639e4482b181caa1bd818eec41
vrexpert <vrexpert2302@gmail.com>
2023-08-10 20:37:55 +0900

    feat: 로그인이 안되면 로그인 화면으로 이동

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/HomeActivity.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/HomeActivity.kt
@@ -3,23 +3,41 @@ package com.festago.festago.presentation.ui.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.festago.festago.R
+import com.festago.festago.data.datasource.AuthLocalDataSource
+import com.festago.festago.data.repository.AuthDefaultRepository
+import com.festago.festago.data.retrofit.AuthRetrofitClient
+import com.festago.festago.data.retrofit.NormalRetrofitClient
 import com.festago.festago.databinding.ActivityHomeBinding
+import com.festago.festago.presentation.ui.home.HomeViewModel.HomeViewModelFactory
 import com.festago.festago.presentation.ui.home.festivallist.FestivalListFragment
 import com.festago.festago.presentation.ui.home.mypage.MyPageFragment
 import com.festago.festago.presentation.ui.home.ticketlist.TicketListFragment
+import com.festago.festago.presentation.ui.signin.SignInActivity
 
 class HomeActivity : AppCompatActivity() {
 
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val vm: HomeViewModel by viewModels {
+        HomeViewModelFactory(
+            AuthDefaultRepository(
+                authRetrofitService = NormalRetrofitClient.authRetrofitService,
+                authDataSource = AuthLocalDataSource.getInstance(this),
+                userRetrofitService = AuthRetrofitClient.instance.userRetrofitService,
+            ),
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBinding()
         initView()
+        initObserve()
     }
 
     private fun initBinding() {
@@ -29,22 +47,7 @@ class HomeActivity : AppCompatActivity() {
 
     private fun initView() {
         binding.bnvHome.setOnItemSelectedListener {
-            when (getItemType(it.itemId)) {
-                HomeItemType.FESTIVAL_LIST -> {
-                    changeFragment<FestivalListFragment>()
-                    binding.fabTicket.isSelected = false
-                }
-
-                HomeItemType.TICKET_LIST -> {
-                    changeFragment<TicketListFragment>()
-                    binding.fabTicket.isSelected = true
-                }
-
-                HomeItemType.MY_PAGE -> {
-                    changeFragment<MyPageFragment>()
-                    binding.fabTicket.isSelected = false
-                }
-            }
+            vm.loadHomeItem(getItemType(it.itemId))
             true
         }
 
@@ -55,6 +58,17 @@ class HomeActivity : AppCompatActivity() {
         changeFragment<FestivalListFragment>()
     }
 
+    private fun initObserve() {
+        vm.event.observe(this) { event ->
+            when (event) {
+                is HomeEvent.ShowFestivalList -> showFestivalList()
+                is HomeEvent.ShowTicketList -> showTicketList()
+                is HomeEvent.ShowMyPage -> showMyPage()
+                is HomeEvent.ShowSignIn -> showSignIn()
+            }
+        }
+    }
+
     private fun getItemType(menuItemId: Int): HomeItemType {
         return when (menuItemId) {
             R.id.item_festival -> HomeItemType.FESTIVAL_LIST
@@ -62,6 +76,25 @@ class HomeActivity : AppCompatActivity() {
             R.id.item_ticket -> HomeItemType.TICKET_LIST
             else -> throw IllegalArgumentException("menu item id not found")
         }
+    }
+
+    private fun showFestivalList() {
+        changeFragment<FestivalListFragment>()
+        binding.fabTicket.isSelected = false
+    }
+
+    private fun showTicketList() {
+        changeFragment<TicketListFragment>()
+        binding.fabTicket.isSelected = true
+    }
+
+    private fun showMyPage() {
+        changeFragment<MyPageFragment>()
+        binding.fabTicket.isSelected = false
+    }
+
+    private fun showSignIn() {
+        startActivity(SignInActivity.getIntent(this))
     }
 
     private inline fun <reified T : Fragment> changeFragment() {
