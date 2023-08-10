400170c4fdd17fd460bdd28e94b6c0e007d3dceb
vrexpert <vrexpert2302@gmail.com>
2023-08-10 15:43:27 +0900

    refactor: 헤더 메세지 상수화

--- android/festago/app/src/main/java/com/festago/festago/data/retrofit/AuthInterceptor.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/retrofit/AuthInterceptor.kt
@@ -7,8 +7,13 @@ class AuthInterceptor(private val token: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
-            .addHeader("Authorization", "Bearer $token")
+            .addHeader(HEADER_AUTHORIZATION, AUTHORIZATION_TOKEN_FORMAT.format(token))
             .build()
         return chain.proceed(request)
+    }
+
+    companion object {
+        private const val HEADER_AUTHORIZATION = "Authorization"
+        private const val AUTHORIZATION_TOKEN_FORMAT = "Bearer %s"
     }
 }
