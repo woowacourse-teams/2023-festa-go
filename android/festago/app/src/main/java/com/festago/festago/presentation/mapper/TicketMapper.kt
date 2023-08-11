dab30cceb68664bf591b21dc387a1473264582dd
vrexpert <vrexpert2302@gmail.com>
2023-08-11 16:47:15 +0900

    feat: 멤버 티켓 모델 생성

--- android/festago/app/src/main/java/com/festago/festago/presentation/mapper/TicketMapper.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/mapper/TicketMapper.kt
@@ -1,7 +1,9 @@
 package com.festago.festago.presentation.mapper
 
 import com.festago.festago.domain.model.Ticket
+import com.festago.festago.presentation.model.MemberTicketUiModel
 import com.festago.festago.presentation.model.TicketUiModel
+import java.time.LocalDateTime
 
 fun Ticket.toPresentation(): TicketUiModel = TicketUiModel(
     id = id,
@@ -15,4 +17,21 @@ fun Ticket.toPresentation(): TicketUiModel = TicketUiModel(
     festivalThumbnail = festivalTicket.thumbnail,
 )
 
-fun List<Ticket>.toPresentation(): List<TicketUiModel> = this.map { ticket -> ticket.toPresentation() }
+fun TicketUiModel.toMemberTicketModel(): MemberTicketUiModel = MemberTicketUiModel(
+    id = id,
+    number = number,
+    entryTime = entryTime,
+    condition = condition,
+    stage = stage,
+    reserveAt = reserveAt,
+    festivalId = festivalId,
+    festivalName = festivalName,
+    festivalThumbnail = festivalThumbnail,
+    canEntry = LocalDateTime.now().isAfter(entryTime),
+)
+
+fun List<Ticket>.toPresentation(): List<TicketUiModel> =
+    this.map { ticket -> ticket.toPresentation() }
+
+fun List<TicketUiModel>.toMemberTicketModel(): List<MemberTicketUiModel> =
+    this.map { ticketUiModel -> ticketUiModel.toMemberTicketModel() }
---
+++ android/festago/app/src/main/java/com/festago/festago/presentation/model/MemberTicketUiModel.kt
@@ -0,0 +1,16 @@
+package com.festago.festago.presentation.model
+
+import java.time.LocalDateTime
+
+data class MemberTicketUiModel(
+    val id: Long = -1,
+    val number: Int = -1,
+    val entryTime: LocalDateTime = LocalDateTime.MIN,
+    val reserveAt: LocalDateTime = LocalDateTime.MIN,
+    val condition: TicketConditionUiModel = TicketConditionUiModel.BEFORE_ENTRY,
+    val stage: StageUiModel = StageUiModel(),
+    val festivalId: Int = -1,
+    val festivalName: String = "",
+    val festivalThumbnail: String = "",
+    val canEntry: Boolean,
+)
