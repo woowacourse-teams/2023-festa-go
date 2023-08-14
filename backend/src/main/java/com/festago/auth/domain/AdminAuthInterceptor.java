d80c2ad5d5a7bf838fff7367f86577469c19294e
seokjin8678 <seokjin8678@gmail.com>
2023-08-14 16:54:51 +0900

    refactor: 쿠키가 없을 때 early return 하도록 변경

--- backend/src/main/java/com/festago/auth/domain/AdminAuthInterceptor.java
+++ backend/src/main/java/com/festago/auth/domain/AdminAuthInterceptor.java
@@ -3,10 +3,13 @@ package com.festago.auth.domain;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Objects;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class AdminAuthInterceptor implements HandlerInterceptor {
+
+    private static final String TOKEN = "token";
 
     private final AuthExtractor authExtractor;
 
@@ -18,16 +21,22 @@ public class AdminAuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
         throws Exception {
         Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (Objects.equals("token", cookie.getName())) {
-                    AuthPayload payload = authExtractor.extract(cookie.getValue());
-                    if (payload.getRole() == Role.ADMIN) {
-                        return true;
-                    }
+        if (cookies == null) {
+            return redirectLoginPage(response);
+        }
+        for (Cookie cookie : cookies) {
+            if (Objects.equals(TOKEN, cookie.getName())) {
+                AuthPayload payload = authExtractor.extract(cookie.getValue());
+                if (payload.getRole() == Role.ADMIN) {
+                    return true;
                 }
+                return redirectLoginPage(response);
             }
         }
+        return redirectLoginPage(response);
+    }
+
+    private boolean redirectLoginPage(HttpServletResponse response) throws IOException {
         response.sendRedirect("/admin/login");
         return false;
     }
