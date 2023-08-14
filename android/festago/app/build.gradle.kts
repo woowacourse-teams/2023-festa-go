99ebfefa984f02e5036b91b9600dd8d500212352
re4rk <37167652+re4rk@users.noreply.github.com>
2023-08-14 17:10:02 +0900

    chore: 시멘틱 버저닝 적용

--- android/festago/app/build.gradle.kts
+++ android/festago/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.festago.festago"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
