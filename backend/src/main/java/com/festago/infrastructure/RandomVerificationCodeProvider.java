6a6fe3aba898cfa4cfa7498ac20ca4d481a9ef1d
seokjin8678 <seokjin8678@gmail.com>
2023-08-16 20:09:35 +0900

    feat: 인증 코드 생성 도메인 추가

---
+++ backend/src/main/java/com/festago/infrastructure/RandomVerificationCodeProvider.java
@@ -0,0 +1,24 @@
+package com.festago.infrastructure;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.joining;
+
+import com.festago.domain.VerificationCode;
+import com.festago.domain.VerificationCodeProvider;
+import java.util.Random;
+import java.util.stream.IntStream;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomVerificationCodeProvider implements VerificationCodeProvider {
+
+    private static final Random random = new Random();
+    private static final int CODE_LENGTH = 6;
+
+    @Override
+    public VerificationCode provide() {
+        return IntStream.range(0, CODE_LENGTH)
+            .mapToObj(i -> String.valueOf(random.nextInt(10)))
+            .collect(collectingAndThen(joining(), VerificationCode::new));
+    }
+}
