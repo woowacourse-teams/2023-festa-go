e0fbad888e660ff6e25aa425ac33f89e64b1d288
BGuga <tmdwns9913@gmail.com>
2023-08-16 22:27:33 +0900

    refactor: bean 초기화 시 LogFunction 을 넣는다.

--- backend/src/main/java/com/festago/presentation/GlobalExceptionHandler.java
+++ backend/src/main/java/com/festago/presentation/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.festago.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.festago.dto.ErrorResponse;
 import com.festago.exception.BadRequestException;
 import com.festago.exception.ErrorCode;
@@ -9,10 +10,7 @@ import com.festago.exception.NotFoundException;
 import com.festago.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.EnumMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
@@ -21,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,47 +28,54 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String LOG_FORMAT_WITH_TRACE = "\n[🚨ERROR]\n{} ({} {})\n[CALLED BY] {} \n[REQUEST BODY] \n{}";
     private static final Logger errorLogger = LoggerFactory.getLogger("ErrorLogger");
 
-    private static final Map<Level, LogFunction> logFunctions = Map.of(
-        Level.INFO, errorLogger::info,
-        Level.WARN, errorLogger::warn,
-        Level.ERROR, errorLogger::error
-    );
+    private final EnumMap<Level, LogFunction> logFunctions = new EnumMap<>(Level.class);
+    private final ObjectMapper objectMapper;
+
+    public GlobalExceptionHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.logFunctions.put(Level.INFO, errorLogger::info);
+        this.logFunctions.put(Level.WARN, errorLogger::warn);
+        this.logFunctions.put(Level.ERROR, errorLogger::error);
+    }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handle(BadRequestException e, HttpServletRequest request) throws IOException {
-        log(Level.INFO, e, request);
+    public ResponseEntity<ErrorResponse> handle(BadRequestException e, HttpServletRequest request) {
+        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
+        log(Level.INFO, e, cachedRequest);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ErrorResponse> handle(UnauthorizedException e, HttpServletRequest request)
-        throws IOException {
-        log(Level.INFO, e, request);
+    public ResponseEntity<ErrorResponse> handle(UnauthorizedException e, HttpServletRequest request) {
+        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
+        log(Level.INFO, e, cachedRequest);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handle(NotFoundException e, HttpServletRequest request) throws IOException {
-        log(Level.INFO, e, request);
+    public ResponseEntity<ErrorResponse> handle(NotFoundException e, HttpServletRequest request) {
+        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
+        log(Level.INFO, e, cachedRequest);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(InternalServerException.class)
-    public ResponseEntity<ErrorResponse> handle(InternalServerException e, HttpServletRequest request)
-        throws IOException {
-        logWithTrace(Level.WARN, e, request);
+    public ResponseEntity<ErrorResponse> handle(InternalServerException e, HttpServletRequest request) {
+        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
+        logWithTrace(Level.WARN, e, cachedRequest);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handle(Exception e, HttpServletRequest request) throws IOException {
-        logWithTrace(Level.ERROR, e, request);
+    public ResponseEntity<ErrorResponse> handle(Exception e, HttpServletRequest request) {
+        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
+        logWithTrace(Level.ERROR, e, cachedRequest);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
-    private void log(Level logLevel, FestaGoException e, HttpServletRequest request) throws IOException {
+    private void log(Level logLevel, FestaGoException e, ContentCachingRequestWrapper request) {
         if (!errorLogger.isEnabledForLevel(logLevel)) {
             return;
         }
@@ -78,32 +84,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 e.getStackTrace()[0], getRequestPayload(request));
     }
 
-    private void logWithTrace(Level logLevel, FestaGoException e, HttpServletRequest request) throws IOException {
+    private void logWithTrace(Level logLevel, FestaGoException e, ContentCachingRequestWrapper request) {
         if (!errorLogger.isEnabledForLevel(logLevel)) {
             return;
         }
         logFunctions.get(logLevel)
             .log(LOG_FORMAT, e.getErrorCode(), e.getMessage(), request.getMethod(), request.getRequestURI(),
-                e.getStackTrace()[0], getRequestPayload(request), e);
+                getRequestPayload(request), e);
     }
 
-    private void logWithTrace(Level logLevel, Exception e, HttpServletRequest request) throws IOException {
+    private void logWithTrace(Level logLevel, Exception e, ContentCachingRequestWrapper request) {
         if (!errorLogger.isEnabledForLevel(logLevel)) {
             return;
         }
         logFunctions.get(logLevel)
             .log(LOG_FORMAT_WITH_TRACE, e.getClass().getSimpleName(), request.getMethod(), request.getRequestURI(),
-                e.getStackTrace()[0], getRequestPayload(request), e);
+                getRequestPayload(request), e);
     }
 
-    private String getRequestPayload(HttpServletRequest request) throws IOException {
-        try (InputStream inputStream = request.getInputStream(); Scanner scanner = new Scanner(inputStream,
-            StandardCharsets.UTF_8)) {
-            if (scanner.useDelimiter("\\A").hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
+    private String getRequestPayload(ContentCachingRequestWrapper request) {
+        try {
+            return objectMapper.readTree(request.getContentAsByteArray()).toPrettyString();
+        } catch (IOException e) {
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
 
