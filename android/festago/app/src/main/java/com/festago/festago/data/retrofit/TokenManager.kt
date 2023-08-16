3ed5b88cb9dfd2fa8b1429fad7ba1db2efe1a89b
re4rk <37167652+re4rk@users.noreply.github.com>
2023-08-16 14:13:09 +0900

    refactor: 불 필요한 runBlocking 제거

--- android/festago/app/src/main/java/com/festago/festago/data/retrofit/TokenManager.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/retrofit/TokenManager.kt
@@ -2,15 +2,12 @@ package com.festago.festago.data.retrofit
 
 import com.festago.festago.domain.repository.TokenRepository
 import com.kakao.sdk.auth.TokenManagerProvider
-import kotlinx.coroutines.runBlocking
 
 class TokenManager(private val tokenRepository: TokenRepository) {
     fun refreshToken() {
-        runBlocking {
-            tokenRepository.refreshToken(
-                token = TokenManagerProvider.instance.manager.getToken()?.accessToken ?: NULL_TOKEN,
-            )
-        }
+        tokenRepository.refreshToken(
+            token = TokenManagerProvider.instance.manager.getToken()?.accessToken ?: NULL_TOKEN,
+        )
     }
 
     val token: String
