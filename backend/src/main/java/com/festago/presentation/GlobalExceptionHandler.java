4ac00fd7423c564e3a6a4cb6e3fdbbb0cb813918
seokjin8678 <seokjin8678@gmail.com>
2023-08-17 02:21:50 +0900

    refactor: 예외 메시지 변경

--- backend/src/main/java/com/festago/presentation/GlobalExceptionHandler.java
+++ backend/src/main/java/com/festago/presentation/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.festago.presentation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.festago.dto.ErrorResponse;
 import com.festago.exception.BadRequestException;
 import com.festago.exception.ErrorCode;
@@ -9,110 +8,75 @@ import com.festago.exception.InternalServerException;
 import com.festago.exception.NotFoundException;
 import com.festago.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.EnumMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final String LOG_FORMAT = "\n[🚨ERROR]\n{}: {} ({} {})\n[CALLED BY] {} \n[REQUEST BODY] \n{}";
-    private static final String LOG_FORMAT_WITH_TRACE = "\n[🚨ERROR]\n{} ({} {})\n[CALLED BY] {} \n[REQUEST BODY] \n{}";
-    private static final Logger errorLogger = LoggerFactory.getLogger("ErrorLogger");
+    private static final String LOG_FORMAT_ERROR_CODE = "\n[🚨ERROR] - ({} {})\n{}";
+    private static final String LOG_FORMAT = "\n[🚨ERROR] - ({} {})";
+    /*
+        [🚨ERROR] - (POST /admin/warn)
+        FOR_TEST_ERROR
+        com.festago.exception.InternalServerException: 테스트용 에러입니다.
+     */
 
-    private final EnumMap<Level, LogFunction> logFunctions = new EnumMap<>(Level.class);
-    private final ObjectMapper objectMapper;
+    private final ErrorLogger errorLogger;
 
-    public GlobalExceptionHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-        this.logFunctions.put(Level.INFO, errorLogger::info);
-        this.logFunctions.put(Level.WARN, errorLogger::warn);
-        this.logFunctions.put(Level.ERROR, errorLogger::error);
+    public GlobalExceptionHandler(ErrorLogger errorLogger) {
+        this.errorLogger = errorLogger;
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handle(BadRequestException e, HttpServletRequest request) {
-        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
-        log(Level.INFO, e, cachedRequest);
+        log(Level.INFO, e, request);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handle(UnauthorizedException e, HttpServletRequest request) {
-        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
-        log(Level.INFO, e, cachedRequest);
+        log(Level.INFO, e, request);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handle(NotFoundException e, HttpServletRequest request) {
-        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
-        log(Level.INFO, e, cachedRequest);
+        log(Level.INFO, e, request);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<ErrorResponse> handle(InternalServerException e, HttpServletRequest request) {
-        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
-        logWithTrace(Level.WARN, e, cachedRequest);
+        log(Level.WARN, e, request);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handle(Exception e, HttpServletRequest request) {
-        ContentCachingRequestWrapper cachedRequest = (ContentCachingRequestWrapper) request;
-        logWithTrace(Level.ERROR, e, cachedRequest);
+        log(Level.ERROR, e, request);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
-    private void log(Level logLevel, FestaGoException e, ContentCachingRequestWrapper request) {
+    private void log(Level logLevel, FestaGoException e, HttpServletRequest request) {
         if (!errorLogger.isEnabledForLevel(logLevel)) {
             return;
         }
-        logFunctions.get(logLevel)
-            .log(LOG_FORMAT, e.getErrorCode(), e.getMessage(), request.getMethod(), request.getRequestURI(),
-                e.getStackTrace()[0], getRequestPayload(request));
+        errorLogger.get(logLevel)
+            .log(LOG_FORMAT_ERROR_CODE, request.getMethod(), request.getRequestURI(), e.getErrorCode(), e);
     }
 
-    private void logWithTrace(Level logLevel, FestaGoException e, ContentCachingRequestWrapper request) {
+    private void log(Level logLevel, Exception e, HttpServletRequest request) {
         if (!errorLogger.isEnabledForLevel(logLevel)) {
             return;
         }
-        logFunctions.get(logLevel)
-            .log(LOG_FORMAT, e.getErrorCode(), e.getMessage(), request.getMethod(), request.getRequestURI(),
-                getRequestPayload(request), e);
-    }
-
-    private void logWithTrace(Level logLevel, Exception e, ContentCachingRequestWrapper request) {
-        if (!errorLogger.isEnabledForLevel(logLevel)) {
-            return;
-        }
-        logFunctions.get(logLevel)
-            .log(LOG_FORMAT_WITH_TRACE, e.getClass().getSimpleName(), request.getMethod(), request.getRequestURI(),
-                getRequestPayload(request), e);
-    }
-
-    private String getRequestPayload(ContentCachingRequestWrapper request) {
-        try {
-            return objectMapper.readTree(request.getContentAsByteArray()).toPrettyString();
-        } catch (IOException e) {
-            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @FunctionalInterface
-    interface LogFunction {
-
-        void log(String format, Object... arguments);
+        errorLogger.get(logLevel)
+            .log(LOG_FORMAT, request.getMethod(), request.getRequestURI(), e);
     }
 }
