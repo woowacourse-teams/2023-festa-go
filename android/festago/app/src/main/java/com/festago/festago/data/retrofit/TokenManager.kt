0939b65ac95a783659aee4c5276047fcd5ada836
re4rk <37167652+re4rk@users.noreply.github.com>
2023-08-16 15:16:56 +0900

    refactor: 코딩 컨벤션 적용

--- android/festago/app/src/main/java/com/festago/festago/data/retrofit/TokenManager.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/retrofit/TokenManager.kt
@@ -4,14 +4,15 @@ import com.festago.festago.domain.repository.TokenRepository
 import com.kakao.sdk.auth.TokenManagerProvider
 
 class TokenManager(private val tokenRepository: TokenRepository) {
+
+    val token: String
+        get() = tokenRepository.token ?: NULL_TOKEN
+
     fun refreshToken() {
         tokenRepository.refreshToken(
             token = TokenManagerProvider.instance.manager.getToken()?.accessToken ?: NULL_TOKEN,
         )
     }
-
-    val token: String
-        get() = tokenRepository.token ?: NULL_TOKEN
 
     companion object {
         private const val NULL_TOKEN = "null"
