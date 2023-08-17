304d41a4e7815b428734761826b69a474b86fb4b
해시 <67777523+EmilyCh0@users.noreply.github.com>
2023-08-17 11:06:30 +0900

    [AN/USER] feat: 프로가드 난독화 수정(#378) #380

--- android/festago/app/build.gradle.kts
+++ android/festago/app/build.gradle.kts
@@ -35,7 +35,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
