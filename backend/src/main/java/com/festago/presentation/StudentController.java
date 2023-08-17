a0612d9abb48d3da943f198fb3e4e06dacdc5623
seokjin8678 <seokjin8678@gmail.com>
2023-08-17 17:24:28 +0900

    fix: 최신 브랜치 반영

--- backend/src/main/java/com/festago/presentation/StudentController.java
+++ backend/src/main/java/com/festago/presentation/StudentController.java
@@ -1,9 +1,10 @@
 package com.festago.presentation;
 
 import com.festago.application.StudentService;
-import com.festago.auth.domain.Login;
-import com.festago.auth.dto.LoginMember;
+import com.festago.auth.annotation.Member;
 import com.festago.dto.StudentSendMailRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
+@Tag(name = "학생 요청")
 public class StudentController {
 
     private final StudentService studentService;
@@ -21,9 +23,10 @@ public class StudentController {
     }
 
     @PostMapping("/send-verification")
-    public ResponseEntity<Void> sendEmail(@Login LoginMember loginMember,
+    @Operation(description = "학교 인증 이메일을 전송한다.", summary = "학생 인증 이메일 전송")
+    public ResponseEntity<Void> sendEmail(@Member Long memberId,
                                           @RequestBody StudentSendMailRequest request) {
-        studentService.sendVerificationMail(loginMember.memberId(), request);
+        studentService.sendVerificationMail(memberId, request);
         return ResponseEntity.ok()
             .build();
     }
--- backend/src/test/java/com/festago/presentation/StudentControllerTest.java
+++ backend/src/test/java/com/festago/presentation/StudentControllerTest.java
@@ -1,20 +1,17 @@
 package com.festago.presentation;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.festago.application.StudentService;
-import com.festago.auth.domain.AuthExtractor;
-import com.festago.auth.domain.AuthPayload;
 import com.festago.dto.StudentSendMailRequest;
+import com.festago.support.CustomWebMvcTest;
+import com.festago.support.WithMockAuth;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -22,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@WebMvcTest(StudentController.class)
+@CustomWebMvcTest(StudentController.class)
 class StudentControllerTest {
 
     @Autowired
@@ -34,15 +31,11 @@ class StudentControllerTest {
     @MockBean
     StudentService studentService;
 
-    @MockBean
-    AuthExtractor authExtractor;
-
     @Test
+    @WithMockAuth
     void 학교_인증_요청() throws Exception {
         // given
         StudentSendMailRequest request = new StudentSendMailRequest("user", 1L);
-        given(authExtractor.extract(any()))
-            .willReturn(new AuthPayload(1L));
 
         // when & then
         mockMvc.perform(post("/students/send-verification")
@@ -50,6 +43,5 @@ class StudentControllerTest {
                 .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                 .content(objectMapper.writeValueAsString(request)))
             .andExpect(status().isOk());
-
     }
 }
