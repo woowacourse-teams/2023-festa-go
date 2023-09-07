c2920022fc73cd770a8c528b44214e0b34ce7519
xxeol2 <jhj01177@naver.com>
2023-09-07 17:48:40 +0900

    chore: Ticketing script 주석 추가

--- backend/src/main/resources/scripts/ticketing.lua
+++ backend/src/main/resources/scripts/ticketing.lua
@@ -1,13 +1,19 @@
 local memberTryCount = redis.call("INCR", KEYS[1])
 redis.call("EXPIRE", KEYS[1], 1)
+
+-- 따닥인 경우
 if memberTryCount > tonumber(ARGV[1])
 then
   return -2
 end
+
 local remainAmount = redis.call("DECR", KEYS[2])
+
+-- 품절인 경우
 if remainAmount < 0
 then
   return -1
 end
+
 return remainAmount
 
