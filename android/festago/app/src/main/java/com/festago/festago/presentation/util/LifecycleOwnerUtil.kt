3b896e402da86d04b942b4b05a1c9b4f505e7a53
vrexpert <vrexpert2302@gmail.com>
2023-09-11 14:08:12 +0900

    refactor: repeatOnStartedState util 추가

---
+++ android/festago/app/src/main/java/com/festago/festago/presentation/util/LifecycleOwnerUtil.kt
@@ -0,0 +1,15 @@
+package com.festago.festago.presentation.util
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+
+fun LifecycleOwner.repeatOnStartedState(action: suspend () -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            action()
+        }
+    }
+}
