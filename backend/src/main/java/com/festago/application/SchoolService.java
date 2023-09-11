ba9b4a9e167451751e6df3376c2a950325252def
xxeol2 <jhj01177@naver.com>
2023-09-11 17:43:07 +0900

    [BE] feat: 학교 목록 조회 기능 구현 (#431) (#432)
    * feat: School 목록 조회 기능 구현
    
    * feat: School 목록 조회 API 구현
    
    * feat: Transactional(readonly=true) 적용

---
+++ backend/src/main/java/com/festago/application/SchoolService.java
@@ -0,0 +1,22 @@
+package com.festago.application;
+
+import com.festago.domain.SchoolRepository;
+import com.festago.dto.SchoolsResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SchoolService {
+
+    private final SchoolRepository schoolRepository;
+
+    public SchoolService(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public SchoolsResponse findAll() {
+        return SchoolsResponse.from(schoolRepository.findAll());
+    }
+}
