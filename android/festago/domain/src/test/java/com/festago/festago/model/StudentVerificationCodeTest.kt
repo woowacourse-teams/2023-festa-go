2c0960594643ca692f46bddb5821ba712dd2fe91
vrexpert <vrexpert2302@gmail.com>
2023-09-11 22:12:21 +0900

    test: 학생 인증 코드 예외 테스트 추가

--- android/festago/domain/src/test/java/com/festago/festago/model/StudentVerificationCodeTest.kt
+++ android/festago/domain/src/test/java/com/festago/festago/model/StudentVerificationCodeTest.kt
@@ -14,8 +14,20 @@ class StudentVerificationCodeTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["1234567", "a123456", "asdfeq", "999999a"])
-    fun `학생 인증 코드가 6자리 숫자가 아니면 예외가 발생한다 `(code: String) {
+    @ValueSource(strings = ["1234567", "11", "45567", "0"])
+    fun `학생 인증 코드가 6자리가 아니면 예외가 발생한다 `(code: String) {
+        assertThrows<IllegalArgumentException> { StudentVerificationCode(code) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["123456a", "a123456", "asdfeq", "999999a"])
+    fun `학생 인증 코드에 숫자가 아닌 문자가 포함되면 예외가 발생한다 `(code: String) {
+        assertThrows<IllegalArgumentException> { StudentVerificationCode(code) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["12345&", "a1#345", "******"])
+    fun `학생 인증 코드가 특수 문자가 포함되면 예외가 발생한다 `(code: String) {
         assertThrows<IllegalArgumentException> { StudentVerificationCode(code) }
     }
 }
