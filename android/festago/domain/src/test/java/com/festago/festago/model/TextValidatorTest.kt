f135fcdc8470be488d1cdbfad9d831b1ef820ad8
vrexpert <vrexpert2302@gmail.com>
2023-09-14 14:30:42 +0900

    refactor: Text 검증 테스트 네이밍 및 구조 리팩터링

--- android/festago/domain/src/test/java/com/festago/festago/model/TextValidatorTest.kt
+++ android/festago/domain/src/test/java/com/festago/festago/model/TextValidatorTest.kt
@@ -8,25 +8,40 @@ class TextValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["asdf", "fkwk", "test"])
-    fun `4자리 알파벳 소문자이면 검증된 문자이다`(sentence: String) {
+    fun `알파벳만 허용하고 기대 길이가 4 일 때 다음은 검증된 Text 이다`(text: String) {
+        // given
         val textValidator = TextValidator.of("abcdefghijklmnopqrstuvwxyz", 4)
 
-        assertThat(textValidator.isValid(sentence)).isTrue
+        // when
+        val isValidText = textValidator.isValid(text)
+
+        // then
+        assertThat(isValidText).isTrue
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["12345", "23456", "20382"])
-    fun `5자리 숫자이면 검증된 문자이다`(sentence: String) {
+    fun `숫자만 허용하고 기대 길이가 5 일 때 다음은 검증된 Text 이다`(text: String) {
+        // given
         val textValidator = TextValidator.of("0123456789", 5)
 
-        assertThat(textValidator.isValid(sentence)).isTrue
+        // when
+        val isValidText = textValidator.isValid(text)
+
+        // then
+        assertThat(isValidText).isTrue
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["asd12", "145667", "11"])
-    fun `4자리 숫자가 아니면 검증된 문자이다`(sentence: String) {
+    fun `숫자만 허용하고 기대 길이가 4 일 때 다음은 검증된 Text 가 아니다`(text: String) {
+        // given
         val textValidator = TextValidator.of("0123456789", 4)
 
-        assertThat(textValidator.isValid(sentence)).isFalse
+        // when
+        val isValidText = textValidator.isValid(text)
+
+        // then
+        assertThat(isValidText).isFalse
     }
 }
