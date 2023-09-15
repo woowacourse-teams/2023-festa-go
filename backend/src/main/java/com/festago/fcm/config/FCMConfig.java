a304bcea3e358c95bb1235a4dd6560cc98a60f50
BGuga <tmdwns9913@gmail.com>
2023-09-15 22:55:38 +0900

    feat: 테스트 환경에서 FirebaseMessaging 을 MockBean 으로 등록한다

--- backend/src/main/java/com/festago/fcm/config/FCMConfig.java
+++ backend/src/main/java/com/festago/fcm/config/FCMConfig.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
+@Profile("!test")
 public class FCMConfig {
 
     @Value("${fcm.key.path}")
--- backend/src/test/java/com/festago/application/integration/ApplicationIntegrationTest.java
+++ backend/src/test/java/com/festago/application/integration/ApplicationIntegrationTest.java
@@ -1,12 +1,16 @@
 package com.festago.application.integration;
 
 import com.festago.support.DatabaseClearExtension;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @ExtendWith(DatabaseClearExtension.class)
 public abstract class ApplicationIntegrationTest {
 
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
 }
