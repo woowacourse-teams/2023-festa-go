4c5c2a54df6318033f1141fbae61be003531738e
BGuga <tmdwns9913@gmail.com>
2023-09-15 21:09:25 +0900

    feat: NotificationFacade 생성

---
+++ backend/src/main/java/com/festago/fcm/application/NotificationFacade.java
@@ -0,0 +1,88 @@
+package com.festago.fcm.application;
+
+import com.festago.common.exception.ErrorCode;
+import com.festago.common.exception.InternalServerException;
+import com.festago.entry.dto.EntryProcessEvent;
+import com.festago.fcm.dto.MemberFCMResponse;
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidNotification;
+import com.google.firebase.messaging.BatchResponse;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.SendResponse;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Service
+public class NotificationFacade {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationFacade.class);
+
+    private final FirebaseMessaging firebaseMessaging;
+
+    private final MemberFCMService memberFCMService;
+
+    public NotificationFacade(FirebaseMessaging firebaseMessaging, MemberFCMService memberFCMService) {
+        this.firebaseMessaging = firebaseMessaging;
+        this.memberFCMService = memberFCMService;
+    }
+
+    @TransactionalEventListener
+    @Async
+    public void receiveEvent(EntryProcessEvent event) {
+        // TODO: channelId로 이벤트 구분 하는 로직 필요
+        List<Message> messages = createMessages(getMemberFCMToken(event.memberId()), null);
+        try {
+            BatchResponse batchResponse = firebaseMessaging.sendAll(messages);
+            checkAllSuccess(batchResponse, event.memberId());
+        } catch (FirebaseMessagingException e) {
+            logger.error("fail send FCM message", e);
+            throw new InternalServerException(ErrorCode.FAIL_SEND_FCM_MESSAGE);
+        }
+    }
+
+    private List<String> getMemberFCMToken(Long memberId) {
+        return memberFCMService.findMemberFCM(memberId).memberFCMs().stream()
+            .map(MemberFCMResponse::fcmToken)
+            .toList();
+    }
+
+    private List<Message> createMessages(List<String> tokens, String channelId) {
+        return tokens.stream()
+            .map(token -> createMessage(token, channelId))
+            .toList();
+    }
+
+    private Message createMessage(String token, String channelId) {
+        return Message.builder()
+            .setAndroidConfig(createAndroidConfig(channelId))
+            .setToken(token)
+            .build();
+    }
+
+    private AndroidConfig createAndroidConfig(String channelId) {
+        return AndroidConfig.builder()
+            .setNotification(createAndroidNotification(channelId))
+            .build();
+    }
+
+    private AndroidNotification createAndroidNotification(String channelId) {
+        return AndroidNotification.builder()
+            .setChannelId(channelId)
+            .build();
+    }
+
+    private void checkAllSuccess(BatchResponse batchResponse, Long memberid) {
+        List<SendResponse> failSend = batchResponse.getResponses().stream()
+            .filter(sendResponse -> !sendResponse.isSuccessful())
+            .toList();
+
+        logger.warn("member {} 에 대한 다음 요청들이 실패했습니다. {}", memberid, failSend);
+        throw new InternalServerException(ErrorCode.FAIL_SEND_FCM_MESSAGE);
+    }
+}
---
+++ backend/src/test/java/com/festago/fcm/application/NotificationFacadeTest.java
@@ -0,0 +1,70 @@
+package com.festago.fcm.application;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.festago.common.exception.InternalServerException;
+import com.festago.entry.dto.EntryProcessEvent;
+import com.festago.fcm.dto.MemberFCMResponse;
+import com.festago.fcm.dto.MemberFCMsResponse;
+import com.google.firebase.messaging.BatchResponse;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.SendResponse;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationFacadeTest {
+
+    @Mock
+    FirebaseMessaging firebaseMessaging;
+
+    @Mock
+    MemberFCMService memberFCMService;
+
+    @InjectMocks
+    NotificationFacade notificationFacade;
+
+    @Test
+    void 유저의_FCM_요청_중_하나라도_실패하면_예외() throws FirebaseMessagingException {
+        // given
+        given(memberFCMService.findMemberFCM(anyLong())).willReturn(
+            new MemberFCMsResponse(List.of(new MemberFCMResponse(1L, 1L, "token1"), new MemberFCMResponse(2L, 1L, "token2"))));
+
+        given(firebaseMessaging.sendAll(any())).willReturn(new MockBatchResponse());
+
+        EntryProcessEvent event = new EntryProcessEvent(1L);
+
+        // when
+        Assertions.assertThatThrownBy(() -> notificationFacade.receiveEvent(event)).isInstanceOf(InternalServerException.class);
+    }
+
+    private static class MockBatchResponse implements BatchResponse {
+
+        @Override
+        public List<SendResponse> getResponses() {
+            SendResponse mockResponse = mock(SendResponse.class);
+            when(mockResponse.isSuccessful()).thenReturn(false);
+            return List.of(mockResponse, mockResponse);
+        }
+
+        @Override
+        public int getSuccessCount() {
+            return 0;
+        }
+
+        @Override
+        public int getFailureCount() {
+            return 0;
+        }
+    }
+}
