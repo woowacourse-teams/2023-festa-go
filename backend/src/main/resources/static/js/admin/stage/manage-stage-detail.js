dc30b055628bec55d459eb9b3af5115c7c74f3e5
seokjin8678 <seokjin8678@gmail.com>
2023-09-26 01:01:51 +0900

    feat: 어드민 페이지 티켓 생성, 조회 기능 추가

--- backend/src/main/resources/static/js/admin/stage/manage-stage-detail.js
+++ backend/src/main/resources/static/js/admin/stage/manage-stage-detail.js
@@ -41,16 +41,66 @@ function fetchStage() {
   })
 }
 
-fetchStage()
+fetchStage();
+
+function fetchTickets() {
+  const ticketGrid = document.getElementById("ticketGrid");
+  const stageId = getResourceId(new URL(window.location.href));
+  fetch(`/stages/${stageId}/tickets`).then(res => {
+    if (!res.ok) {
+      throw new Error("서버에 연결할 수 없습니다.")
+    }
+    return res.json();
+  }).then(data => {
+    const tickets = data.tickets;
+    for (const ticket of tickets) {
+      const row = document.createElement("div");
+      row.classList.add("row", "align-items-center", "gx-0", "py-1",
+          "border-top");
+
+      const idColumn = document.createElement("div");
+      idColumn.classList.add("col-1");
+      idColumn.textContent = ticket.id;
+
+      const ticketTypeColumn = document.createElement("div");
+      ticketTypeColumn.classList.add("col-3");
+      ticketTypeColumn.textContent = ticket.ticketType;
+
+      const totalAmountColumn = document.createElement("div");
+      totalAmountColumn.classList.add("col-3");
+      totalAmountColumn.textContent = ticket.totalAmount;
+
+      const remainAmountColumn = document.createElement("div");
+      remainAmountColumn.classList.add("col-3");
+      remainAmountColumn.textContent = ticket.remainAmount;
+
+      const buttonColumn = document.createElement("div");
+      buttonColumn.classList.add("col-2")
+      const button = document.createElement("a");
+      button.classList.add("btn", "btn-primary");
+      button.setAttribute("href", `/admin/tickets/${ticket.id}`);
+      button.textContent = "편집";
+      buttonColumn.append(button);
+
+      row.append(idColumn, ticketTypeColumn, totalAmountColumn,
+          remainAmountColumn, buttonColumn);
+      ticketGrid.append(row);
+    }
+  })
+}
+
+fetchTickets();
 
 function init() {
   const schoolUpdateForm = document.getElementById("schoolUpdateForm");
+  const ticketCreateForm = document.getElementById("ticketCreateForm");
   const deleteBtn = document.getElementById("deleteBtn");
   const actualDeleteBtn = document.getElementById("actualDeleteBtn");
 
   schoolUpdateForm.addEventListener("submit", updateStage);
   deleteBtn.addEventListener("click", openDeleteConfirmModal);
   actualDeleteBtn.addEventListener("click", deleteStage)
+  ticketCreateForm.addEventListener("submit", createTicket)
 }
 
 function updateStage(e) {
@@ -117,6 +167,61 @@ function deleteStage() {
     deleteConfirmModal.hide();
     alert(error.message);
   });
+}
+
+function createTicket(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const ticketData = {
+    stageId: document.getElementById("id").value,
+    ticketType: formData.get("ticketType"),
+    amount: formData.get("amount"),
+    entryTime: formData.get("entryTime"),
+  };
+  validateTicket(ticketData);
+
+  fetch("/admin/api/tickets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(ticketData)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      return response.json().then(data => {
+        throw new Error(data.message || "티켓 생성에 실패하였습니다.");
+      });
+    }
+  })
+  .then(() => {
+    alert("티켓이 성공적으로 생성되었습니다!");
+    location.reload();
+  })
+  .catch(error => {
+    alert(error.message);
+  });
+}
+
+function validateTicket(ticketData) {
+  let hasError = false;
+  if (ticketData.amount <= 0) {
+    document.getElementById("amount").classList.add("is-invalid");
+    document.getElementById("amount-feedback")
+        .textContent = "수량은 0보다 많아야 합니다."
+    hasError = true;
+  }
+  if (ticketData.amount > 100000) {
+    document.getElementById("amount").classList.add("is-invalid");
+    document.getElementById("amount-feedback")
+        .textContent = "너무 많은 수량은 생성할 수 없습니다."
+    hasError = true;
+  }
+  if (hasError) {
+    throw new Error("검증이 실패하였습니다.");
+  }
 }
 
 init();
