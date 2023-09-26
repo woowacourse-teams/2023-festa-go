df074d547958c5aff2a6076bd53511e9f95c43a7
xxeol2 <jhj01177@naver.com>
2023-09-26 16:52:00 +0900

    feat: StudentSendMailRequest validation 추가

--- backend/src/main/java/com/festago/student/dto/StudentSendMailRequest.java
+++ backend/src/main/java/com/festago/student/dto/StudentSendMailRequest.java
@@ -1,8 +1,11 @@
 package com.festago.student.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record StudentSendMailRequest(
-    String username,
-    Long schoolId
+    @NotBlank(message = "username은 공백일 수 없습니다.") String username,
+    @NotNull(message = "schoolId는 null 일 수 없습니다.") Long schoolId
 ) {
 
 }
