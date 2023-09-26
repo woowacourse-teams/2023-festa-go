903fc5fd09fd901a81b6bae60164e47c7c61ac76
BGuga <tmdwns9913@gmail.com>
2023-09-26 16:28:37 +0900

    chore: 파라미터 네이밍 변경

--- backend/src/main/java/com/festago/fcm/dto/MemberFCMsResponse.java
+++ backend/src/main/java/com/festago/fcm/dto/MemberFCMsResponse.java
@@ -9,8 +9,8 @@ import java.util.List;
 public record MemberFCMsResponse(List<MemberFCMResponse> memberFCMs
 ) {
 
-    public static MemberFCMsResponse from(List<MemberFCM> memberFCMS) {
-        return memberFCMS.stream()
+    public static MemberFCMsResponse from(List<MemberFCM> memberFCMs) {
+        return memberFCMs.stream()
             .map(MemberFCMResponse::from)
             .collect(collectingAndThen(toList(), MemberFCMsResponse::new));
     }
