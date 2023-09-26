c271b9168e3b22d0539ab731dc8c7e9385a4d379
xxeol2 <jhj01177@naver.com>
2023-09-26 16:52:57 +0900

    refactor: FestivalCreateRequest NotNull/NotBlank 제약조건 추가

--- backend/src/main/java/com/festago/festival/dto/FestivalCreateRequest.java
+++ backend/src/main/java/com/festago/festival/dto/FestivalCreateRequest.java
@@ -1,15 +1,20 @@
 package com.festago.festival.dto;
 
 import com.festago.festival.domain.Festival;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public record FestivalCreateRequest(
-    @NotNull(message = "name 은 null 일 수 없습니다.") String name,
-    @DateTimeFormat(iso = ISO.DATE) LocalDate startDate,
-    @DateTimeFormat(iso = ISO.DATE) LocalDate endDate,
+    @NotBlank(message = "name 은 공백일 수 없습니다.") String name,
+    @NotNull(message = "startDate 는 null 일 수 없습니다.")
+    @DateTimeFormat(iso = ISO.DATE)
+    LocalDate startDate,
+    @NotNull(message = "endDate 는 null 일 수 없습니다.")
+    @DateTimeFormat(iso = ISO.DATE)
+    LocalDate endDate,
     String thumbnail) {
 
     public Festival toEntity() {
