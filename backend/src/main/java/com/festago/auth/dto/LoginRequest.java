1943633c49e794cf49c8289aa7a4b592c190a3e5
xxeol2 <jhj01177@naver.com>
2023-09-26 16:55:48 +0900

    refactor: LoginRequest NotNull -> NotBlank 로 변경

--- backend/src/main/java/com/festago/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/festago/auth/dto/LoginRequest.java
@@ -1,10 +1,11 @@
 package com.festago.auth.dto;
 
 import com.festago.auth.domain.SocialType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
     @NotNull(message = "socialType 은 null 일 수 없습니다.") SocialType socialType,
-    @NotNull(message = "acessToken 은 null 일 수 없습니다.") String accessToken) {
+    @NotBlank(message = "acessToken 은 공백일 수 없습니다.") String accessToken) {
 
 }
