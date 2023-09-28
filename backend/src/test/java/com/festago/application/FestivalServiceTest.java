060abff6a4e69348e075bce88469742a7c007865
xxeol2 <jhj01177@naver.com>
2023-09-29 02:18:25 +0900

    test: FestivalServiceTest에 ApplicationEventPublisher 주입

--- backend/src/test/java/com/festago/application/FestivalServiceTest.java
+++ backend/src/test/java/com/festago/application/FestivalServiceTest.java
@@ -35,6 +35,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -49,6 +50,9 @@ class FestivalServiceTest {
 
     @Mock
     SchoolRepository schoolRepository;
+
+    @Mock
+    ApplicationEventPublisher publisher;
 
     @InjectMocks
     FestivalService festivalService;
