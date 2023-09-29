2fcae3558f5c51ab2a9a309a7e7aeef5e01b6d49
SeongHoonC <108349655+SeongHoonC@users.noreply.github.com>
2023-09-29 22:47:13 +0900

    [AN/USER] feat: 학생 인증 API 연동(#456)  (#470)
    * feat: 학생 인증 저장소 API 연동
    
    * fix: Request 이름 변경
    
    * refactor: userName 으로 텍스트 가져오기 변경
    
    * refactor: 인증 번호 위치 수정
    
    * feat: 인증 화면 이벤트 처리
    
    * refactor: uiState 타입 분기 변경

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/studentverification/StudentVerificationActivity.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/studentverification/StudentVerificationActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.festago.festago.R
 import com.festago.festago.databinding.ActivityStudentVerificationBinding
+import com.festago.festago.presentation.ui.customview.OkDialogFragment
+import com.festago.festago.presentation.ui.home.HomeActivity
 import com.festago.festago.presentation.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalTime
@@ -42,7 +44,7 @@ class StudentVerificationActivity : AppCompatActivity() {
 
     private fun initRequestVerificationCodeBtn(schoolId: Long) {
         binding.btnRequestVerificationCode.setOnClickListener {
-            vm.sendVerificationCode(binding.tieVerificationCode.text.toString(), schoolId)
+            vm.sendVerificationCode(binding.tieUserName.text.toString(), schoolId)
         }
     }
 
@@ -62,8 +64,9 @@ class StudentVerificationActivity : AppCompatActivity() {
     private fun handleUiState(uiState: StudentVerificationUiState) {
         binding.uiState = uiState
         when (uiState) {
+            is StudentVerificationUiState.Loading -> Unit
             is StudentVerificationUiState.Success -> handleSuccess(uiState)
-            is StudentVerificationUiState.Loading, StudentVerificationUiState.Error -> Unit
+            is StudentVerificationUiState.Error -> Unit
         }
     }
 
@@ -81,15 +84,31 @@ class StudentVerificationActivity : AppCompatActivity() {
 
     private fun handleEvent(event: StudentVerificationEvent) {
         when (event) {
-            is StudentVerificationEvent.VerificationSuccess -> Unit
-            is StudentVerificationEvent.VerificationFailure -> Unit
-            is StudentVerificationEvent.CodeTimeOut -> Unit
+            is StudentVerificationEvent.VerificationSuccess -> handleVerificationSuccess()
+            is StudentVerificationEvent.VerificationFailure -> showDialog(FAILURE_VERIFICATION)
+            is StudentVerificationEvent.VerificationTimeOut -> showDialog(TIME_OUT_VERIFICATION)
+            is StudentVerificationEvent.SendingEmailFailure -> showDialog(FAILURE_SENDING_EMAIL)
         }
     }
 
-    companion object {
+    private fun handleVerificationSuccess() {
+        val intent = HomeActivity.getIntent(this).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        finishAffinity()
+        startActivity(intent)
+    }
 
+    private fun showDialog(message: String) {
+        val dialog = OkDialogFragment.newInstance(message)
+        dialog.show(supportFragmentManager, OkDialogFragment::class.java.name)
+    }
+
+    companion object {
         private const val KEY_SCHOOL_ID = "KEY_SCHOOL_ID"
+        private const val FAILURE_VERIFICATION = "인증에 실패하였습니다"
+        private const val TIME_OUT_VERIFICATION = "인증 시간이 만료되었습니다"
+        private const val FAILURE_SENDING_EMAIL = "이메일 입력을 확인해주세요"
 
         fun getIntent(context: Context, schoolId: Long): Intent {
             return Intent(context, StudentVerificationActivity::class.java).apply {
