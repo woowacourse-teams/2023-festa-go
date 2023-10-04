0a769d8a3e9d4ea84f95e5ac88e992421b2cf3ed
vrexpert <vrexpert2302@gmail.com>
2023-10-05 01:28:17 +0900

    refactor: 티켓 기록 화면 uiState StateFlow 로 리팩터링

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/tickethistory/TicketHistoryActivity.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/tickethistory/TicketHistoryActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.festago.festago.databinding.ActivityTicketHistoryBinding
+import com.festago.festago.presentation.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,17 +31,19 @@ class TicketHistoryActivity : AppCompatActivity() {
     }
 
     private fun initObserve() {
-        vm.uiState.observe(this) { uiState ->
-            when (uiState) {
-                is TicketHistoryUiState.Loading,
-                is TicketHistoryUiState.Error,
-                -> Unit
+        repeatOnStarted(this) {
+            vm.uiState.collect { uiState ->
+                when (uiState) {
+                    is TicketHistoryUiState.Loading,
+                    is TicketHistoryUiState.Error,
+                    -> Unit
 
-                is TicketHistoryUiState.Success -> {
-                    adapter.submitList(uiState.tickets)
+                    is TicketHistoryUiState.Success -> {
+                        adapter.submitList(uiState.tickets)
+                    }
                 }
+                binding.uiState = uiState
             }
-            binding.uiState = uiState
         }
     }
 
