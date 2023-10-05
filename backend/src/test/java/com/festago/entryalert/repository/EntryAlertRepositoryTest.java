26fed9fad563d7cb81a34aeef670393451f8aae6
xxeol2 <jhj01177@naver.com>
2023-10-05 11:15:26 +0900

    test: EntryAlertRepositoryTest 작성

--- backend/src/test/java/com/festago/entryalert/repository/EntryAlertRepositoryTest.java
+++ backend/src/test/java/com/festago/entryalert/repository/EntryAlertRepositoryTest.java
@@ -1,7 +1,15 @@
 package com.festago.entryalert.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.festago.entryalert.domain.AlertStatus;
+import com.festago.entryalert.domain.EntryAlert;
+import com.festago.support.EntryAlertFixture;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,5 +25,49 @@ class EntryAlertRepositoryTest {
     @Test
     void 상태로_전부_조회() {
         // given
+        EntryAlert entryAlert1 = entryAlertRepository.save(
+            EntryAlertFixture.entryAlert().status(AlertStatus.PENDING).build());
+        EntryAlert entryAlert2 = entryAlertRepository.save(
+            EntryAlertFixture.entryAlert().status(AlertStatus.PENDING).build());
+        EntryAlert entryAlert3 = entryAlertRepository.save(
+            EntryAlertFixture.entryAlert().status(AlertStatus.REQUESTED).build());
+
+        // when
+        List<EntryAlert> actual = entryAlertRepository.findAllByStatus(AlertStatus.PENDING);
+
+        // then
+        assertThat(actual).containsExactlyInAnyOrder(entryAlert1, entryAlert2);
+    }
+
+    @Nested
+    class id와_상태로_조회 {
+
+        @Test
+        void id와_상태로_조회() {
+            // given
+            EntryAlert entryAlert = entryAlertRepository.save(
+                EntryAlertFixture.entryAlert().status(AlertStatus.PENDING).build());
+
+            // when
+            Optional<EntryAlert> actual = entryAlertRepository.findByIdAndStatusForUpdate(
+                entryAlert.getId(), AlertStatus.PENDING);
+
+            // then
+            assertThat(actual.get()).isEqualTo(entryAlert);
+        }
+
+        @Test
+        void 상태가_다르면_조회되지_않음() {
+            // given
+            EntryAlert entryAlert = entryAlertRepository.save(
+                EntryAlertFixture.entryAlert().status(AlertStatus.REQUESTED).build());
+
+            // when
+            Optional<EntryAlert> actual = entryAlertRepository.findByIdAndStatusForUpdate(
+                entryAlert.getId(), AlertStatus.PENDING);
+
+            // then
+            assertThat(actual).isEmpty();
+        }
     }
 }
