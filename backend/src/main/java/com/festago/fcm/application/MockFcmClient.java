896502210dfed23586d00ccaad2cdddbc299c8cc
xxeol2 <jhj01177@naver.com>
2023-10-05 21:56:47 +0900

    fix: MockFcmClient Profile 수정

--- backend/src/main/java/com/festago/fcm/application/MockFcmClient.java
+++ backend/src/main/java/com/festago/fcm/application/MockFcmClient.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"!prod", "!dev"})
+@Profile({"!prod & !dev"})
 public class MockFcmClient implements FcmClient {
 
     private static final Logger log = LoggerFactory.getLogger(MockFcmClient.class);
