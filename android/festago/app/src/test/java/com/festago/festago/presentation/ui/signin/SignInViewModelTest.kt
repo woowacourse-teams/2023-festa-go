2a696e60269ace31a7521277272bd98e0a68be4f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-06 21:41:26 +0900

    fix: 테스트코드 빌드에러 수정

--- android/festago/app/src/test/java/com/festago/festago/presentation/ui/signin/SignInViewModelTest.kt
+++ android/festago/app/src/test/java/com/festago/festago/presentation/ui/signin/SignInViewModelTest.kt
@@ -3,13 +3,11 @@ package com.festago.festago.presentation.ui.signin
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.festago.festago.analytics.AnalyticsHelper
 import com.festago.festago.repository.AuthRepository
-import com.google.firebase.messaging.FirebaseMessaging
 import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -23,7 +21,6 @@ class SignInViewModelTest {
     private lateinit var vm: SignInViewModel
     private lateinit var authRepository: AuthRepository
     private lateinit var analyticsHelper: AnalyticsHelper
-    private lateinit var firebaseMessaging: FirebaseMessaging
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -36,7 +33,7 @@ class SignInViewModelTest {
         mockkStatic("kotlinx.coroutines.tasks.TasksKt")
 
         authRepository = mockk(relaxed = true)
-        firebaseMessaging = mockk(relaxed = true)
+        analyticsHelper = mockk(relaxed = true)
         vm = SignInViewModel(authRepository, analyticsHelper)
     }
 
@@ -49,8 +46,6 @@ class SignInViewModelTest {
     @Test
     fun `로그인 성공하면 성공 이벤트가 발생한다`() {
         // given
-        coEvery { firebaseMessaging.token.await() } returns "fakeFcmToken"
-
         coEvery { authRepository.signIn(any(), any()) } answers { Result.success(Unit) }
 
         // when
@@ -63,8 +58,6 @@ class SignInViewModelTest {
     @Test
     fun `로그인 실패하면 실패 이벤트가 발생한다`() {
         // given
-        coEvery { firebaseMessaging.token.await() } returns "fakeFcmToken"
-
         coEvery {
             authRepository.signIn(any(), any())
         } answers { Result.failure(Exception()) }
@@ -89,8 +82,6 @@ class SignInViewModelTest {
     @Test
     fun `FCM 토큰을 불러오지 못하면 실패 이벤트가 발생한다`() {
         // given
-        coEvery { firebaseMessaging.token } throws Exception()
-
         // when
         vm.signIn("testToken")
 
