ee461c829722189ca83286fe96049b34465b28fb
seokjin8678 <seokjin8678@gmail.com>
2023-10-06 16:53:44 +0900

    feat: Validator 검증 로직 추가

--- backend/src/main/java/com/festago/common/util/Validator.java
+++ backend/src/main/java/com/festago/common/util/Validator.java
@@ -1,5 +1,7 @@
 package com.festago.common.util;
 
+import com.festago.common.exception.ValidException;
+
 public final class Validator {
 
     private Validator() {
@@ -11,18 +13,19 @@ public final class Validator {
      * @param input     검증할 문자열
      * @param maxLength 검증할 문자열의 최대 길이
      * @param message   예외 메시지
-     * @throws IllegalArgumentException 문자열의 길이가 초과되거나, 최대 길이가 0 이하이면
+     * @throws IllegalArgumentException 최대 길이가 0 이하이면
+     * @throws ValidException           문자열의 길이가 초과되면
      */
     public static void maxLength(CharSequence input, int maxLength, String message) {
         if (maxLength <= 0) {
-            throw new IllegalArgumentException("검증 길이는 0보다 커야합니다.");
+            throw new IllegalArgumentException("최대 길이는 0보다 커야합니다.");
         }
         // avoid NPE
         if (input == null) {
             return;
         }
         if (input.length() > maxLength) {
-            throw new IllegalArgumentException(message);
+            throw new ValidException(message);
         }
     }
 
@@ -32,18 +35,104 @@ public final class Validator {
      * @param input     검증할 문자열
      * @param minLength 검증할 문자열의 최소 길이
      * @param message   예외 메시지
-     * @throws IllegalArgumentException 문자열의 길이가 작으면, 최대 길이가 0 이하이면
+     * @throws IllegalArgumentException 최소 길이가 0 이하이면
+     * @throws ValidException           문자열의 길이가 작으면
      */
     public static void minLength(CharSequence input, int minLength, String message) {
         if (minLength <= 0) {
-            throw new IllegalArgumentException("검증 길이는 0보다 커야합니다.");
+            throw new IllegalArgumentException("최소 길이는 0보다 커야합니다.");
         }
         // avoid NPE
         if (input == null) {
             return;
         }
         if (input.length() < minLength) {
-            throw new IllegalArgumentException(message);
+            throw new ValidException(message);
+        }
+    }
+
+    /**
+     * 객체가 null인지 검사합니다.
+     *
+     * @param object  검증할 객체
+     * @param message 예외 메시지
+     * @throws ValidException 객체가 null 이면
+     */
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new ValidException(message);
+        }
+    }
+
+    /**
+     * 값의 최대 값을 검증합니다. 최대 값이 0 이하이면 예외를 던집니다.
+     *
+     * @param value    검증할 값
+     * @param maxValue 검증할 값의 최대 값
+     * @param message  예외 메시지
+     * @throws ValidException           검증할 값이 최대 값보다 크면
+     * @throws IllegalArgumentException 최대 값이 0 이하이면
+     */
+    public static void maxValue(int value, int maxValue, String message) {
+        if (maxValue <= 0) {
+            throw new IllegalArgumentException("최대 값은 0보다 커야합니다.");
+        }
+        if (value > maxValue) {
+            throw new ValidException(message);
+        }
+    }
+
+    /**
+     * 값의 최대 값을 검증합니다. 최대 값이 0 이하이면 예외를 던집니다.
+     *
+     * @param value    검증할 값
+     * @param maxValue 검증할 값의 최대 값
+     * @param message  예외 메시지
+     * @throws ValidException           검증할 값이 최대 값보다 크면
+     * @throws IllegalArgumentException 최대 값이 0 이하이면
+     */
+    public static void maxValue(long value, int maxValue, String message) {
+        if (maxValue <= 0) {
+            throw new IllegalArgumentException("최대 값은 0보다 커야합니다.");
+        }
+        if (value > maxValue) {
+            throw new ValidException(message);
+        }
+    }
+
+    /**
+     * 값의 최소 값을 검증합니다. 최소 값이 0 이하이면 예외를 던집니다.
+     *
+     * @param value    검증할 값
+     * @param minValue 검증할 값의 최소 값
+     * @param message  예외 메시지
+     * @throws ValidException           검증할 값이 최소 값보다 작으면
+     * @throws IllegalArgumentException 최소 값이 0 이하이면
+     */
+    public static void minValue(int value, int minValue, String message) {
+        if (minValue <= 0) {
+            throw new IllegalArgumentException("최소 값은 0보다 커야합니다.");
+        }
+        if (value < minValue) {
+            throw new ValidException(message);
+        }
+    }
+
+    /**
+     * 값의 최소 값을 검증합니다. 최소 값이 0 이하이면 예외를 던집니다.
+     *
+     * @param value    검증할 값
+     * @param minValue 검증할 값의 최소 값
+     * @param message  예외 메시지
+     * @throws ValidException           검증할 값이 최소 값보다 작으면
+     * @throws IllegalArgumentException 최소 값이 0 이하이면
+     */
+    public static void minValue(long value, int minValue, String message) {
+        if (minValue <= 0) {
+            throw new IllegalArgumentException("최소 값은 0보다 커야합니다.");
+        }
+        if (value < minValue) {
+            throw new ValidException(message);
         }
     }
 }
--- backend/src/test/java/com/festago/common/util/ValidatorTest.java
+++ backend/src/test/java/com/festago/common/util/ValidatorTest.java
@@ -3,65 +3,200 @@ package com.festago.common.util;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.festago.common.exception.ValidException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class ValidatorTest {
 
-    @Test
-    void 문자열_maxLength_검증_성공() {
-        // given
-        String input = "1234567890"; // 10
+    @Nested
+    class StringValid {
 
-        // when & then
-        assertThatNoException()
-            .isThrownBy(() -> Validator.maxLength(input, 10, ""));
+        @Test
+        void 문자열의_길이가_10이고_최대_길이가_10이면_통과() {
+            // given
+            String input = "1234567890"; // 10
+            int maxLength = 10;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.maxLength(input, maxLength, ""));
+        }
+
+        @Test
+        void 문자열의_길이가_11이고_최대_길이가_10이면_예외() {
+            // given
+            String input = "12345678901"; // 11
+            int maxLength = 10;
+
+            // when & then
+            assertThatThrownBy(() -> Validator.maxLength(input, maxLength, ""))
+                .isInstanceOf(ValidException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, -1})
+        void 최대_길이가_0이하면_IllegalArgumentException(int maxLength) {
+            // given
+            String input = "1234567890"; // 10
+
+            // when & then
+            assertThatThrownBy(() -> Validator.maxLength(input, maxLength, ""))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @NullSource
+        void 문자열이_null이고_최대_길이가_10이면_통과(String input) {
+            // given
+            int maxLength = 10;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.maxLength(input, maxLength, ""));
+        }
+
+        @Test
+        void 문자열의_길이가_10이고_최소_길이가_10이면_통과() {
+            // given
+            String input = "1234567890"; // 10
+            int minLength = 10;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.minLength(input, minLength, ""));
+        }
+
+        @Test
+        void 문자열의_길이가_9이고_최소_길이가_10이면_예외() {
+            // given
+            String input = "123456789"; // 9
+            int minLength = 10;
+
+            // when & then
+            assertThatThrownBy(() -> Validator.minLength(input, minLength, ""))
+                .isInstanceOf(ValidException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, -1})
+        void 최소_길이가_0이하면_IllegalArgumentException(int minLength) {
+            // given
+            String input = "1234567890"; // 10
+
+            // when & then
+            assertThatThrownBy(() -> Validator.maxLength(input, minLength, ""))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @NullSource
+        void 문자열이_null이고_최소_길이가_10이면_통과(String input) {
+            // given
+            int minLength = 10;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.minLength(input, minLength, ""));
+        }
     }
 
-    @Test
-    void 문자열_maxLength_검증_실패() {
-        // given
-        String input = "12345678901"; // 11
+    @Nested
+    class NullValid {
 
-        // when & then
-        assertThatThrownBy(() -> Validator.maxLength(input, 10, ""))
-            .isInstanceOf(IllegalArgumentException.class);
+        @ParameterizedTest
+        @NullSource
+        void 객체가_null이면_예외(Object object) {
+            // when & then
+            assertThatThrownBy(() -> Validator.notNull(object, ""))
+                .isInstanceOf(ValidException.class);
+        }
+
+        @Test
+        void 객체가_null이_아니면_통과() {
+            // given
+            Object object = "";
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.notNull(object, ""));
+        }
     }
 
-    @Test
-    void 문자열_maxLength_검증_null_성공() {
-        // when & then
-        assertThatNoException()
-            .isThrownBy(() -> Validator.maxLength(null, 10, ""));
-    }
+    @Nested
+    class NumberValid {
 
-    @Test
-    void 문자열_minLength_검증_성공() {
-        // given
-        String input = "1234567890"; // 10
+        @Test
+        void 값이_100이고_최대_값이_100이면_통과() {
+            // given
+            int value = 100;
+            int maxValue = 100;
 
-        // when & then
-        assertThatNoException()
-            .isThrownBy(() -> Validator.minLength(input, 10, ""));
-    }
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.maxValue(value, maxValue, ""));
+        }
 
-    @Test
-    void 문자열_minLength_검증_실패() {
-        // given
-        String input = "123456789"; // 9
+        @Test
+        void 값이_101이고_최대_값이_100이면_예외() {
+            // given
+            int value = 101;
+            int maxValue = 100;
 
-        // when & then
-        assertThatThrownBy(() -> Validator.minLength(input, 10, ""))
-            .isInstanceOf(IllegalArgumentException.class);
-    }
+            // when & then
+            assertThatThrownBy(() -> Validator.maxValue(value, maxValue, ""))
+                .isInstanceOf(ValidException.class);
+        }
 
-    @Test
-    void 문자열_minLength_검증_null_성공() {
-        // when & then
-        assertThatNoException()
-            .isThrownBy(() -> Validator.minLength(null, 1, ""));
+        @ParameterizedTest
+        @ValueSource(ints = {0, -1})
+        void 최대_값이_0이하면_IllegalArgumentException(int maxValue) {
+            // given
+            int value = 100;
+
+            // when & then
+            assertThatThrownBy(() -> Validator.maxValue(value, maxValue, ""))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 값이_100이고_최소_값이_100이면_통과() {
+            // given
+            int value = 100;
+            int minValue = 100;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.maxValue(value, minValue, ""));
+        }
+
+        @Test
+        void 값이_99이고_최소_값이_100이면_예외() {
+            // given
+            int value = 99;
+            int minValue = 100;
+
+            // when & then
+            assertThatNoException()
+                .isThrownBy(() -> Validator.maxValue(value, minValue, ""));
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, -1})
+        void 최소_값이_0이하면_IllegalArgumentException(int minValue) {
+            // given
+            int value = 100;
+
+            // when & then
+            assertThatThrownBy(() -> Validator.maxValue(value, minValue, ""))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
