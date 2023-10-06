fe85d27341ee74be9689868b350b325f12d0f9a2
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-06 20:15:00 +0900

    refactor: FestivalDefault 내부 함수에서 터지더라도 Catch 할 수 있도록 변경

--- android/festago/app/src/main/java/com/festago/festago/data/repository/FestivalDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/FestivalDefaultRepository.kt
@@ -1,7 +1,8 @@
 package com.festago.festago.data.repository
 
 import com.festago.festago.data.service.FestivalRetrofitService
-import com.festago.festago.data.util.runCatchingWithErrorHandler
+import com.festago.festago.data.util.runRetrofitWithErrorHandler
+import com.festago.festago.data.util.tryConvert
 import com.festago.festago.model.Festival
 import com.festago.festago.model.Reservation
 import com.festago.festago.repository.FestivalRepository
@@ -10,15 +11,11 @@ import javax.inject.Inject
 class FestivalDefaultRepository @Inject constructor(
     private val festivalRetrofitService: FestivalRetrofitService,
 ) : FestivalRepository {
-    override suspend fun loadFestivals(): Result<List<Festival>> {
-        festivalRetrofitService.getFestivals().runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadFestivals(): Result<List<Festival>> =
+        runRetrofitWithErrorHandler { festivalRetrofitService.getFestivals() }
+            .tryConvert { it.toDomain() }
 
-    override suspend fun loadFestivalDetail(festivalId: Long): Result<Reservation> {
-        festivalRetrofitService.getFestivalDetail(festivalId).runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadFestivalDetail(festivalId: Long): Result<Reservation> =
+        runRetrofitWithErrorHandler { festivalRetrofitService.getFestivalDetail(festivalId) }
+            .tryConvert { it.toDomain() }
 }
