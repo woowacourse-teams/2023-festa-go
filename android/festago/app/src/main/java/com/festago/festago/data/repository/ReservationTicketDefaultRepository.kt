49a25351e68b298ac0bf14e974656b9c472d001f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-06 20:45:31 +0900

    refactor: 예약 리포지터리 변경된 함수로 값을 받아오도록 변경

--- android/festago/app/src/main/java/com/festago/festago/data/repository/ReservationTicketDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/ReservationTicketDefaultRepository.kt
@@ -1,7 +1,8 @@
 package com.festago.festago.data.repository
 
 import com.festago.festago.data.service.ReservationTicketRetrofitService
-import com.festago.festago.data.util.runCatchingWithErrorHandler
+import com.festago.festago.data.util.onSuccessOrCatch
+import com.festago.festago.data.util.runRetrofitWithErrorHandler
 import com.festago.festago.model.ReservationTicket
 import com.festago.festago.repository.ReservationTicketRepository
 import javax.inject.Inject
@@ -10,10 +11,7 @@ class ReservationTicketDefaultRepository @Inject constructor(
     private val reservationTicketRetrofitService: ReservationTicketRetrofitService,
 ) : ReservationTicketRepository {
 
-    override suspend fun loadTicketTypes(stageId: Int): Result<List<ReservationTicket>> {
-        reservationTicketRetrofitService.getReservationTickets(stageId)
-            .runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadTicketTypes(stageId: Int): Result<List<ReservationTicket>> =
+        runRetrofitWithErrorHandler { reservationTicketRetrofitService.getReservationTickets(stageId) }
+            .onSuccessOrCatch { it.toDomain() }
 }
