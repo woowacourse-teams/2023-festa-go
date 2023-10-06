64cd9f4ceede6299eb4ad0339cb488efecdc53c4
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-06 20:57:35 +0900

    refactor: 사용하지 않는 확장함수 제거

--- android/festago/app/src/main/java/com/festago/festago/data/util/ResponseExt.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/util/ResponseExt.kt
@@ -2,26 +2,6 @@ package com.festago.festago.data.util
 
 import retrofit2.Response
 
-fun <T> Response<T>.runCatchingWithErrorHandler(): Result<T> {
-    try {
-        if (this.isSuccessful && this.body() != null) {
-            return Result.success(this.body()!!)
-        }
-
-        return Result.failure(
-            Throwable(
-                "{" +
-                        "code: ${this.code()}," +
-                        "message: ${this.message()}, " +
-                        "body: ${this.errorBody()?.string()}" +
-                        "}",
-            ),
-        )
-    } catch (e: Exception) {
-        return Result.failure(e)
-    }
-}
-
 suspend fun <T> runCatchingResponse(
     block: suspend () -> Response<T>,
 ): Result<T> {
