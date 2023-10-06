f5937759362fa6cbb8b01d1ba19874e11b25cecf
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-06 20:43:04 +0900

    refactor: 티켓 리포지터리 변경된 함수로 값을 받아오도록 변경

--- android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/TicketDefaultRepository.kt
@@ -2,7 +2,8 @@ package com.festago.festago.data.repository
 
 import com.festago.festago.data.dto.ReservedTicketRequest
 import com.festago.festago.data.service.TicketRetrofitService
-import com.festago.festago.data.util.runCatchingWithErrorHandler
+import com.festago.festago.data.util.onSuccessOrCatch
+import com.festago.festago.data.util.runRetrofitWithErrorHandler
 import com.festago.festago.model.ReservedTicket
 import com.festago.festago.model.Ticket
 import com.festago.festago.model.TicketCode
@@ -13,34 +14,26 @@ class TicketDefaultRepository @Inject constructor(
     private val ticketRetrofitService: TicketRetrofitService,
 ) : TicketRepository {
 
-    override suspend fun loadTicket(ticketId: Long): Result<Ticket> {
-        ticketRetrofitService.getTicket(ticketId).runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadTicket(ticketId: Long): Result<Ticket> =
+        runRetrofitWithErrorHandler { ticketRetrofitService.getTicket(ticketId) }
+            .onSuccessOrCatch { it.toDomain() }
 
-    override suspend fun loadCurrentTickets(): Result<List<Ticket>> {
-        ticketRetrofitService.getCurrentTickets().runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadCurrentTickets(): Result<List<Ticket>> =
+        runRetrofitWithErrorHandler { ticketRetrofitService.getCurrentTickets() }
+            .onSuccessOrCatch { it.toDomain() }
 
-    override suspend fun loadTicketCode(ticketId: Long): Result<TicketCode> {
-        ticketRetrofitService.getTicketCode(ticketId).runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadTicketCode(ticketId: Long): Result<TicketCode> =
+        runRetrofitWithErrorHandler { ticketRetrofitService.getTicketCode(ticketId) }
+            .onSuccessOrCatch { it.toDomain() }
 
-    override suspend fun loadHistoryTickets(size: Int): Result<List<Ticket>> {
-        ticketRetrofitService.getHistoryTickets(size).runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun loadHistoryTickets(size: Int): Result<List<Ticket>> =
+        runRetrofitWithErrorHandler { ticketRetrofitService.getHistoryTickets(size) }
+            .onSuccessOrCatch { it.toDomain() }
 
-    override suspend fun reserveTicket(ticketId: Int): Result<ReservedTicket> {
-        ticketRetrofitService.postReserveTicket(ReservedTicketRequest(ticketId))
-            .runCatchingWithErrorHandler()
-            .getOrElse { error -> return Result.failure(error) }
-            .let { return Result.success(it.toDomain()) }
-    }
+    override suspend fun reserveTicket(ticketId: Int): Result<ReservedTicket> =
+        runRetrofitWithErrorHandler {
+            ticketRetrofitService.postReserveTicket(
+                ReservedTicketRequest(ticketId),
+            )
+        }.onSuccessOrCatch { it.toDomain() }
 }
