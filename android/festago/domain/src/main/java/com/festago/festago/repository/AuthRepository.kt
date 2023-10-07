2a0a5d003ba56a2691c443dbd8aa769dc947dc5f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-10-07 19:54:36 +0900

    refactor: 반환에 Unit? 제거

--- android/festago/domain/src/main/java/com/festago/festago/repository/AuthRepository.kt
+++ android/festago/domain/src/main/java/com/festago/festago/repository/AuthRepository.kt
@@ -2,7 +2,7 @@ package com.festago.festago.repository
 
 interface AuthRepository {
     val isSigned: Boolean
-    suspend fun signIn(): Result<Unit?>
+    suspend fun signIn(): Result<Unit>
     suspend fun signOut(): Result<Unit>
     suspend fun deleteAccount(): Result<Unit>
 }
