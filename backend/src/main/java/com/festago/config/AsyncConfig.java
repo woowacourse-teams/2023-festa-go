e2b1eaaef18e68a158ce7fdb3ee4a7d9f7e64dd6
xxeol2 <jhj01177@naver.com>
2023-10-08 15:47:42 +0900

    refactor: graceful shutdown 설정

--- backend/src/main/java/com/festago/config/AsyncConfig.java
+++ backend/src/main/java/com/festago/config/AsyncConfig.java
@@ -1,10 +1,22 @@
 package com.festago.config;
 
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
 
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
 }
