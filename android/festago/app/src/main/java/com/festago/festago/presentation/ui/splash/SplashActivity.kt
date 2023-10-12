ba4d3afee7e02281263d17a41d178efa204dca25
vrexpert <vrexpert2302@gmail.com>
2023-10-12 14:15:49 +0900

    refactor: splash 코드 리팩터링

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/splash/SplashActivity.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/splash/SplashActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.festago.festago.R
 import com.festago.festago.databinding.ActivitySplashBinding
 import com.festago.festago.presentation.ui.home.HomeActivity
+import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,18 +35,26 @@ class SplashActivity : ComponentActivity() {
 
     private fun checkAppUpdate(splashScreen: SplashScreen) {
         val appUpdateManager = AppUpdateManagerFactory.create(this)
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
-                splashScreen.setKeepOnScreenCondition { false }
-                requestUpdate()
-            } else {
-                startActivity(HomeActivity.getIntent(this))
-                finish()
+        appUpdateManager.appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                handleOnSuccess(appUpdateInfo, splashScreen)
+            }.addOnFailureListener {
+                showHome()
             }
-        }.addOnFailureListener {
-            startActivity(HomeActivity.getIntent(this))
-            finish()
+    }
+
+    private fun handleOnSuccess(appUpdateInfo: AppUpdateInfo, splashScreen: SplashScreen) {
+        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+            splashScreen.setKeepOnScreenCondition { false }
+            requestUpdate()
+        } else {
+            showHome()
         }
+    }
+
+    private fun showHome() {
+        startActivity(HomeActivity.getIntent(this))
+        finish()
     }
 
     private fun requestUpdate() {
@@ -62,17 +71,17 @@ class SplashActivity : ComponentActivity() {
         }.show()
     }
 
-    private fun handleOkUpdate() {
-        navigateToAppStore()
-        finish()
-    }
-
     private fun handleCancelUpdate() {
         Toast.makeText(
             this@SplashActivity,
             getString(R.string.splash_app_update_denied),
             Toast.LENGTH_SHORT,
         ).show()
+        finish()
+    }
+
+    private fun handleOkUpdate() {
+        navigateToAppStore()
         finish()
     }
 
