4ca5d968131d55cf91f8127f9427baec4cb756d1
EmilyCh0 <emily9803@ewhain.net>
2023-10-12 09:49:51 +0900

    refactor: application에서 채널 생성

--- android/festago/app/src/main/java/com/festago/festago/presentation/service/NotificationManager.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/service/NotificationManager.kt
@@ -1,17 +1,16 @@
 package com.festago.festago.presentation.service
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.festago.festago.R
-import com.festago.festago.presentation.ui.home.HomeActivity
 import com.festago.festago.presentation.service.FcmMessageType.ENTRY_ALERT
+import com.festago.festago.presentation.ui.home.HomeActivity
 
 
-class NotificationManager(context: Context) {
+class NotificationManager(private val context: Context) {
 
     private val notificationManager by lazy {
         context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -34,19 +33,11 @@ class NotificationManager(context: Context) {
             .setAutoCancel(true)
             .setContentIntent(pendingIntent)
 
-    init {
-        val channel = NotificationChannel(
-            ENTRY_ALERT.channelId,
-            context.getString(R.string.entry_alert_channel_name),
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
-        notificationManager.createNotificationChannel(channel)
-    }
-
     fun sendEntryAlertNotification(title: String, body: String) {
         entryAlertNotificationBuilder
             .setContentTitle(title)
             .setContentText(body)
+
         notificationManager.notify(ENTRY_ALERT.id, entryAlertNotificationBuilder.build())
     }
 }
