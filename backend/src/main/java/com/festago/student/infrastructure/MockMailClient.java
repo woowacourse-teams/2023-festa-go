3e1cc2ada81cbee6a7097323cb6dfad476ea5341
seokjin8678 <seokjin8678@gmail.com>
2023-10-13 17:17:52 +0900

    fix: dev, prod 프로파일이 아니면 등록되게 변경

--- backend/src/main/java/com/festago/student/infrastructure/MockMailClient.java
+++ backend/src/main/java/com/festago/student/infrastructure/MockMailClient.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"local", "test"})
+@Profile("!{dev|prod}")
 public class MockMailClient implements MailClient {
 
     @Override
     public void send(VerificationMailPayload payload) {
+        // no-op
     }
 }
