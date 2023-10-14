658bfe82fa13a8e39b9342eea5e07f8839d49400
vrexpert <vrexpert2302@gmail.com>
2023-10-14 17:12:38 +0900

    refactor: 홈 화면 회전시 축제리스트로 되돌아가는 문제 해결

--- android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/HomeViewModel.kt
+++ android/festago/app/src/main/java/com/festago/festago/presentation/ui/home/HomeViewModel.kt
@@ -2,14 +2,14 @@ package com.festago.festago.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.festago.festago.presentation.ui.home.HomeItemType.FESTIVAL_LIST
-import com.festago.festago.presentation.ui.home.HomeItemType.MY_PAGE
-import com.festago.festago.presentation.ui.home.HomeItemType.TICKET_LIST
 import com.festago.festago.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,13 +19,23 @@ class HomeViewModel @Inject constructor(private val authRepository: AuthReposito
     private val _event = MutableSharedFlow<HomeEvent>()
     val event: SharedFlow<HomeEvent> = _event.asSharedFlow()
 
-    fun loadHomeItem(homeItemType: HomeItemType) {
+    private val _selectedItem = MutableStateFlow(HomeItemType.FESTIVAL_LIST)
+    val selectedItem: StateFlow<HomeItemType> = _selectedItem.asStateFlow()
+
+    fun selectItem(homeItemType: HomeItemType) {
+        when (homeItemType) {
+            HomeItemType.FESTIVAL_LIST -> _selectedItem.value = homeItemType
+            HomeItemType.TICKET_LIST -> selectItemWhenSignIn(HomeItemType.TICKET_LIST)
+            HomeItemType.MY_PAGE -> selectItemWhenSignIn(HomeItemType.MY_PAGE)
+        }
+    }
+
+    private fun selectItemWhenSignIn(homeItemType: HomeItemType) {
         viewModelScope.launch {
-            when {
-                homeItemType == FESTIVAL_LIST -> _event.emit(HomeEvent.ShowFestivalList)
-                !authRepository.isSigned -> _event.emit(HomeEvent.ShowSignIn)
-                homeItemType == TICKET_LIST -> _event.emit(HomeEvent.ShowTicketList)
-                homeItemType == MY_PAGE -> _event.emit(HomeEvent.ShowMyPage)
+            if (!authRepository.isSigned) {
+                _event.emit(HomeEvent.ShowSignIn)
+            } else {
+                _selectedItem.emit(homeItemType)
             }
         }
     }
