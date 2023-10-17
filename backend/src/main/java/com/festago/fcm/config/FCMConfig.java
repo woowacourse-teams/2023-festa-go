54b3abe493566c0267a995cecb6a198149e33d51
seokjin8678 <seokjin8678@gmail.com>
2023-10-17 10:15:05 +0900

    fix: 프로파일 빈 등록 설정 수정 (#553)

--- backend/src/main/java/com/festago/fcm/config/FCMConfig.java
+++ backend/src/main/java/com/festago/fcm/config/FCMConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@Profile({"prod", "dev"})
+@Profile("prod | dev")
 public class FCMConfig {
 
     @Value("${fcm.key.path}")
