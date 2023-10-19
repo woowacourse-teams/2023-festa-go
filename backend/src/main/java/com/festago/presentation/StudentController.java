b3987db4828913adbab72d8cbbde05081d55ddfd
xxeol2 <jhj01177@naver.com>
2023-10-19 11:59:20 +0900

    [BE] feat: 회원의 학생 인증 정보 조회 API 구현 (#496) (#531)
    * refactor: 메서드 네이밍 수정 (verifiacte -> verify)
    
    * feat: 학생 인증 정보 조회 기능 구현
    
    * refactor: findByMemberIdWithFetch INNER JOIN으로 수정
    
    * refactor: inner line return 으로 수정
    
    * refactor: 캐싱된 Response 반환

--- backend/src/main/java/com/festago/presentation/StudentController.java
+++ backend/src/main/java/com/festago/presentation/StudentController.java
@@ -2,6 +2,7 @@ package com.festago.presentation;
 
 import com.festago.auth.annotation.Member;
 import com.festago.student.application.StudentService;
+import com.festago.student.dto.StudentResponse;
 import com.festago.student.dto.StudentSendMailRequest;
 import com.festago.student.dto.StudentVerificateRequest;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,10 +35,18 @@ public class StudentController {
 
     @PostMapping("/verification")
     @Operation(description = "학교 인증을 수행한다.", summary = "학생 인증 수행")
-    public ResponseEntity<Void> verificate(@Member Long memberId,
-                                           @RequestBody @Valid StudentVerificateRequest request) {
-        studentService.verificate(memberId, request);
+    public ResponseEntity<Void> verify(@Member Long memberId,
+                                       @RequestBody @Valid StudentVerificateRequest request) {
+        studentService.verify(memberId, request);
         return ResponseEntity.ok()
             .build();
+    }
+
+    @GetMapping
+    @Operation(description = "학생 인증 정보를 조회한다.", summary = "학생 인증 정보 조회")
+    public ResponseEntity<StudentResponse> findVerification(@Member Long memberId) {
+        StudentResponse response = studentService.findVerification(memberId);
+        return ResponseEntity.ok()
+            .body(response);
     }
 }
--- backend/src/test/java/com/festago/presentation/StudentControllerTest.java
+++ backend/src/test/java/com/festago/presentation/StudentControllerTest.java
@@ -1,14 +1,22 @@
 package com.festago.presentation;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.festago.student.application.StudentService;
+import com.festago.student.dto.StudentResponse;
+import com.festago.student.dto.StudentSchoolResponse;
 import com.festago.student.dto.StudentSendMailRequest;
 import com.festago.student.dto.StudentVerificateRequest;
 import com.festago.support.CustomWebMvcTest;
 import com.festago.support.WithMockAuth;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
@@ -91,6 +99,44 @@ class StudentControllerTest {
                     .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                     .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class 학생_인증_정보_조회 {
+
+        @Test
+        void 인증이_되지_않으면_401() throws Exception {
+            // given
+            StudentVerificateRequest request = new StudentVerificateRequest("123456");
+
+            // when & then
+            mockMvc.perform(get("/students")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized());
+        }
+
+        @Test
+        @WithMockAuth
+        void 학생_인증_조회() throws Exception {
+            // given
+            StudentResponse expected = new StudentResponse(true, new StudentSchoolResponse(1L, "테코대학교", "teco.ac.kr"));
+            given(studentService.findVerification(anyLong()))
+                .willReturn(expected);
+
+            // when & then
+            String content = mockMvc.perform(get("/students")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+            StudentResponse actual = objectMapper.readValue(content, StudentResponse.class);
+            assertThat(actual).isEqualTo(expected);
         }
     }
 }
