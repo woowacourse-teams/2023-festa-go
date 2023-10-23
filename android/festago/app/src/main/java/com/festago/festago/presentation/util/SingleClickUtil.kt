09f986a7757a6353c37e8ed1a5dac10099cb4f59
vrexpert <vrexpert2302@gmail.com>
2023-10-23 14:43:57 +0900

    feat: SingleClick Util 추가

---
+++ android/festago/app/src/main/java/com/festago/festago/presentation/util/SingleClickUtil.kt
@@ -0,0 +1,30 @@
+package com.festago.festago.presentation.util
+
+import android.os.SystemClock
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+class OnSingleClickListener(
+    private var interval: Int = 600,
+    private var onSingleClick: (View) -> Unit,
+) : View.OnClickListener {
+
+    private var lastClickTime: Long = 0
+
+    override fun onClick(v: View) {
+        val elapsedRealtime = SystemClock.elapsedRealtime()
+        if ((elapsedRealtime - lastClickTime) < interval) {
+            return
+        }
+        lastClickTime = elapsedRealtime
+        onSingleClick(v)
+    }
+}
+
+@BindingAdapter("onSingleClick")
+fun View.onSingleClick(onSingleClick: (View) -> Unit) {
+    val oneClick = OnSingleClickListener {
+        onSingleClick(it)
+    }
+    setOnClickListener(oneClick)
+}
