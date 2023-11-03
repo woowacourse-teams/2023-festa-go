c2445c95f6fcc46fb5ade4272d04bdb4281f253a
hyunseo <>
2023-11-03 14:45:56 +0900

    feat: SchoolController에 Swagger 정보 추가

--- backend/src/main/java/com/festago/presentation/SchoolController.java
+++ backend/src/main/java/com/festago/presentation/SchoolController.java
@@ -3,6 +3,8 @@ package com.festago.presentation;
 import com.festago.school.application.SchoolService;
 import com.festago.school.dto.SchoolResponse;
 import com.festago.school.dto.SchoolsResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/schools")
+@Tag(name = "학교 정보 요청")
 @RequiredArgsConstructor
 public class SchoolController {
 
     private final SchoolService schoolService;
 
     @GetMapping
+    @Operation(description = "모든 학교 정보를 조회한다.", summary = "전체 학교 조회")
     public ResponseEntity<SchoolsResponse> findAll() {
         return ResponseEntity.ok()
             .body(schoolService.findAll());
     }
 
     @GetMapping("/{schoolId}")
+    @Operation(description = "단일 학교 정보를 조회한다.", summary = "단일 학교 조회")
     public ResponseEntity<SchoolResponse> findById(@PathVariable Long schoolId) {
         return ResponseEntity.ok()
             .body(schoolService.findById(schoolId));
