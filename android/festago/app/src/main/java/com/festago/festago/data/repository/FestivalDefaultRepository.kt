85131dce1cc11fbe8fed58e83628b6aee8737761
vrexpert <vrexpert2302@gmail.com>
2023-11-12 17:24:07 +0900

    feat: FestivalRepository 필터링 요청 가능하도록 재정의

--- android/festago/app/src/main/java/com/festago/festago/data/repository/FestivalDefaultRepository.kt
+++ android/festago/app/src/main/java/com/festago/festago/data/repository/FestivalDefaultRepository.kt
@@ -4,6 +4,7 @@ import com.festago.festago.data.service.FestivalRetrofitService
 import com.festago.festago.data.util.onSuccessOrCatch
 import com.festago.festago.data.util.runCatchingResponse
 import com.festago.festago.model.Festival
+import com.festago.festago.model.FestivalFilter
 import com.festago.festago.model.Reservation
 import com.festago.festago.repository.FestivalRepository
 import javax.inject.Inject
@@ -11,9 +12,11 @@ import javax.inject.Inject
 class FestivalDefaultRepository @Inject constructor(
     private val festivalRetrofitService: FestivalRetrofitService,
 ) : FestivalRepository {
-    override suspend fun loadFestivals(): Result<List<Festival>> =
-        runCatchingResponse { festivalRetrofitService.getFestivals() }
-            .onSuccessOrCatch { it.toDomain() }
+    override suspend fun loadFestivals(festivalFilter: FestivalFilter): Result<List<Festival>> =
+        runCatchingResponse {
+            // TODO: 필터링 API 연동 작업 필요
+            festivalRetrofitService.getFestivals()
+        }.onSuccessOrCatch { it.toDomain() }
 
     override suspend fun loadFestivalDetail(festivalId: Long): Result<Reservation> =
         runCatchingResponse { festivalRetrofitService.getFestivalDetail(festivalId) }
