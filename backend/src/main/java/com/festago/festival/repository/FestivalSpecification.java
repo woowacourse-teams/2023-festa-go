9bb33fc0d9f698774d0437f0cbecbfc047ebc950
BGuga <tmdwns9913@gmail.com>
2023-11-13 16:49:40 +0900

    feat: 축제 진행 상황별 정렬 조건 추가

--- backend/src/main/java/com/festago/festival/repository/FestivalSpecification.java
+++ backend/src/main/java/com/festago/festival/repository/FestivalSpecification.java
@@ -13,18 +13,26 @@ public class FestivalSpecification {
     private static final String END_DATE = "endDate";
 
     public static Specification<Festival> progress(LocalDate currentTime) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.and(
-            criteriaBuilder.lessThanOrEqualTo(root.get(START_DATE), currentTime),
-            criteriaBuilder.greaterThanOrEqualTo(root.get(END_DATE), currentTime)
-        );
+        return (root, query, criteriaBuilder) -> {
+            query.orderBy(criteriaBuilder.asc(root.get(START_DATE)));
+            return criteriaBuilder.and(
+                criteriaBuilder.lessThanOrEqualTo(root.get(START_DATE), currentTime),
+                criteriaBuilder.greaterThanOrEqualTo(root.get(END_DATE), currentTime)
+            );
+        };
     }
 
     public static Specification<Festival> planned(LocalDate currentTime) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get(START_DATE), currentTime);
+        return (root, query, criteriaBuilder) -> {
+            query.orderBy(criteriaBuilder.asc(root.get(START_DATE)));
+            return criteriaBuilder.greaterThan(root.get(START_DATE), currentTime);
+        };
     }
 
     public static Specification<Festival> end(LocalDate currentTime) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get(END_DATE), currentTime);
+        return (root, query, criteriaBuilder) -> {
+            query.orderBy(criteriaBuilder.desc(root.get(END_DATE)));
+            return criteriaBuilder.lessThan(root.get(END_DATE), currentTime);
+        };
     }
-
 }
--- backend/src/test/java/com/festago/festival/repository/FestivalRepositoryTest.java
+++ backend/src/test/java/com/festago/festival/repository/FestivalRepositoryTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -32,56 +33,153 @@ class FestivalRepositoryTest {
 
     LocalDate now = LocalDate.parse("2023-06-30");
 
+    School school;
+
     @BeforeEach
     public void setting() {
-        School school = schoolRepository.save(new School("domain", "name"));
-        festivalRepository.save(new Festival(PAST_FESTIVAL, now.minusDays(3L), now.minusDays(1L), school));
-        festivalRepository.save(new Festival(CURRENT_FESTIVAL, now, now, school));
-        festivalRepository.save(new Festival(FUTURE_FESTIVAL, now.plusDays(1L), now.plusDays(3L), school));
+        this.school = schoolRepository.save(new School("domain", "name"));
     }
 
     @Test
-    void 진행_예정_축제_반환() {
-        // given
-        FestivalFilter filter = FestivalFilter.PLANNED;
-
-        // when
-        List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
-
-        // then
-        assertSoftly(softAssertions -> {
-            assertThat(actual).hasSize(1);
-            assertThat(actual.get(0)).matches(festival -> festival.getName().equals(FUTURE_FESTIVAL));
-        });
-    }
-
-    @Test
-    void 진행_중_축제_반환() {
-        // given
-        FestivalFilter filter = FestivalFilter.PROGRESS;
-
-        // when
-        List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
-
-        // then
-        assertSoftly(softAssertions -> {
-            assertThat(actual).hasSize(1);
-            assertThat(actual.get(0)).matches(festival -> festival.getName().equals(CURRENT_FESTIVAL));
-        });
-    }
-
-    @Test
-    void 종료_축제_반환() {
+    void 종료_축제는_종료_시점이_최근인_순서로_반환된다() {
         // given
         FestivalFilter filter = FestivalFilter.END;
+        Festival festival1 = festivalRepository.save(
+            new Festival(PAST_FESTIVAL, now.minusDays(3L), now.minusDays(1L), school));
+        Festival festival2 = festivalRepository.save(
+            new Festival(CURRENT_FESTIVAL, now, now, school));
+        Festival festival3 = festivalRepository.save(
+            new Festival(FUTURE_FESTIVAL, now.plusDays(1L), now.plusDays(3L), school));
+
+        LocalDate currentTime = now.plusDays(4L);
 
         // when
-        List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
+        List<Festival> actual = festivalRepository.findAll(filter.getSpecification(currentTime));
 
         // then
-        assertSoftly(softAssertions -> {
-            assertThat(actual).hasSize(1);
-            assertThat(actual.get(0)).matches(festival -> festival.getName().equals(PAST_FESTIVAL));
-        });
+        assertThat(actual).isEqualTo(List.of(festival3, festival2, festival1));
+    }
+
+    private void prepareNotOrderedFestivals() {
+        festivalRepository.save(new Festival(FUTURE_FESTIVAL, now.plusDays(1L), now.plusDays(3L), school));
+        festivalRepository.save(new Festival(CURRENT_FESTIVAL, now, now, school));
+        festivalRepository.save(new Festival(PAST_FESTIVAL, now.minusDays(3L), now.minusDays(1L), school));
+    }
+
+    @Nested
+    class 진행_에정_축제_반환 {
+
+        @Test
+        void 성공() {
+            // given
+            FestivalFilter filter = FestivalFilter.PLANNED;
+            prepareNotOrderedFestivals();
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
+
+            // then
+            assertSoftly(softAssertions -> {
+                assertThat(actual).hasSize(1);
+                assertThat(actual.get(0)).matches(festival -> festival.getName().equals(FUTURE_FESTIVAL));
+            });
+        }
+
+        @Test
+        void 은_시작_시점이_빠른_순서로_반환된다() {
+            // given
+            FestivalFilter filter = FestivalFilter.PLANNED;
+            LocalDate currentTime = LocalDate.parse("2023-10-01");
+            Festival festival2 = festivalRepository.save(
+                new Festival("festival2", currentTime.plusDays(2), currentTime.plusDays(10), school));
+            Festival festival3 = festivalRepository.save(
+                new Festival("festival3", currentTime.plusDays(3), currentTime.plusDays(10), school));
+            Festival festival1 = festivalRepository.save(
+                new Festival("festival1", currentTime.plusDays(1), currentTime.plusDays(10), school));
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(currentTime));
+
+            // then
+            assertThat(actual).isEqualTo(List.of(festival1, festival2, festival3));
+        }
+    }
+
+    @Nested
+    class 진행_축제_반환 {
+
+        @Test
+        void 성공() {
+            // given
+            FestivalFilter filter = FestivalFilter.PROGRESS;
+            prepareNotOrderedFestivals();
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
+
+            // then
+            assertSoftly(softAssertions -> {
+                assertThat(actual).hasSize(1);
+                assertThat(actual.get(0)).matches(festival -> festival.getName().equals(CURRENT_FESTIVAL));
+            });
+        }
+
+        @Test
+        void 은_시작_시점이_빠른_순서로_반환된다() {
+            // given
+            FestivalFilter filter = FestivalFilter.PROGRESS;
+            LocalDate currentTime = LocalDate.parse("2023-10-01");
+            Festival festival2 = festivalRepository.save(
+                new Festival("festival2", currentTime.minusDays(2), currentTime.plusDays(10), school));
+            Festival festival3 = festivalRepository.save(
+                new Festival("festival3", currentTime.minusDays(1), currentTime.plusDays(10), school));
+            Festival festival1 = festivalRepository.save(
+                new Festival("festival1", currentTime.minusDays(3), currentTime.plusDays(10), school));
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(currentTime));
+
+            // then
+            assertThat(actual).isEqualTo(List.of(festival1, festival2, festival3));
+        }
+    }
+
+    @Nested
+    class 종료_축제_반환 {
+
+        @Test
+        void 성공() {
+            // given
+            FestivalFilter filter = FestivalFilter.END;
+            prepareNotOrderedFestivals();
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(now));
+
+            // then
+            assertSoftly(softAssertions -> {
+                assertThat(actual).hasSize(1);
+                assertThat(actual.get(0)).matches(festival -> festival.getName().equals(PAST_FESTIVAL));
+            });
+        }
+
+        @Test
+        void 은_시작_시점이_빠른_순서로_반환된다() {
+            // given
+            FestivalFilter filter = FestivalFilter.END;
+            LocalDate currentTime = LocalDate.parse("2023-10-01");
+            Festival festival2 = festivalRepository.save(
+                new Festival("festival2", currentTime.minusDays(10), currentTime.minusDays(2), school));
+            Festival festival3 = festivalRepository.save(
+                new Festival("festival3", currentTime.minusDays(10), currentTime.minusDays(1), school));
+            Festival festival1 = festivalRepository.save(
+                new Festival("festival1", currentTime.minusDays(10), currentTime.minusDays(3), school));
+
+            // when
+            List<Festival> actual = festivalRepository.findAll(filter.getSpecification(currentTime));
+
+            // then
+            assertThat(actual).isEqualTo(List.of(festival3, festival2, festival1));
+        }
     }
 }
