a21401f883a41e1a07ef9989fb45e8bf22945969
SeongHoonC <108349655+SeongHoonC@users.noreply.github.com>
2023-11-14 12:30:13 +0900

    [AN/USER] feat: 홈화면 필터링 기능을 제공한다 (#600) (#606)
    * feat: 축제 목록 필터 이름 상수화
    
    * feat: 축제 목록 칩 색깔 및 텍스트 스타일 작성
    
    * feat: 축제 목록 칩으로 필터링하기 화면 그리기
    
    * feat: 축제 필터 분류 정의
    
    * feat: FestivalRepository 필터링 요청 가능하도록 재정의
    
    * feat: 축제 목록 ViewModel 에 축제 필터링 기능 적용
    
    * feat: 축제 목록 Fragment 에 필터링 기능 적용
    
    * feat: 축제 목록 필터링 API 연동 작업
    
    * feat: 페스타고 홈화면 아이콘 추가
    
    * refactor: 칩 색깔 selector 네이밍 수정
    
    * refactor: 축제 목록 칩 색깔 네이밍 변경
    
    * feat: 필터링 칩 다크모드 적용
    
    * feat: 필터링 상태를 ViewModel 에서 관리하지 않는다
    
    * feat: 현재 선택된 칩의 필터링에 해당하는 리스트를 요청한다
    
    * fix: Loading 이 아니면 구성 변경 시 축제 목록을 재요청하지 않는다

--- android/festago/domain/src/main/java/com/festago/festago/repository/FestivalRepository.kt
+++ android/festago/domain/src/main/java/com/festago/festago/repository/FestivalRepository.kt
@@ -1,9 +1,10 @@
 package com.festago.festago.repository
 
 import com.festago.festago.model.Festival
+import com.festago.festago.model.FestivalFilter
 import com.festago.festago.model.Reservation
 
 interface FestivalRepository {
-    suspend fun loadFestivals(): Result<List<Festival>>
+    suspend fun loadFestivals(festivalFilter: FestivalFilter): Result<List<Festival>>
     suspend fun loadFestivalDetail(festivalId: Long): Result<Reservation>
 }
