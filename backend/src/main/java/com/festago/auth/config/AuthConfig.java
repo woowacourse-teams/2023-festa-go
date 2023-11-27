cfef8d0b7906a26537a43151d83316a195225387
seokjin8678 <seokjin8678@gmail.com>
2023-11-27 14:21:03 +0900

    refactor: BCryptPasswordEncoder -> DelegatingPasswordEncoder 구현체 변경

--- backend/src/main/java/com/festago/auth/config/AuthConfig.java
+++ backend/src/main/java/com/festago/auth/config/AuthConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -43,6 +43,6 @@ public class AuthConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
