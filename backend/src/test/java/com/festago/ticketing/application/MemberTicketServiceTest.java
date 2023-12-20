46d11889d69ae89bb1a0578f827c60ed33a4ff04
seokjin8678 <seokjin8678@gmail.com>
2023-12-20 22:11:10 +0900

    [BE] refactor: MemberTicketService 로직을 개선한다. (#616) (#617)
    * refactor: 불필요한 stream 제거
    
    * refactor: 불필요한 Member 검증 제거
    
    * refactor: 사용하지 않는 정적 필드, 메서드 제거

--- backend/src/test/java/com/festago/ticketing/application/MemberTicketServiceTest.java
+++ backend/src/test/java/com/festago/ticketing/application/MemberTicketServiceTest.java
@@ -1,6 +1,5 @@
 package com.festago.ticketing.application;
 
-import static com.festago.common.exception.ErrorCode.MEMBER_NOT_FOUND;
 import static com.festago.common.exception.ErrorCode.MEMBER_TICKET_NOT_FOUND;
 import static com.festago.common.exception.ErrorCode.NOT_MEMBER_TICKET_OWNER;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,11 +12,8 @@ import static org.mockito.BDDMockito.given;
 import com.festago.common.exception.BadRequestException;
 import com.festago.common.exception.NotFoundException;
 import com.festago.member.domain.Member;
-import com.festago.member.repository.MemberRepository;
-import com.festago.stage.domain.Stage;
 import com.festago.support.MemberFixture;
 import com.festago.support.MemberTicketFixture;
-import com.festago.support.StageFixture;
 import com.festago.ticketing.domain.MemberTicket;
 import com.festago.ticketing.dto.MemberTicketResponse;
 import com.festago.ticketing.dto.MemberTicketsResponse;
@@ -46,9 +42,6 @@ class MemberTicketServiceTest {
     @Mock
     MemberTicketRepository memberTicketRepository;
 
-    @Mock
-    MemberRepository memberRepository;
-
     @Spy
     Clock clock = Clock.systemDefaultZone();
 
@@ -56,39 +49,108 @@ class MemberTicketServiceTest {
     MemberTicketService memberTicketService;
 
     @Nested
-    class 사용자의_멤버티켓_전체_조회 {
+    class 멤버_티켓_아이디로_단건_조회 {
 
         @Test
-        void 멤버가_없으면_예외() {
+        void 멤버_티켓이_없으면_예외() {
             // given
             Long memberId = 1L;
+            Long memberTicketId = 1L;
 
-            given(memberRepository.findById(memberId))
+            given(memberTicketRepository.findById(memberTicketId))
                 .willReturn(Optional.empty());
 
             // when & then
-            assertThatThrownBy(() -> memberTicketService.findAll(memberId, PageRequest.ofSize(1)))
+            assertThatThrownBy(() -> memberTicketService.findById(memberId, memberTicketId))
                 .isInstanceOf(NotFoundException.class)
-                .hasMessage(MEMBER_NOT_FOUND.getMessage());
+                .hasMessage(MEMBER_TICKET_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 사용자가_티켓의_주인이_아니면_예외() {
+            // given
+            Long memberId = 1L;
+            Long memberTicketId = 1L;
+            Member other = MemberFixture.member()
+                .id(2L)
+                .build();
+
+            MemberTicket otherMemberTicket = MemberTicketFixture.memberTicket()
+                .id(memberTicketId)
+                .owner(other)
+                .build();
+
+            given(memberTicketRepository.findById(memberTicketId))
+                .willReturn(Optional.of(otherMemberTicket));
+
+            // when & then
+            assertThatThrownBy(() -> memberTicketService.findById(memberId, memberTicketId))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(NOT_MEMBER_TICKET_OWNER.getMessage());
+        }
+
+        @Test
+        void 성공() {
+            // given
+            Long memberId = 1L;
+            Long memberTicketId = 1L;
+            Member member = MemberFixture.member()
+                .id(memberId)
+                .build();
+            MemberTicket memberTicket = MemberTicketFixture.memberTicket()
+                .id(memberTicketId)
+                .owner(member)
+                .build();
+
+            given(memberTicketRepository.findById(memberTicketId))
+                .willReturn(Optional.of(memberTicket));
+
+            // when
+            MemberTicketResponse response = memberTicketService.findById(memberId, memberTicketId);
+
+            // then
+            assertThat(response.id()).isEqualTo(memberTicketId);
+        }
+    }
+
+    @Nested
+    class 사용자의_멤버티켓_전체_조회 {
+
+        @Test
+        void 멤버_티켓이_없으면_빈_리스트() {
+            // given
+            Long memberId = 1L;
+
+            // when
+            MemberTicketsResponse response = memberTicketService.findAll(memberId, PageRequest.ofSize(1));
+
+            // then
+            assertThat(response.memberTickets()).isEmpty();
+        }
+
+        @Test
+        void 성공() {
+            // given
+            Long memberId = 1L;
+            MemberTicket first = MemberTicketFixture.memberTicket()
+                .id(1L)
+                .build();
+            MemberTicket second = MemberTicketFixture.memberTicket()
+                .id(2L)
+                .build();
+            given(memberTicketRepository.findAllByOwnerId(eq(memberId), any(Pageable.class)))
+                .willReturn(List.of(first, second));
+
+            // when
+            MemberTicketsResponse response = memberTicketService.findAll(memberId, PageRequest.ofSize(1));
+
+            // then
+            assertThat(response.memberTickets()).hasSize(2);
         }
     }
 
     @Nested
     class 현재_멤버티켓_조회 {
-
-        @Test
-        void 멤버가_없으면_예외() {
-            // given
-            Long memberId = 1L;
-
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.empty());
-
-            // when & then
-            assertThatThrownBy(() -> memberTicketService.findCurrent(memberId, Pageable.ofSize(10)))
-                .isInstanceOf(NotFoundException.class)
-                .hasMessage(MEMBER_NOT_FOUND.getMessage());
-        }
 
         @Test
         void 입장시간이_24시간_지난_티켓은_조회되지_않는다() {
@@ -100,8 +162,6 @@ class MemberTicketServiceTest {
 
             given(memberTicketRepository.findAllByOwnerId(anyLong(), any(Pageable.class)))
                 .willReturn(List.of(memberTicket));
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
 
             // when
             MemberTicketsResponse response = memberTicketService.findCurrent(memberId, Pageable.ofSize(100));
@@ -125,8 +185,6 @@ class MemberTicketServiceTest {
 
             given(memberTicketRepository.findAllByOwnerId(eq(memberId), any(Pageable.class)))
                 .willReturn(List.of(pendingMemberTicket, activateMemberTicket));
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
 
             // when
             MemberTicketsResponse response = memberTicketService.findCurrent(memberId, Pageable.ofSize(100));
@@ -162,8 +220,6 @@ class MemberTicketServiceTest {
             given(memberTicketRepository.findAllByOwnerId(eq(memberId), any(Pageable.class)))
                 .willReturn(
                     List.of(pendingMemberTicket1, pendingMemberTicket2, activateMemberTicket1, activateMemberTicket2));
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
 
             // when
             MemberTicketsResponse response = memberTicketService.findCurrent(memberId, Pageable.ofSize(100));
@@ -173,94 +229,6 @@ class MemberTicketServiceTest {
                 .map(MemberTicketResponse::id)
                 .toList();
             assertThat(memberTicketIds).containsExactly(4L, 3L, 1L, 2L);
-        }
-    }
-
-    @Nested
-    class 멤버_티켓_아이디로_단건_조회 {
-
-        @Test
-        void 멤버가_없으면_예외() {
-            // given
-            Long memberId = 1L;
-
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.empty());
-
-            // when & then
-            assertThatThrownBy(() -> memberTicketService.findById(memberId, 1L))
-                .isInstanceOf(NotFoundException.class)
-                .hasMessage(MEMBER_NOT_FOUND.getMessage());
-        }
-
-        @Test
-        void 사용자의_티켓이_없으면_예외() {
-            // given
-            Long memberId = 1L;
-            Long memberTicketId = 1L;
-
-            given(memberTicketRepository.findById(memberTicketId))
-                .willReturn(Optional.empty());
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
-
-            // when & then
-            assertThatThrownBy(() -> memberTicketService.findById(memberId, memberTicketId))
-                .isInstanceOf(NotFoundException.class)
-                .hasMessage(MEMBER_TICKET_NOT_FOUND.getMessage());
-        }
-
-        @Test
-        void 사용자가_티켓의_주인이_아니면_예외() {
-            // given
-            Long memberId = 1L;
-            Member other = MemberFixture.member()
-                .id(2L)
-                .build();
-
-            MemberTicket otherMemberTicket = MemberTicketFixture.memberTicket()
-                .owner(other)
-                .build();
-
-            Long otherTicketId = otherMemberTicket.getId();
-
-            given(memberTicketRepository.findById(otherTicketId))
-                .willReturn(Optional.of(otherMemberTicket));
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
-
-            // when & then
-            assertThatThrownBy(() -> memberTicketService.findById(memberId, otherTicketId))
-                .isInstanceOf(BadRequestException.class)
-                .hasMessage(NOT_MEMBER_TICKET_OWNER.getMessage());
-        }
-
-        @Test
-        void 성공() {
-            // given
-            Long memberId = 2L;
-            Member member = MemberFixture.member()
-                .id(memberId)
-                .build();
-            Stage stage = StageFixture.stage()
-                .build();
-            Long memberTicketId = 1L;
-            MemberTicket memberTicket = MemberTicketFixture.memberTicket()
-                .id(1L)
-                .owner(member)
-                .stage(stage)
-                .build();
-
-            given(memberTicketRepository.findById(memberTicketId))
-                .willReturn(Optional.of(memberTicket));
-            given(memberRepository.findById(memberId))
-                .willReturn(Optional.of(new Member(memberId)));
-
-            // when
-            MemberTicketResponse response = memberTicketService.findById(member.getId(), memberTicketId);
-
-            // then
-            assertThat(response.id()).isEqualTo(memberTicketId);
         }
     }
 }
