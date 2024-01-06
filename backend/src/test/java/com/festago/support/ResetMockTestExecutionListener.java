3aab659cf26cc9a8ed8755bc51027541ae5aba84
seokjin8678 <seokjin8678@gmail.com>
2024-01-07 00:36:39 +0900

    refactor: Mock 캐싱하여 성능 향상

--- backend/src/test/java/com/festago/support/ResetMockTestExecutionListener.java
+++ backend/src/test/java/com/festago/support/ResetMockTestExecutionListener.java
@@ -1,5 +1,7 @@
 package com.festago.support;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.mockito.MockingDetails;
 import org.mockito.Mockito;
 import org.springframework.context.ApplicationContext;
@@ -8,14 +10,30 @@ import org.springframework.test.context.TestExecutionListener;
 
 public class ResetMockTestExecutionListener implements TestExecutionListener {
 
+    private static long applicationContextStartupDate;
+    private static final List<Object> mockCache = new ArrayList<>();
+
     @Override
     public void afterTestMethod(TestContext testContext) throws Exception {
         ApplicationContext applicationContext = testContext.getApplicationContext();
+        if (mockCache.isEmpty() || isNewContext(applicationContext)) {
+            mockCache.clear();
+            applicationContextStartupDate = applicationContext.getStartupDate();
+            initMocks(applicationContext);
+        }
+        mockCache.forEach(Mockito::reset);
+    }
+
+    private boolean isNewContext(ApplicationContext applicationContext) {
+        return applicationContext.getStartupDate() != applicationContextStartupDate;
+    }
+
+    private void initMocks(ApplicationContext applicationContext) {
         for (String beanName : applicationContext.getBeanDefinitionNames()) {
             Object bean = applicationContext.getBean(beanName);
             MockingDetails mockingDetails = Mockito.mockingDetails(bean);
             if (mockingDetails.isMock()) {
-                Mockito.reset(bean);
+                mockCache.add(bean);
             }
         }
     }
