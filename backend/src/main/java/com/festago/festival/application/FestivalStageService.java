daea6cffe077259eccbc61b32b95eca222820b37
BGuga <tmdwns9913@gmail.com>
2024-01-07 00:02:38 +0900

    refactor: 불 필요한 @Component 제거

--- backend/src/main/java/com/festago/festival/application/FestivalStageService.java
+++ backend/src/main/java/com/festago/festival/application/FestivalStageService.java
@@ -1,9 +1,7 @@
 package com.festago.festival.application;
 
 import com.festago.festival.dto.DetailFestivalResponse;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface FestivalStageService {
 
     DetailFestivalResponse findDetail(Long festivalId);
