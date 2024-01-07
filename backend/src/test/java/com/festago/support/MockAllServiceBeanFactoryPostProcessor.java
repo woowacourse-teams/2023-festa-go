3e12348720e33fd7d698626dae42998ebe24cc2c
seokjin8678 <seokjin8678@gmail.com>
2024-01-07 19:08:11 +0900

    refactor: BeanDefinition GenericBeanDefinition 타입 등록하게 변경
    - RootBeanDefinition에서 선언적 방식은 GenericBeanDefinition 권장

--- backend/src/test/java/com/festago/support/MockAllServiceBeanFactoryPostProcessor.java
+++ backend/src/test/java/com/festago/support/MockAllServiceBeanFactoryPostProcessor.java
@@ -5,10 +5,11 @@ import static org.mockito.Mockito.mock;
 import org.junit.platform.commons.util.ClassFilter;
 import org.junit.platform.commons.util.ReflectionUtils;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.stereotype.Service;
 
 public class MockAllServiceBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
@@ -19,7 +20,9 @@ public class MockAllServiceBeanFactoryPostProcessor implements BeanFactoryPostPr
         ClassFilter classFilter = ClassFilter.of(clazz -> clazz.isAnnotationPresent(Service.class));
         ReflectionUtils.findAllClassesInPackage("com.festago", classFilter)
             .forEach(clazz -> {
-                registry.registerBeanDefinition(clazz.getSimpleName(), new RootBeanDefinition(clazz));
+                BeanDefinition bean = BeanDefinitionBuilder.genericBeanDefinition(clazz)
+                    .getBeanDefinition();
+                registry.registerBeanDefinition(clazz.getSimpleName(), bean);
                 beanFactory.registerSingleton(clazz.getSimpleName(), mock(clazz));
             });
     }
