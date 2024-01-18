8d8bab789492299f343c4601f4cb1632da82bdcd
vrexpert <vrexpert2302@gmail.com>
2024-01-18 17:44:16 +0900

    feat: 바텀 프레그먼드 변경 로직 구현

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/HomeActivity.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/HomeActivity.kt
@@ -1,9 +1,19 @@
 package com.festago.festago.presentation.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.festago.festago.presentation.R
 import com.festago.festago.presentation.databinding.ActivityHomeBinding
+import com.festago.festago.presentation.ui.home.bookmarklist.BookmarkListFragment
+import com.festago.festago.presentation.ui.home.festivallist.FestivalListFragment
+import com.festago.festago.presentation.ui.home.mypage.MyPageFragment
+import com.festago.festago.presentation.ui.home.ticketlist.TicketListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,9 +26,89 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBinding()
+        initView()
+        initBackPressedDispatcher()
     }
 
     private fun initBinding() {
         setContentView(binding.root)
+    }
+
+    private fun initView() {
+        binding.nvHome.setOnItemSelectedListener {
+            selectView(it.itemId)
+            true
+        }
+        changeFragment<FestivalListFragment>()
+    }
+
+    private fun selectView(menuItemId: Int) {
+        return when (menuItemId) {
+            R.id.item_festival -> showFestivalList()
+            R.id.item_ticket -> showTicketList()
+            R.id.item_bookmark -> showBookmarkList()
+            R.id.item_my_page -> showMyPage()
+            else -> throw IllegalArgumentException("menu item id not found")
+        }
+    }
+
+    private fun showFestivalList() {
+        changeFragment<FestivalListFragment>()
+    }
+
+    private fun showTicketList() {
+        changeFragment<TicketListFragment>()
+    }
+
+    private fun showBookmarkList() {
+        changeFragment<BookmarkListFragment>()
+    }
+
+    private fun showMyPage() {
+        changeFragment<MyPageFragment>()
+    }
+
+    private inline fun <reified T : Fragment> changeFragment() {
+        val tag = T::class.java.name
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        supportFragmentManager.fragments.forEach { fragment ->
+            fragmentTransaction.hide(fragment)
+        }
+
+        var targetFragment = supportFragmentManager.findFragmentByTag(tag)
+
+        if (targetFragment == null) {
+            targetFragment = T::class.java.newInstance()
+            fragmentTransaction.add(R.id.fcvHomeContainer, targetFragment, tag)
+        } else {
+            fragmentTransaction.show(targetFragment)
+        }
+
+        fragmentTransaction.commit()
+    }
+
+    private fun initBackPressedDispatcher() {
+        var backPressedTime = START_BACK_PRESSED_TIME
+        onBackPressedDispatcher.addCallback {
+            if ((System.currentTimeMillis() - backPressedTime) > FINISH_BACK_PRESSED_TIME) {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(
+                    this@HomeActivity,
+                    getString(R.string.home_back_pressed),
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val START_BACK_PRESSED_TIME = 0L
+        private const val FINISH_BACK_PRESSED_TIME = 3000L
+        fun getIntent(context: Context): Intent {
+            return Intent(context, HomeActivity::class.java)
+        }
     }
 }
---
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
@@ -0,0 +1,24 @@
+package com.festago.festago.presentation.ui.home.festivallist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.festago.festago.presentation.R
+
+class FestivalListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_festival_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
