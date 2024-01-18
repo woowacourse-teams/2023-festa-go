401a01a736c75b0cdc3f2fe71866c98d713370f1
seokjin8678 <seokjin8678@gmail.com>
2024-01-18 22:30:23 +0900

    feat: SchoolDeleteService 추가

--- backend/src/main/java/com/festago/festival/repository/FestivalRepository.java
+++ backend/src/main/java/com/festago/festival/repository/FestivalRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long>, FestivalRepositoryCustom {
 
+    boolean existsBySchoolId(Long schoolId);
 }
--- backend/src/main/java/com/festago/school/application/SchoolDeleteService.java
+++ backend/src/main/java/com/festago/school/application/SchoolDeleteService.java
@@ -3,20 +3,19 @@ package com.festago.school.application;
 import com.festago.school.repository.SchoolRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-@Slf4j
 public class SchoolDeleteService {
 
     private final SchoolRepository schoolRepository;
     private final List<SchoolDeleteValidator> validators;
 
     public void deleteSchool(Long schoolId) {
-        // TODO
+        validators.forEach(validator -> validator.validate(schoolId));
+        schoolRepository.deleteById(schoolId);
     }
 }
