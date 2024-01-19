0cce5caef255c651eb9298919dc9efa5f3c02376
vrexpert <vrexpert2302@gmail.com>
2024-01-20 01:26:07 +0900

    feat: 축제 목록 화면 Fragment 정의

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/bindingadapter/Image.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/bindingadapter/Image.kt
@@ -3,7 +3,6 @@ package com.festago.festago.presentation.ui.bindingadapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.festago.festago.presentation.R
 import jp.wasabeef.glide.transformations.BlurTransformation
@@ -22,7 +21,6 @@ fun ImageView.setBlurImage(imageUrl: String?, blurRadius: Int, blurSampling: Int
     val transformation = BlurTransformation(blurRadius, blurSampling)
     Glide.with(context)
         .load(imageUrl)
-        .transition(DrawableTransitionOptions.withCrossFade())
         .apply(RequestOptions.bitmapTransform(transformation))
         .error(R.drawable.ic_launcher_background)
         .fallback(R.drawable.ic_launcher_background)
--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
@@ -5,20 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.festago.festago.presentation.R
+import com.festago.festago.presentation.databinding.FragmentFestivalListBinding
+import com.festago.festago.presentation.ui.home.festivallist.popularfestival.PopularFestivalViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FestivalListFragment : Fragment() {
+
+    private var _binding: FragmentFestivalListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var popularFestivalViewPager: PopularFestivalViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_festival_list, container, false)
+    ): View {
+        _binding = FragmentFestivalListBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        popularFestivalViewPager = PopularFestivalViewPagerAdapter(
+            context = requireContext(),
+            foregroundViewPager = binding.vpPopularFestivalForeground,
+            backgroundViewPager = binding.vpPopularFestivalBackground,
+        )
+
+        TabLayoutMediator(
+            binding.intoTabLayout,
+            binding.vpPopularFestivalForeground,
+        ) { tab, position -> }.attach()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
