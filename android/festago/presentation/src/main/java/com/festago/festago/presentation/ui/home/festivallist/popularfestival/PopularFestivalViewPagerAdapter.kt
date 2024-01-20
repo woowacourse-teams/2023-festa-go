6af1e921a17d454f09d5bbb34738d96ee47bbf72
vrexpert <vrexpert2302@gmail.com>
2024-01-20 18:28:20 +0900

    fix: dp to px 변환으로 원한는대로 비율 조정

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/popularfestival/PopularFestivalViewPagerAdapter.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/popularfestival/PopularFestivalViewPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.festago.festago.presentation.ui.home.festivallist.popularfestival
 
+import android.content.res.Resources
 import androidx.viewpager2.widget.ViewPager2
 import com.festago.festago.presentation.ui.home.festivallist.popularfestival.background.PopularFestivalBackgroundAdapter
 import com.festago.festago.presentation.ui.home.festivallist.popularfestival.foreground.PopularFestivalForegroundAdapter
@@ -38,9 +39,14 @@ class PopularFestivalViewPagerAdapter(
 
     private fun narrowSpaceViewPager(viewPager: ViewPager2) {
         viewPager.setPageTransformer { page, position ->
-            val offset = position * -(2 * OFFSET_BETWEEN_PAGES)
+            val offset = position * -(2 * dpToPx(OFFSET_BETWEEN_PAGES))
             page.translationX = offset
         }
+    }
+
+    private fun dpToPx(dp: Float): Int {
+        val density = Resources.getSystem().displayMetrics.density
+        return (dp * density).toInt()
     }
 
     private fun setOffscreenPagesLimit(viewPager: ViewPager2, limit: Int) {
