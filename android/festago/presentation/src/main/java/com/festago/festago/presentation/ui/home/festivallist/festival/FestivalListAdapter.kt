6648296a8ef8472f7a3a11091ae6d481053b34a9
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-22 05:54:00 +0900

    feat(festival-list): 뷰홀더에서 역할 이동

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListAdapter.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListAdapter.kt
@@ -3,19 +3,37 @@ package com.festago.festago.presentation.ui.home.festivallist.festival
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalItemUiState
+import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalListUiState
+import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalTabUiState
 
 class FestivalListAdapter : ListAdapter<Any, FestivalListViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FestivalListViewHolder {
-        return FestivalListViewHolder.of(parent, viewType)
+        return when (viewType) {
+            1 -> FestivalListPopularViewHolder.of(parent)
+            2 -> FestivalListFestivalViewHolder.of(parent)
+            3 -> FestivalListTabViewHolder.of(parent)
+            else -> throw IllegalArgumentException("Invalid viewType")
+        }
     }
 
     override fun onBindViewHolder(holder: FestivalListViewHolder, position: Int) {
-        return FestivalListViewHolder.bind(holder, getItem(position))
+        val item = getItem(position)
+        return when (holder) {
+            is FestivalListPopularViewHolder -> holder.bind((item as FestivalListUiState.Success).festivals)
+            is FestivalListFestivalViewHolder -> holder.bind(item as FestivalItemUiState)
+            is FestivalListTabViewHolder -> holder.bind(item as FestivalTabUiState)
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return FestivalListViewHolder.viewTypeOf(getItem(position))
+        return when (getItem(position)) {
+            is FestivalListUiState -> 1
+            is FestivalItemUiState -> 2
+            is FestivalTabUiState -> 3
+            else -> throw IllegalArgumentException("Invalid item")
+        }
     }
 
     companion object {
--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListViewHolder.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListViewHolder.kt
@@ -1,39 +1,7 @@
 package com.festago.festago.presentation.ui.home.festivallist.festival
 
-import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalItemUiState
-import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalListUiState
-import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalTabUiState
 
 sealed class FestivalListViewHolder(binding: ViewDataBinding) :
-    RecyclerView.ViewHolder(binding.root) {
-    companion object {
-        fun viewTypeOf(item: Any): Int {
-            return when (item) {
-                is FestivalListUiState -> 1
-                is FestivalItemUiState -> 2
-                is FestivalTabUiState -> 3
-                else -> throw IllegalArgumentException("Invalid item")
-            }
-        }
-
-        fun of(parent: ViewGroup, viewType: Int): FestivalListViewHolder {
-            return when (viewType) {
-                1 -> FestivalListPopularViewHolder.of(parent)
-                2 -> FestivalListFestivalViewHolder.of(parent)
-                3 -> FestivalListTabViewHolder.of(parent)
-                else -> throw IllegalArgumentException("Invalid viewType")
-            }
-        }
-
-        fun bind(holder: FestivalListViewHolder, item: Any) {
-            when (holder) {
-                is FestivalListPopularViewHolder -> holder.bind((item as FestivalListUiState.Success).festivals)
-                is FestivalListFestivalViewHolder -> holder.bind(item as FestivalItemUiState)
-                is FestivalListTabViewHolder -> holder.bind(item as FestivalTabUiState)
-            }
-        }
-    }
-}
+    RecyclerView.ViewHolder(binding.root)
