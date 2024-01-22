6b8132cbf659b67345f3f1c6baf2711c487e2f18
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-22 20:33:02 +0900

    refactor(festival-list): 탭 레이아 설정을 프로그래밍으로 변경

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListTabViewHolder.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/festival/FestivalListTabViewHolder.kt
@@ -2,12 +2,25 @@ package com.festago.festago.presentation.ui.home.festivallist.festival
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.festago.festago.presentation.R
 import com.festago.festago.presentation.databinding.ItemFestivalListTabBinding
 import com.festago.festago.presentation.ui.home.festivallist.uistate.FestivalTabUiState
 import com.google.android.material.tabs.TabLayout
 
 class FestivalListTabViewHolder(val binding: ItemFestivalListTabBinding) :
     FestivalListViewHolder(binding) {
+    init {
+        binding.tlFestivalListTab.addTab(
+            binding.tlFestivalListTab.newTab().setText(
+                binding.root.context.getText(R.string.festival_list_ti_current_festival),
+            ),
+        )
+        binding.tlFestivalListTab.addTab(
+            binding.tlFestivalListTab.newTab().setText(
+                binding.root.context.getText(R.string.festival_list_ti_upcoming_festival),
+            ),
+        )
+    }
 
     fun bind(festivalTabUiState: FestivalTabUiState) {
         binding.tlFestivalListTab.addOnTabSelectedListener(object :
