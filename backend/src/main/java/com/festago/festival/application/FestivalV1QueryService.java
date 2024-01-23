65682e723f29e8349de492f5ef2961f6430267f2
BGuga <tmdwns9913@gmail.com>
2024-01-23 20:24:35 +0900

    refactor: FestivalV1ListResponse 변환 로직을 service 로 이동

--- backend/src/main/java/com/festago/festival/application/FestivalV1QueryService.java
+++ backend/src/main/java/com/festago/festival/application/FestivalV1QueryService.java
@@ -1,10 +1,15 @@
 package com.festago.festival.application;
 
+import com.festago.common.exception.ErrorCode;
+import com.festago.common.exception.InternalServerException;
 import com.festago.festival.domain.Festival;
 import com.festago.festival.domain.FestivalInfo;
 import com.festago.festival.domain.FestivalInfoSerializer;
+import com.festago.festival.dto.v1.ArtistV1Response;
 import com.festago.festival.dto.v1.FestivalV1ListResponse;
+import com.festago.festival.dto.v1.FestivalV1Response;
 import com.festago.festival.dto.v1.FestivalV1lListRequest;
+import com.festago.festival.dto.v1.SchoolV1Response;
 import com.festago.festival.repository.FestivalInfoRepository;
 import com.festago.festival.repository.FestivalPage;
 import com.festago.festival.repository.FestivalPageable;
@@ -44,10 +49,41 @@ public class FestivalV1QueryService {
             .map(Festival::getId)
             .toList();
         List<FestivalInfo> festivalInfos = festivalInfoRepository.findAllByFestivalIdIn(festivalIds);
-        return FestivalV1ListResponse.of(festivalPage.isLastPage(),
+        return makeDto(festivalPage.isLastPage(),
             festivalPage.getFestivals(),
-            festivalInfos,
-            infoSerializer);
+            festivalInfos);
+    }
+
+    private FestivalV1ListResponse makeDto(boolean isLastPage, List<Festival> festivals,
+                                           List<FestivalInfo> festivalInfos) {
+        List<FestivalV1Response> festivalV1Responses = festivals.stream()
+            .map(festival -> toFestivalV1Response(festival, festivalInfos))
+            .toList();
+        return new FestivalV1ListResponse(isLastPage, festivalV1Responses);
+    }
+
+    private FestivalV1Response toFestivalV1Response(
+        Festival festival,
+        List<FestivalInfo> festivalInfos) {
+        FestivalInfo info = festivalInfos.stream()
+            .filter(festivalInfo -> festivalInfo.getFestivalId().equals(festival.getId()))
+            .findAny()
+            .orElseThrow(() -> new InternalServerException(ErrorCode.FESTIVAL_INFO_ERROR));
+        return new FestivalV1Response(
+            festival.getId(),
+            festival.getName(),
+            festival.getStartDate(),
+            festival.getEndDate(),
+            festival.getThumbnail(),
+            SchoolV1Response.from(festival.getSchool()),
+            makeArtistResponse(info.getArtistInfo(), infoSerializer)
+        );
+    }
+
+    private List<ArtistV1Response> makeArtistResponse(String artistInfo, FestivalInfoSerializer infoSerializer) {
+        return infoSerializer.deserialize(artistInfo).stream()
+            .map(ArtistV1Response::from)
+            .toList();
     }
 
     private FestivalPageable makePageable(FestivalV1lListRequest request) {
--- backend/src/main/java/com/festago/festival/dto/v1/FestivalV1ListResponse.java
+++ backend/src/main/java/com/festago/festival/dto/v1/FestivalV1ListResponse.java
@@ -1,51 +1,10 @@
 package com.festago.festival.dto.v1;
 
-import com.festago.common.exception.ErrorCode;
-import com.festago.common.exception.InternalServerException;
-import com.festago.festival.domain.Festival;
-import com.festago.festival.domain.FestivalInfo;
-import com.festago.festival.domain.FestivalInfoSerializer;
 import java.util.List;
 
 public record FestivalV1ListResponse(
     boolean isLastPage,
     List<FestivalV1Response> festivals
-
 ) {
 
-    public static FestivalV1ListResponse of(
-        boolean isLastPage,
-        List<Festival> festivals,
-        List<FestivalInfo> festivalInfos,
-        FestivalInfoSerializer infoSerializer) {
-        List<FestivalV1Response> festivalV1Responses = festivals.stream()
-            .map(festival -> toFestivalV1Response(festival, festivalInfos, infoSerializer))
-            .toList();
-        return new FestivalV1ListResponse(isLastPage, festivalV1Responses);
-    }
-
-    private static FestivalV1Response toFestivalV1Response(
-        Festival festival,
-        List<FestivalInfo> festivalInfos,
-        FestivalInfoSerializer infoSerializer) {
-        FestivalInfo info = festivalInfos.stream()
-            .filter(festivalInfo -> festivalInfo.getFestivalId().equals(festival.getId()))
-            .findAny()
-            .orElseThrow(() -> new InternalServerException(ErrorCode.FESTIVAL_INFO_ERROR));
-        return new FestivalV1Response(
-            festival.getId(),
-            festival.getName(),
-            festival.getStartDate(),
-            festival.getEndDate(),
-            festival.getThumbnail(),
-            SchoolV1Response.from(festival.getSchool()),
-            makeArtistResponse(info.getArtistInfo(), infoSerializer)
-        );
-    }
-
-    private static List<ArtistV1Response> makeArtistResponse(String artistInfo, FestivalInfoSerializer infoSerializer) {
-        return infoSerializer.deserialize(artistInfo).stream()
-            .map(ArtistV1Response::from)
-            .toList();
-    }
 }
