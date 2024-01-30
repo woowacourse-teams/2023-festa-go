8bb2e04598ff769364bad947a5ac55e1fed9c149
Seokjin Jeon <seokjin8678@gmail.com>
2024-01-30 20:06:11 +0900

    [BE] feat: FesitvalQueryInfo에 BaseTimeEntity 상속, 유니크 인덱스 추가 (#685) (#686)
    * feat: FestivalQueryInfo BaseTimeEntity 상속 추가
    
    * feat: festival_query_info 테이블 festival_id 유니크 인덱스 추가
    
    * feat: festival 테이블 start_date 내림차순 인덱스 추가

--- backend/src/main/resources/db/migration/V11__modify_school_and_festival_query_info.sql
+++ backend/src/main/resources/db/migration/V11__modify_school_and_festival_query_info.sql
@@ -6,12 +6,15 @@ alter table school
 
 create table if not exists festival_query_info
 (
-    id              bigint not null auto_increment,
-    festival_id     bigint  not null,
-    created_at      datetime(6),
-    updated_at      datetime(6),
-    artist_info     text not null,
+    id          bigint not null auto_increment,
+    festival_id bigint not null,
+    created_at  datetime(6),
+    updated_at  datetime(6),
+    artist_info text   not null,
     primary key (id)
 ) engine innodb
   default charset = utf8mb4
   collate = utf8mb4_0900_ai_ci;
+
+alter table festival_query_info
+    add constraint UNIQUE_FESTIVAL_ID unique (festival_id);
