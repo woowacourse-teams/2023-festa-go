5eea17983a19f6f99142913c2c905a93a6d1a649
seokjin8678 <seokjin8678@gmail.com>
2024-01-30 16:26:07 +0900

    feat: FestivalQueryInfo BaseTimeEntity 상속 추가

--- backend/src/main/java/com/festago/festival/domain/FestivalQueryInfo.java
+++ backend/src/main/java/com/festago/festival/domain/FestivalQueryInfo.java
@@ -1,6 +1,7 @@
 package com.festago.festival.domain;
 
 import com.festago.artist.domain.Artist;
+import com.festago.common.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FestivalQueryInfo {
+public class FestivalQueryInfo extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
