5d3c4b34580d0a567a82968fcb51c1b9d29ee0bd
BGuga <tmdwns9913@gmail.com>
2024-01-30 20:16:13 +0900

    chore: swagger 추가

--- backend/src/main/java/com/festago/festival/presentation/v1/PopularFestivalV1Controller.java
+++ backend/src/main/java/com/festago/festival/presentation/v1/PopularFestivalV1Controller.java
@@ -2,6 +2,8 @@ package com.festago.festival.presentation.v1;
 
 import com.festago.festival.application.PopularFestivalV1QueryService;
 import com.festago.festival.dto.PopularFestivalsV1Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/popular/festivals")
+@Tag(name = "인기 축제 정보 요청 V1")
 @RequiredArgsConstructor
 public class PopularFestivalV1Controller {
 
     private final PopularFestivalV1QueryService popularFestivalV1QueryService;
 
     @GetMapping
+    @Operation(description = "인기 축제 목록을 7개 반환한다.", summary = "인기 축제 목록 조회")
     public ResponseEntity<PopularFestivalsV1Response> popularFestivals() {
         return ResponseEntity.ok(popularFestivalV1QueryService.findPopularFestivals());
     }
