722d4e8bd34e4f91fdcf453e7515fa8741365925
re4rk <37167652+re4rk@users.noreply.github.com>
2024-02-05 03:37:15 +0900

    refactor(domain): 날짜 자료형 LocalDate로 변환

--- android/festago/data/src/main/java/com/festago/festago/data/repository/FakeArtistRepository.kt
+++ android/festago/data/src/main/java/com/festago/festago/data/repository/FakeArtistRepository.kt
@@ -6,6 +6,7 @@ import com.festago.festago.domain.model.artist.ArtistMedia
 import com.festago.festago.domain.model.artist.Stage
 import com.festago.festago.domain.model.artist.Stages
 import com.festago.festago.domain.repository.ArtistRepository
+import java.time.LocalDate
 import javax.inject.Inject
 
 class FakeArtistRepository @Inject constructor() : ArtistRepository {
@@ -39,23 +40,23 @@ class FakeArtistRepository @Inject constructor() : ArtistRepository {
                 Stage(
                     1,
                     "예시 페스티벌 1",
-                    "2024-05-01",
-                    "2024-05-03",
+                    LocalDate.parse("2024-05-01"),
+                    LocalDate.parse("2024-05-03"),
                     "https://example.com/festival1-image.jpg",
                     listOf(
-                        Artist(101, "아티스트 A", "https://example.com/artistA-profile.jpg"),
-                        Artist(102, "아티스트 B", "https://example.com/artistB-profile.jpg")
+                        Artist(101, "뉴진스", "https://static.wikia.nocookie.net/witchers/images/d/d9/New_Jeans_Cover.png/revision/latest?cb=20220801091438"),
+                        Artist(102, "아티스트 B", "https://i0.wp.com/m.izm.co.kr/wp-content/uploads/2023/04/image-13.png?w=1200&ssl=1")
                     )
                 ),
                 Stage(
                     2,
                     "예시 페스티벌 2",
-                    "2024-06-10",
-                    "2024-06-12",
+                    LocalDate.parse("2024-06-10"),
+                    LocalDate.parse("2024-06-12"),
                     "https://example.com/festival2-image.jpg",
                     listOf(
-                        Artist(201, "아티스트 X", "https://example.com/artistX-profile.jpg"),
-                        Artist(202, "아티스트 Y", "https://example.com/artistY-profile.jpg")
+                        Artist(101, "뉴진스", "https://static.wikia.nocookie.net/witchers/images/d/d9/New_Jeans_Cover.png/revision/latest?cb=20220801091438"),
+                        Artist(102, "아티스트 B", "https://i0.wp.com/m.izm.co.kr/wp-content/uploads/2023/04/image-13.png?w=1200&ssl=1")
                     )
                 )
             )
--- android/festago/domain/src/main/java/com/festago/festago/domain/model/artist/Stage.kt
+++ android/festago/domain/src/main/java/com/festago/festago/domain/model/artist/Stage.kt
@@ -1,10 +1,12 @@
 package com.festago.festago.domain.model.artist
 
+import java.time.LocalDate
+
 data class Stage(
     val id: Int,
     val name: String,
-    val startDate: String,
-    val endDate: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
     val imageUrl: String,
     val artists: List<Artist>,
 )
