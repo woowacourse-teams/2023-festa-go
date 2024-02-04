7fcb709ea9825d61cba7371294d885b5b43c9964
re4rk <37167652+re4rk@users.noreply.github.com>
2024-02-05 07:26:57 +0900

    feat(color): 소셜 화면 추가

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/artistdetail/ArtistDetailFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/artistdetail/ArtistDetailFragment.kt
@@ -1,17 +1,27 @@
 package com.festago.festago.presentation.ui.artistdetail
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.festago.festago.presentation.R
 import com.festago.festago.presentation.databinding.FragmentArtistDetailBinding
 import com.festago.festago.presentation.ui.artistdetail.festival.ArtistDetailAdapter
 import com.festago.festago.presentation.ui.artistdetail.uistate.ArtistDetailUiState
 import com.festago.festago.presentation.util.repeatOnStarted
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class ArtistDetailFragment : Fragment() {
@@ -59,9 +69,53 @@ class ArtistDetailFragment : Fragment() {
         is ArtistDetailUiState.Loading,
         is ArtistDetailUiState.Error -> Unit
 
-        is ArtistDetailUiState.Success -> {
-            binding.uiState = uiState
-            adapter.submitList(uiState.stages)
+        is ArtistDetailUiState.Success -> handleSuccess(uiState)
+    }
+
+    private fun handleSuccess(uiState: ArtistDetailUiState.Success) {
+        binding.uiState = uiState
+        adapter.submitList(uiState.stages)
+        uiState.artist.artistMedia.forEach {
+            binding.cgArtistMedia.addView(
+                (layoutInflater.inflate(R.layout.chip_media, binding.cgArtistMedia, false) as Chip)
+                    .apply {
+                        syncIcon(it.logoUrl)
+                        text = it.name
+                    }
+            )
         }
     }
+
+    private fun Chip.syncIcon(url: String) {
+        Glide.with(this)
+            .load(url)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any,
+                    target: Target<Drawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    chipIcon =
+                        ResourcesCompat.getDrawable(
+                            resources,
+                            R.drawable.ic_launcher_background,
+                            null
+                        )
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any?,
+                    target: Target<Drawable>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    chipIcon = resource
+                    return false
+                }
+            }).preload()
+    }
 }
+
