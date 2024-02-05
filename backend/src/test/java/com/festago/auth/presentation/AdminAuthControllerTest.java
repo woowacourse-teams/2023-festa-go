2d419d5e7e4b6a0b3ac0ae032e90dcb2e77d9af4
seokjin8678 <seokjin8678@gmail.com>
2024-02-05 14:30:08 +0900

    test: 컨벤션에 맞게 Controller 테스트 코드 변경

--- backend/src/test/java/com/festago/auth/presentation/AdminAuthControllerTest.java
+++ backend/src/test/java/com/festago/auth/presentation/AdminAuthControllerTest.java
@@ -18,6 +18,7 @@ import com.festago.auth.dto.RootAdminInitializeRequest;
 import com.festago.support.CustomWebMvcTest;
 import com.festago.support.WithMockAuth;
 import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
@@ -31,7 +32,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SuppressWarnings("NonAsciiCharacters")
 class AdminAuthControllerTest {
 
-    private static final String AUTH_TOKEN_KEY = "token";
+    private static final Cookie AUTH_TOKEN = new Cookie("token", "token");
 
     @Autowired
     MockMvc mockMvc;
@@ -45,86 +46,111 @@ class AdminAuthControllerTest {
     @Nested
     class 어드민_로그인 {
 
-        @Test
-        void 유효한_요청이_보내지면_200_응답과_쿠키에_token이_있어야한다() throws Exception {
-            // given
-            var request = new AdminLoginRequest("admin", "1234");
-            given(adminAuthService.login(any(AdminLoginRequest.class)))
-                .willReturn("token");
+        final String uri = "/admin/api/login";
 
-            // when & then
-            mockMvc.perform(post("/admin/api/login")
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(cookie().exists(AUTH_TOKEN_KEY))
-                .andExpect(cookie().path(AUTH_TOKEN_KEY, "/"))
-                .andExpect(cookie().secure(AUTH_TOKEN_KEY, true))
-                .andExpect(cookie().httpOnly(AUTH_TOKEN_KEY, true))
-                .andExpect(cookie().sameSite(AUTH_TOKEN_KEY, "None"));
+        @Nested
+        @DisplayName("POST " + uri)
+        class 올바른_주소로 {
+
+            @Test
+            void 요청을_보내면_200_응답과_로그인_토큰이_담긴_쿠키가_반환된다() throws Exception {
+                // given
+                var request = new AdminLoginRequest("admin", "1234");
+                given(adminAuthService.login(any(AdminLoginRequest.class)))
+                    .willReturn("token");
+
+                // when & then
+                mockMvc.perform(post(uri)
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(cookie().exists(AUTH_TOKEN.getName()))
+                    .andExpect(cookie().path(AUTH_TOKEN.getName(), "/"))
+                    .andExpect(cookie().secure(AUTH_TOKEN.getName(), true))
+                    .andExpect(cookie().httpOnly(AUTH_TOKEN.getName(), true))
+                    .andExpect(cookie().sameSite(AUTH_TOKEN.getName(), "None"));
+            }
         }
     }
 
     @Nested
     class 어드민_회원가입 {
 
-        @Test
-        void 쿠키에_토큰이_없으면_401_응답이_반환된다() throws Exception {
-            // given
-            var request = new AdminSignupRequest("newAdmin", "1234");
+        final String uri = "/admin/api/signup";
 
-            // when & then
-            mockMvc.perform(post("/admin/api/signup")
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isUnauthorized());
-        }
+        @Nested
+        @DisplayName("POST " + uri)
+        class 올바른_주소로 {
 
-        @Test
-        @WithMockAuth(role = Role.MEMBER)
-        void 쿠키에_토큰의_권한이_올바르지_않으면_404_응답이_반환된다() throws Exception {
-            // given
-            var request = new AdminSignupRequest("newAdmin", "1234");
+            @Test
+            @WithMockAuth(role = Role.ADMIN)
+            void 요청을_보내면_200_응답과_생성한_계정이_반환된다() throws Exception {
+                var request = new AdminSignupRequest("newAdmin", "1234");
+                var response = new AdminSignupResponse("newAdmin");
+                given(adminAuthService.signup(anyLong(), any(AdminSignupRequest.class)))
+                    .willReturn(response);
 
-            // when & then
-            mockMvc.perform(post("/admin/api/signup")
-                    .cookie(new Cookie("token", "token"))
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-        }
+                // when & then
+                Cookie token = new Cookie("token", "token");
+                mockMvc.perform(post(uri)
+                        .cookie(token)
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.username").value("newAdmin"));
+            }
 
-        @Test
-        @WithMockAuth(role = Role.ADMIN)
-        void 유효한_요청이_보내지면_200_응답과_생성한_계정이_반환된다() throws Exception {
-            var request = new AdminSignupRequest("newAdmin", "1234");
-            var response = new AdminSignupResponse("newAdmin");
-            given(adminAuthService.signup(anyLong(), any(AdminSignupRequest.class)))
-                .willReturn(response);
+            @Test
+            void 토큰_없이_보내면_401_응답이_반환된다() throws Exception {
+                // when & then
+                mockMvc.perform(post(uri))
+                    .andExpect(status().isUnauthorized());
+            }
 
-            // when & then
-            mockMvc.perform(post("/admin/api/signup")
-                    .cookie(new Cookie("token", "token"))
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value("newAdmin"));
+            @Test
+            @WithMockAuth(role = Role.MEMBER)
+            void 토큰의_권한이_Admin이_아니면_404_응답이_반환된다() throws Exception {
+                // when & then
+                mockMvc.perform(post(uri)
+                        .cookie(AUTH_TOKEN))
+                    .andExpect(status().isNotFound());
+            }
         }
     }
 
     @Nested
     class 루트_어드민_활성화 {
 
-        @Test
-        void 유효한_요청이_보내지면_200_응답이_반환된다() throws Exception {
-            // given
-            var request = new RootAdminInitializeRequest("1234");
+        final String uri = "/admin/api/initialize";
 
-            // when & then
-            mockMvc.perform(post("/admin/api/initialize")
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        @Nested
+        @DisplayName("POST " + uri)
+        class 올바른_주소로 {
+
+            @Test
+            void 요청을_보내면_200_응답이_반환된다() throws Exception {
+                // given
+                var request = new RootAdminInitializeRequest("1234");
+
+                // when & then
+                mockMvc.perform(post(uri)
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+            }
+
+            @Test
+            @WithMockAuth(role = Role.ANONYMOUS)
+            void 권한이_없어도_200_응답이_반환된다() throws Exception {
+                // given
+                var request = new RootAdminInitializeRequest("1234");
+
+                // when & then
+                mockMvc.perform(post(uri)
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+            }
         }
     }
 }
