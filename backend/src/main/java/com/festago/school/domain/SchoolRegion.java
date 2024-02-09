b7e6fa6f1284ac09115dd58905fba62deac57f43
seokjin8678 <seokjin8678@gmail.com>
2024-02-09 18:47:05 +0900

    refactor: 사용하지 않는 메서드, 에러코드 제거

--- backend/src/main/java/com/festago/school/domain/SchoolRegion.java
+++ backend/src/main/java/com/festago/school/domain/SchoolRegion.java
@@ -1,19 +1,8 @@
 package com.festago.school.domain;
 
-import com.festago.common.exception.BadRequestException;
-import com.festago.common.exception.ErrorCode;
-
 public enum SchoolRegion {
     서울,
     부산,
     대구,
     ANY;
-
-    public static SchoolRegion from(String regionName) {
-        try {
-            return SchoolRegion.valueOf(regionName);
-        } catch (IllegalArgumentException e) {
-            throw new BadRequestException(ErrorCode.INVALID_REGION);
-        }
-    }
 }
