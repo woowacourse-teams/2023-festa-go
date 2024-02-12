7a99a5d1f8d68551cd8ca1e49206383d194cae73
seokjin8678 <seokjin8678@gmail.com>
2024-02-12 23:29:11 +0900

    fix: FestivalQueryInfoEventListener 트랜잭션 전파 수준 수정
    - SUPPORTS -> MANDATORY

--- backend/src/main/java/com/festago/festival/application/FestivalQueryInfoEventListener.java
+++ backend/src/main/java/com/festago/festival/application/FestivalQueryInfoEventListener.java
@@ -20,7 +20,7 @@ public class FestivalQueryInfoEventListener {
      * 해당 이벤트는 비동기로 실행하면 문제가 발생할 수 있으니, 동기적으로 처리해야함 <br/> 축제가 생성되면 FestivalQueryInfo는 반드시! 생성되어야 함
      */
     @EventListener
-    @Transactional(propagation = Propagation.SUPPORTS)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void festivalCreatedEventHandler(FestivalCreatedEvent event) {
         FestivalQueryInfo festivalQueryInfo = FestivalQueryInfo.create(event.festivalId());
         festivalQueryInfoRepository.save(festivalQueryInfo);
@@ -30,7 +30,7 @@ public class FestivalQueryInfoEventListener {
      * 삭제의 경우 동기적으로 처리될 필요가 없음 <br/> 하지만 일관성을 위해 동기적으로 처리함
      */
     @EventListener
-    @Transactional(propagation = Propagation.SUPPORTS)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void festivalDeletedEventHandler(FestivalDeletedEvent event) {
         festivalQueryInfoRepository.deleteByFestivalId(event.festivalId());
     }
