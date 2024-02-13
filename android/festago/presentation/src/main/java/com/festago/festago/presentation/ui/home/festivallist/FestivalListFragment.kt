2e487e45b610f61537f1f3fc88da5cf5c554a244
vrexpert <vrexpert2302@gmail.com>
2024-02-13 14:51:17 +0900

    feat: 탭 클릭 시 축제 요청

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
@@ -7,7 +7,6 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
@@ -54,7 +53,6 @@ class FestivalListFragment : Fragment() {
     }
 
     private fun initView() {
-        vm.loadFestivals()
         initViewPager()
         initRecyclerView()
         initRefresh()
@@ -62,7 +60,7 @@ class FestivalListFragment : Fragment() {
 
     private fun initRefresh() {
         binding.srlFestivalList.setOnRefreshListener {
-            vm.loadFestivals()
+            vm.loadFestivals(festivalFilterUiState = null)
             binding.srlFestivalList.isRefreshing = false
         }
     }
@@ -109,13 +107,14 @@ class FestivalListFragment : Fragment() {
         festivalListAdapter.submitList(
             listOf(
                 uiState,
-                FestivalTabUiState(
-                    {
-                        Toast.makeText(requireContext(), "Clicked $it", Toast.LENGTH_SHORT).show()
-                        /* TODO: Handle tab click */
-                        vm.loadFestivals()
-                    },
-                ),
+                FestivalTabUiState {
+                    val festivalFilter = when (it) {
+                        0 -> FestivalFilterUiState.PLANNED
+                        1 -> FestivalFilterUiState.PROGRESS
+                        else -> FestivalFilterUiState.PLANNED
+                    }
+                    vm.loadFestivals(festivalFilter)
+                },
             ) + uiState.festivals,
         )
     }
