558a03d689d3a692ae4c823d074edcdb37a99619
vrexpert <vrexpert2302@gmail.com>
2024-02-13 18:24:11 +0900

    feat: Fake Festival 목록 객체를 모듈로 연결

--- android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
+++ android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
@@ -1,7 +1,6 @@
 package com.festago.festago.data.di.singletonscope
 
 import com.festago.festago.data.repository.FakeFestivalRepository
-import com.festago.festago.data.repository.FestivalDefaultRepository
 import com.festago.festago.domain.repository.FestivalRepository
 import dagger.Binds
 import dagger.Module
