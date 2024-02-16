05918a42042030bee67b0fb3594e785e93bcf134
BGuga <tmdwns9913@gmail.com>
2024-02-17 02:51:02 +0900

    chore: Swagger 설명 추가

--- backend/src/main/java/com/festago/artist/presentation/v1/ArtistDetailV1Controller.java
+++ backend/src/main/java/com/festago/artist/presentation/v1/ArtistDetailV1Controller.java
@@ -5,6 +5,8 @@ import com.festago.artist.dto.ArtistDetailV1Response;
 import com.festago.artist.dto.ArtistFestivalDetailV1Response;
 import com.festago.common.aop.ValidPageable;
 import com.festago.common.exception.ValidException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -17,17 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/artists")
+@Tag(name = "아티스트 정보 요청 V1")
 @RequiredArgsConstructor
 public class ArtistDetailV1Controller {
 
     private final ArtistDetailV1QueryService artistDetailV1QueryService;
 
     @GetMapping("/{artistId}")
+    @Operation(description = "아티스트의 정보를 조회한다.")
     public ResponseEntity<ArtistDetailV1Response> getArtistInfo(@PathVariable Long artistId) {
         return ResponseEntity.ok(artistDetailV1QueryService.findArtistDetail(artistId));
     }
 
     @GetMapping("/{artistId}/festivals")
+    @Operation(description = "아티스트가 참석한 축제를 조회한다. isPast 값으로 종료 축제 와 진행, 예정 축제를 구분 가능하", summary = "아티스트 축제 조회")
     @ValidPageable(maxSize = 20)
     public ResponseEntity<Slice<ArtistFestivalDetailV1Response>> getArtistInfo(
         @PathVariable Long artistId,
