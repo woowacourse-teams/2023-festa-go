48848ad89ce36492cea3e6dd4542a32e1a12d286
EmilyCh0 <emily9803@ewhain.net>
2024-02-20 19:59:14 +0900

    refactor: ktlint check

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/festivallist/FestivalListFragment.kt
@@ -77,7 +77,7 @@ class FestivalListFragment : Fragment() {
             vm.loadFestivals()
             binding.srlFestivalList.isRefreshing = false
         }
-        binding.ivSearch.setOnClickListener {// 임시 연결
+        binding.ivSearch.setOnClickListener { // 임시 연결
             showSchoolDetail()
         }
     }
