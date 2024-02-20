702afb4c88b8d50dd2a8bc18815b44f4f195cf64
EmilyCh0 <emily9803@ewhain.net>
2024-02-20 19:41:35 +0900

    refactor: ktlint check

--- android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
+++ android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
@@ -2,8 +2,8 @@ package com.festago.festago.data.di.singletonscope
 
 import com.festago.festago.data.repository.FakeArtistRepository
 import com.festago.festago.data.repository.FakeFestivalRepository
-import com.festago.festago.domain.repository.ArtistRepository
 import com.festago.festago.data.repository.SchoolDefaultRepository
+import com.festago.festago.domain.repository.ArtistRepository
 import com.festago.festago.domain.repository.FestivalRepository
 import com.festago.festago.domain.repository.SchoolRepository
 import dagger.Binds
--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/schooldetail/SchoolDetailFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/schooldetail/SchoolDetailFragment.kt
@@ -2,10 +2,10 @@ package com.festago.festago.presentation.ui.schooldetail
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.festago.festago.presentation.databinding.FragmentSchoolDetailBinding
 import com.festago.festago.presentation.ui.schooldetail.uistate.SchoolDetailUiState
