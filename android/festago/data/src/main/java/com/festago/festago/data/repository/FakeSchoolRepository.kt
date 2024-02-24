52824ad39e7e9f813da831efae25c60e9a67f0ed
EmilyCh0 <emily9803@ewhain.net>
2024-02-24 18:10:16 +0900

    refactor: FakeSchoolRepository 주석 삭제

--- android/festago/data/src/main/java/com/festago/festago/data/repository/FakeSchoolRepository.kt
+++ android/festago/data/src/main/java/com/festago/festago/data/repository/FakeSchoolRepository.kt
@@ -5,16 +5,12 @@ import com.festago.festago.domain.model.school.SchoolInfo
 import com.festago.festago.domain.repository.SchoolRepository
 import javax.inject.Inject
 
-class FakeSchoolRepository @Inject constructor(
-    // TODO: Service 연결
-) : SchoolRepository {
+class FakeSchoolRepository @Inject constructor() : SchoolRepository {
     override suspend fun loadSchoolInfo(schoolId: Long): Result<SchoolInfo> {
-        // TODO: API 연동
         return Result.success(FakeSchool.googleSchool)
     }
 
     override suspend fun loadSchoolFestivals(schoolId: Long): Result<FestivalsPage> {
-        // TODO: API 연동
         return Result.success(
             FestivalsPage(
                 isLastPage = true,
