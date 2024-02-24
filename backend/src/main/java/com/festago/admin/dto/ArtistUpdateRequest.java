0c836fce1719a97a14a7b66f4f0df50978d0e8fa
BGuga <tmdwns9913@gmail.com>
2024-02-24 20:17:44 +0900

    chore: bean validation 의 기본 메시지를 사용하도록 변경.

--- backend/src/main/java/com/festago/admin/dto/ArtistUpdateRequest.java
+++ backend/src/main/java/com/festago/admin/dto/ArtistUpdateRequest.java
@@ -3,9 +3,9 @@ package com.festago.admin.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ArtistUpdateRequest(
-    @NotBlank(message = "아티스트 이름은 비어있을 수 없습니다.")
+    @NotBlank
     String name,
-    @NotBlank(message = "아티스트 이미지는 비어있을 수 없습니다.")
+    @NotBlank
     String profileImage,
     @NotBlank
     String backgroundImageUrl
