222abf99c6a1428a9e23bf5f93cf2061db80bbca
carsago <ohseo8813@gmail.com>
2024-03-03 18:03:45 +0900

    feat: 아티스트 검색 API 추가

---
+++ backend/src/main/java/com/festago/artist/presentation/v1/ArtistSearchV1Controller.java
@@ -0,0 +1,32 @@
+package com.festago.artist.presentation.v1;
+
+
+import com.festago.artist.application.ArtistV1SearchQueryService;
+import com.festago.artist.dto.ArtistSearchResponse;
+import com.festago.common.util.Validator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/search/artists")
+@Tag(name = "아티스트 검색 V1")
+@RequiredArgsConstructor
+public class ArtistSearchV1Controller {
+
+    private final ArtistV1SearchQueryService artistV1SearchQueryService;
+
+    @GetMapping
+    @Operation(description = "키워드로 아티스트 목록을 검색한다", summary = "아티스트 목록 검색 조회")
+    public ResponseEntity<List<ArtistSearchResponse>> searchByKeyword(@RequestParam String keyword) {
+        Validator.notBlank(keyword, "keyword");
+        List<ArtistSearchResponse> response = artistV1SearchQueryService.findAllByKeyword(keyword);
+        return ResponseEntity.ok(response);
+    }
+}
