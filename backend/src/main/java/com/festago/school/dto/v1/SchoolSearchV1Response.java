658eab04113bea6ea8985ead260840843e1618ea
seokjin8678 <seokjin8678@gmail.com>
2024-03-03 17:00:28 +0900

    feat: 학교 검색 Repository 구현

--- backend/src/main/java/com/festago/school/dto/v1/SchoolSearchV1Response.java
+++ backend/src/main/java/com/festago/school/dto/v1/SchoolSearchV1Response.java
@@ -1,9 +1,14 @@
 package com.festago.school.dto.v1;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 public record SchoolSearchV1Response(
     Long id,
     String name,
     String logoUrl
 ) {
 
+    @QueryProjection
+    public SchoolSearchV1Response {
+    }
 }
