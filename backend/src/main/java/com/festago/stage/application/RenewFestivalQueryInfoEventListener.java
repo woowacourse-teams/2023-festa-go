8b0c80419cfa6ea2f5f5188448977a85c3a9b8bd
seokjin8678 <seokjin8678@gmail.com>
2024-03-03 22:57:41 +0900

    refactor: RenewFestivalQueryInfoEventListener 의존 변경
    - FestivalQueryInfoArtistRenewService 의존하도록 변경

--- backend/src/main/java/com/festago/stage/application/RenewFestivalQueryInfoEventListener.java
+++ backend/src/main/java/com/festago/stage/application/RenewFestivalQueryInfoEventListener.java
@@ -1,20 +1,10 @@
 package com.festago.stage.application;
 
-import com.festago.artist.domain.Artist;
-import com.festago.artist.domain.ArtistsSerializer;
-import com.festago.artist.repository.ArtistRepository;
-import com.festago.common.exception.ErrorCode;
-import com.festago.common.exception.NotFoundException;
-import com.festago.festival.domain.FestivalQueryInfo;
-import com.festago.festival.repository.FestivalInfoRepository;
+import com.festago.festival.application.FestivalQueryInfoArtistRenewService;
 import com.festago.stage.domain.Stage;
 import com.festago.stage.dto.event.StageCreatedEvent;
 import com.festago.stage.dto.event.StageDeletedEvent;
 import com.festago.stage.dto.event.StageUpdatedEvent;
-import com.festago.stage.repository.StageArtistRepository;
-import com.festago.stage.repository.StageRepository;
-import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -25,42 +15,26 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RenewFestivalQueryInfoEventListener {
 
-    private final FestivalInfoRepository festivalInfoRepository;
-    private final StageRepository stageRepository;
-    private final StageArtistRepository stageArtistRepository;
-    private final ArtistRepository artistRepository;
-    private final ArtistsSerializer serializer;
+    private final FestivalQueryInfoArtistRenewService festivalQueryInfoArtistRenewService;
 
     @EventListener
     @Transactional(propagation = Propagation.MANDATORY)
     public void stageCreatedEventHandler(StageCreatedEvent event) {
         Stage stage = event.stage();
-        renewFestivalQueryInfo(stage);
-    }
-
-    private void renewFestivalQueryInfo(Stage stage) {
-        Long festivalId = stage.getFestival().getId();
-        List<Long> stageIds = stageRepository.findAllByFestivalId(festivalId).stream()
-            .map(Stage::getId)
-            .toList();
-        Set<Long> artistIds = stageArtistRepository.findAllArtistIdByStageIdIn(stageIds);
-        List<Artist> artists = artistRepository.findByIdIn(artistIds);
-        FestivalQueryInfo festivalQueryInfo = festivalInfoRepository.findByFestivalId(festivalId)
-            .orElseThrow(() -> new NotFoundException(ErrorCode.FESTIVAL_NOT_FOUND));
-        festivalQueryInfo.updateArtistInfo(artists, serializer);
+        festivalQueryInfoArtistRenewService.renewArtistInfo(stage.getFestival().getId());
     }
 
     @EventListener
     @Transactional(propagation = Propagation.MANDATORY)
     public void stageUpdatedEventHandler(StageUpdatedEvent event) {
         Stage stage = event.stage();
-        renewFestivalQueryInfo(stage);
+        festivalQueryInfoArtistRenewService.renewArtistInfo(stage.getFestival().getId());
     }
 
     @EventListener
     @Transactional(propagation = Propagation.MANDATORY)
     public void stageDeletedEventHandler(StageDeletedEvent event) {
         Stage stage = event.stage();
-        renewFestivalQueryInfo(stage);
+        festivalQueryInfoArtistRenewService.renewArtistInfo(stage.getFestival().getId());
     }
 }
