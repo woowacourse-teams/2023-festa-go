40d034d474c243a1895e81381cc85532aca543ec
BGuga <tmdwns9913@gmail.com>
2024-03-05 15:38:16 +0900

    feat: 스케쥴러 등록

---
+++ backend/src/main/java/com/festago/scheduler/application/SchedulerService.java
@@ -0,0 +1,13 @@
+package com.festago.scheduler.application;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulerService {
+
+    @Scheduled(fixedDelay = 1000)
+    public void run() {
+        System.out.println("called");
+    }
+}
