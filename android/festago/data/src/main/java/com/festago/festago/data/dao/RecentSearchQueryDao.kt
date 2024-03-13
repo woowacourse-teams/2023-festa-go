6d1c8dc5e350c5a429dac9558c2cc3966061fbad
vrexpert <vrexpert2302@gmail.com>
2024-03-13 14:52:39 +0900

    feat(RecentSearchQueryEntity): 최근검색어 dao 정의

---
+++ android/festago/data/src/main/java/com/festago/festago/data/dao/RecentSearchQueryDao.kt
@@ -0,0 +1,23 @@
+package com.festago.festago.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.festago.festago.data.model.RecentSearchQueryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecentSearchQueryDao {
+    @Query(value = "SELECT * FROM recentSearchQueries ORDER BY queriedDate DESC LIMIT :limit")
+    fun getRecentSearchQueryEntities(limit: Int): Flow<List<RecentSearchQueryEntity>>
+
+    @Upsert
+    suspend fun insertOrReplaceRecentSearchQuery(recentSearchQuery: RecentSearchQueryEntity)
+
+    @Delete
+    fun deleteRecentSearchQuery(recentSearchQuery: RecentSearchQueryEntity)
+
+    @Query(value = "DELETE FROM recentSearchQueries")
+    suspend fun clearRecentSearchQueries()
+}
