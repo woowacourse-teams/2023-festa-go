c2780a3caf4c082b1d977488041c07ccd19d8824
vrexpert <vrexpert2302@gmail.com>
2024-03-19 11:26:00 +0900

    feat(SearchViewModel): 뷰모델 검색 기능 작성

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/search/SearchViewModel.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/search/SearchViewModel.kt
@@ -2,13 +2,28 @@ package com.festago.festago.presentation.ui.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.festago.festago.common.analytics.AnalyticsHelper
+import com.festago.festago.common.analytics.logNetworkFailure
+import com.festago.festago.domain.model.festival.Festival
 import com.festago.festago.domain.model.recentsearch.RecentSearchQuery
+import com.festago.festago.domain.model.search.ArtistSearch
+import com.festago.festago.domain.model.search.SchoolSearch
 import com.festago.festago.domain.repository.RecentSearchRepository
+import com.festago.festago.domain.repository.SearchRepository
+import com.festago.festago.presentation.ui.search.uistate.ArtistSearchItemUiState
+import com.festago.festago.presentation.ui.search.uistate.ArtistUiState
+import com.festago.festago.presentation.ui.search.uistate.FestivalSearchItemUiState
 import com.festago.festago.presentation.ui.search.uistate.RecentSearchItemUiState
+import com.festago.festago.presentation.ui.search.uistate.SchoolSearchItemUiState
+import com.festago.festago.presentation.ui.search.uistate.SchoolUiState
 import com.festago.festago.presentation.ui.search.uistate.SearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,49 +31,63 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val recentSearchRepository: RecentSearchRepository,
+    private val searchRepository: SearchRepository,
+    private val analyticsHelper: AnalyticsHelper,
 ) : ViewModel() {
 
     private val _uiState: MutableStateFlow<SearchUiState> = MutableStateFlow(SearchUiState.Loading)
     val uiState: StateFlow<SearchUiState> = _uiState.asStateFlow()
 
-    init {
+    private val _event = MutableSharedFlow<SearchEvent>()
+    val event: SharedFlow<SearchEvent> = _event.asSharedFlow()
+
+    fun loadRecentSearch() {
+        if (uiState.value is SearchUiState.SearchSuccess) return
         viewModelScope.launch {
-            recentSearchRepository.getRecentSearchQueries(10).collect { recentSearchQueries ->
-                _uiState.value = SearchUiState.RecentSearchSuccess(
-                    recentSearchQueries.map { it.toUiState() },
-                )
-            }
+            _uiState.value = SearchUiState.RecentSearchSuccess(
+                recentSearchRepository.getRecentSearchQueries(10).map { it.toUiState() },
+            )
         }
     }
 
     fun search(searchQuery: String) {
         viewModelScope.launch {
+            if (searchQuery == "") {
+                _event.emit(SearchEvent.SearchBlank)
+                return@launch
+            }
             recentSearchRepository.insertOrReplaceRecentSearch(searchQuery)
-            searchArtists(searchQuery)
-            searchFestivals(searchQuery)
-            searchSchool(searchQuery)
+            val deferredFestivals = async { searchRepository.searchFestivals(searchQuery) }
+            val deferredArtist = async { searchRepository.searchArtists(searchQuery) }
+            val deferredSchools = async { searchRepository.searchSchools(searchQuery) }
+            runCatching {
+                _uiState.value = SearchUiState.SearchSuccess(
+                    deferredFestivals.await().getOrThrow().map { it.toUiState() },
+                    deferredArtist.await().getOrThrow().map { it.toUiState() },
+                    deferredSchools.await().getOrThrow().map { it.toUiState() },
+                )
+            }.onFailure {
+                _uiState.value = SearchUiState.Error
+                analyticsHelper.logNetworkFailure(
+                    key = KEY_SEARCH,
+                    value = it.message.toString(),
+                )
+            }
         }
     }
 
     private fun deleteRecentSearch(searchQuery: String) {
         viewModelScope.launch {
             recentSearchRepository.deleteRecentSearch(searchQuery)
+            loadRecentSearch()
         }
     }
 
     fun deleteAllRecentSearch() {
         viewModelScope.launch {
             recentSearchRepository.clearRecentSearches()
+            loadRecentSearch()
         }
-    }
-
-    private fun searchFestivals(searchQuery: String) {
-    }
-
-    private fun searchArtists(searchQuery: String) {
-    }
-
-    private fun searchSchool(searchQuery: String) {
     }
 
     private fun RecentSearchQuery.toUiState() = RecentSearchItemUiState(
@@ -66,4 +95,59 @@ class SearchViewModel @Inject constructor(
         onQuerySearched = ::search,
         onRecentSearchDeleted = ::deleteRecentSearch,
     )
+
+    private fun Festival.toUiState() = FestivalSearchItemUiState(
+        id = id,
+        name = name,
+        startDate = startDate,
+        endDate = endDate,
+        imageUrl = imageUrl,
+        schoolUiState = SchoolUiState(
+            id = school.id,
+            name = school.name,
+        ),
+        artists = artists.map { artist ->
+            ArtistUiState(artist.id, artist.name, artist.imageUrl, ::showArtistDetail)
+        },
+        ::showFestivalDetail,
+    )
+
+    private fun ArtistSearch.toUiState() = ArtistSearchItemUiState(
+        id = id,
+        name = name,
+        profileImageUrl = profileImageUrl,
+        todayStage = todayStage,
+        upcomingStage = upcomingStage,
+        onArtistDetailClick = ::showArtistDetail,
+    )
+
+    private fun SchoolSearch.toUiState() = SchoolSearchItemUiState(
+        id = id,
+        name = name,
+        logoUrl = logoUrl,
+        upcomingFestivalStartDate = upcomingFestivalStartDate,
+        onSchoolSearchClick = ::showSchoolDetail,
+    )
+
+    private fun showFestivalDetail(festivalId: Long) {
+        viewModelScope.launch {
+            _event.emit(SearchEvent.ShowFestivalDetail(festivalId))
+        }
+    }
+
+    private fun showArtistDetail(artistId: Long) {
+        viewModelScope.launch {
+            _event.emit(SearchEvent.ShowArtistDetail(artistId))
+        }
+    }
+
+    private fun showSchoolDetail(schoolId: Long) {
+        viewModelScope.launch {
+            _event.emit(SearchEvent.ShowSchoolDetail(schoolId))
+        }
+    }
+
+    companion object {
+        private const val KEY_SEARCH = "KEY_SEARCH"
+    }
 }
