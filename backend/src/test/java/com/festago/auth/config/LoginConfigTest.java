bcf27048af082957e6f10694a8e0f783d167fa64
seokjin8678 <seokjin8678@gmail.com>
2024-03-23 16:46:46 +0900

    fix: 테스트 컨트롤러에 /api 경로 추가

--- backend/src/test/java/com/festago/auth/config/LoginConfigTest.java
+++ backend/src/test/java/com/festago/auth/config/LoginConfigTest.java
@@ -34,14 +34,14 @@ class LoginConfigTest {
         @Test
         @WithMockAuth(role = Role.ANONYMOUS)
         void 토큰이_없으면_401_응답이_반환된다() throws Exception {
-            mockMvc.perform(get("/annotation-member-auth"))
+            mockMvc.perform(get("/api/annotation-member-auth"))
                 .andExpect(status().isUnauthorized());
         }
 
         @Test
         @WithMockAuth
         void 토큰이_있으면_200_응답이_반환된다() throws Exception {
-            mockMvc.perform(get("/annotation-member-auth")
+            mockMvc.perform(get("/api/annotation-member-auth")
                     .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
                 .andExpect(status().isOk());
         }
@@ -49,7 +49,7 @@ class LoginConfigTest {
         @Test
         @WithMockAuth(role = Role.ADMIN)
         void 토큰의_권한이_어드민이면_404_응답이_반환된다() throws Exception {
-            mockMvc.perform(get("/annotation-member-auth")
+            mockMvc.perform(get("/api/annotation-member-auth")
                     .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.errorCode").value(ErrorCode.NOT_ENOUGH_PERMISSION.name()));
@@ -61,7 +61,7 @@ class LoginConfigTest {
 class AnnotationMemberAuthController {
 
     @MemberAuth
-    @GetMapping("/annotation-member-auth")
+    @GetMapping("/api/annotation-member-auth")
     public ResponseEntity<Void> testAuthHandler() {
         return ResponseEntity.ok().build();
     }
