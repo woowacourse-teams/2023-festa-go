a7ea199f5e9622144c1dee7e5327c4dfa7ef6c9b
carsago <ohseo8813@gmail.com>
2024-03-23 15:14:41 +0900

    feat: 학교 북마크 조회 API 추가

---
+++ backend/src/test/java/com/festago/bookmark/presentation/SchoolBookmarkControllerTest.java
@@ -0,0 +1,55 @@
+package com.festago.bookmark.presentation;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.festago.support.CustomWebMvcTest;
+import com.festago.support.WithMockAuth;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@CustomWebMvcTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class SchoolBookmarkControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Nested
+    class 학교_북마크_목록_조회 {
+
+        final String uri = "/api/v1/bookmarks/schools";
+
+        @Nested
+        @DisplayName("GET " + uri)
+        class 올바른_주소로 {
+
+            @Test
+            @WithMockAuth
+            void 요청을_보내면_200_응답을_반환한다() throws Exception {
+                // when & then
+                mockMvc.perform(get(uri)
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+            }
+
+            @Test
+            void 인증을_안했으면_4xx_응답을_반환한다() throws Exception {
+                // when & then
+                mockMvc.perform(get(uri)
+                        .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().is4xxClientError());
+            }
+        }
+    }
+}
