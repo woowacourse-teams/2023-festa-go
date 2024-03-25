884ed4357167a1155eee59dd1a82087ce0abe4ac
BGuga <tmdwns9913@gmail.com>
2024-03-26 00:37:09 +0900

    feat: 아티스트 북마크 목록 조회 구현

---
+++ backend/src/main/java/com/festago/bookmark/presentation/ArtistBookmarkV1Controller.java
@@ -0,0 +1,24 @@
+package com.festago.bookmark.presentation;
+
+import com.festago.auth.annotation.Member;
+import com.festago.bookmark.application.ArtistBookmarkV1QueryService;
+import com.festago.bookmark.dto.v1.ArtistBookmarkV1Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "아티스트 북마크 V1")
+public class ArtistBookmarkV1Controller {
+
+    private final ArtistBookmarkV1QueryService artistBookmarkV1QueryService;
+
+    @Operation(description = "유저의 아티스트 북마크 목록을 조회한다.", summary = "아티스트 북마크 조회")
+    public ResponseEntity<List<ArtistBookmarkV1Response>> findArtistBookmarksByMemberId(@Member Long memberId) {
+        return ResponseEntity.ok(artistBookmarkV1QueryService.findArtistBookmarksByMemberId(memberId));
+    }
+}
