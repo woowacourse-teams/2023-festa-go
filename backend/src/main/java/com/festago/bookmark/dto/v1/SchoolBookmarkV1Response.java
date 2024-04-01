b228a5f6b8fe074a8bf04748f51b5a30f71d439b
carsago <ohseo8813@gmail.com>
2024-04-01 16:35:42 +0900

    refactor: SchoolBookmarkV1Response의 생성시간 필드명을 createdAt으로 변경

--- backend/src/main/java/com/festago/bookmark/dto/v1/SchoolBookmarkV1Response.java
+++ backend/src/main/java/com/festago/bookmark/dto/v1/SchoolBookmarkV1Response.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 public record SchoolBookmarkV1Response(
     SchoolBookmarkInfoV1Response school,
-    LocalDateTime bookmarkCreatedAt
+    LocalDateTime createdAt
 ) {
 
     @QueryProjection
