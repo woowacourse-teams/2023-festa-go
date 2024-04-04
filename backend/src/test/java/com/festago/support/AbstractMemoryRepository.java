3935e45d29406e2e42bd2cbf442cf283622edcad
seokjin8678 <seokjin8678@gmail.com>
2024-04-04 19:23:41 +0900

    feat: AbstractMemoryRepository 추가
    (cherry picked from commit d679b066e4b4077f46017b323f7e01d7fb3db3a9)

---
+++ backend/src/test/java/com/festago/support/AbstractMemoryRepository.java
@@ -0,0 +1,34 @@
+package com.festago.support;
+
+import jakarta.persistence.Id;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.SneakyThrows;
+
+public abstract class AbstractMemoryRepository<T> {
+
+    protected final HashMap<Long, T> memory = new HashMap<>();
+    private final AtomicLong autoIncrement = new AtomicLong();
+
+    @SneakyThrows
+    final public T save(T entity) {
+        Field[] fields = entity.getClass()
+            .getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Id.class)) {
+                field.setAccessible(true);
+                long id = autoIncrement.incrementAndGet();
+                field.set(entity, id);
+                memory.put(id, entity);
+                return entity;
+            }
+        }
+        throw new IllegalArgumentException("해당 엔티티에 @Id 어노테이션이 붙은 식별자가 존재하지 않습니다.");
+    }
+
+    final public Optional<T> findById(Long id) {
+        return Optional.ofNullable(memory.get(id));
+    }
+}
