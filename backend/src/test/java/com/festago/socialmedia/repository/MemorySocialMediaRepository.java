7d8ff3da4c831fff5030ba8ba5422d6ef0b62829
seokjin8678 <seokjin8678@gmail.com>
2024-04-08 18:41:42 +0900

    refactor: MemorySocialMediaRepository AbstractMemoryRepository 상속하도록 변경

--- backend/src/test/java/com/festago/socialmedia/repository/MemorySocialMediaRepository.java
+++ backend/src/test/java/com/festago/socialmedia/repository/MemorySocialMediaRepository.java
@@ -3,35 +3,11 @@ package com.festago.socialmedia.repository;
 import com.festago.socialmedia.domain.OwnerType;
 import com.festago.socialmedia.domain.SocialMedia;
 import com.festago.socialmedia.domain.SocialMediaType;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import com.festago.support.AbstractMemoryRepository;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-import lombok.SneakyThrows;
 
-// TODO #841 이슈 해결되면 반영할 것
-public class MemorySocialMediaRepository implements SocialMediaRepository {
-
-    private final Map<Long, SocialMedia> memory = new HashMap<>();
-    private final AtomicLong autoIncrement = new AtomicLong();
-
-    @Override
-    @SneakyThrows
-    public SocialMedia save(SocialMedia socialMedia) {
-        Field idField = socialMedia.getClass()
-            .getDeclaredField("id");
-        idField.setAccessible(true);
-        idField.set(socialMedia, autoIncrement.incrementAndGet());
-        memory.put(socialMedia.getId(), socialMedia);
-        return socialMedia;
-    }
-
-    @Override
-    public Optional<SocialMedia> findById(Long id) {
-        return Optional.ofNullable(memory.get(id));
-    }
+public class MemorySocialMediaRepository extends AbstractMemoryRepository<SocialMedia> implements
+    SocialMediaRepository {
 
     @Override
     public boolean existsByOwnerIdAndOwnerTypeAndMediaType(
@@ -43,10 +19,5 @@ public class MemorySocialMediaRepository implements SocialMediaRepository {
             .filter(it -> Objects.equals(it.getOwnerId(), ownerId))
             .filter(it -> it.getOwnerType() == ownerType)
             .anyMatch(it -> it.getMediaType() == mediaType);
-    }
-
-    @Override
-    public void deleteById(Long socialMediaId) {
-        memory.remove(socialMediaId);
     }
 }
