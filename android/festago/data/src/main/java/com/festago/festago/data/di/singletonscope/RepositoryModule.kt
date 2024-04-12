1f7e1524f8432f23e33da4f3d9a408eb3368ee45
re4rk <37167652+re4rk@users.noreply.github.com>
2024-04-12 18:52:31 +0900

    feat(bookmark): Repository와 구현체 추가

--- android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
+++ android/festago/data/src/main/java/com/festago/festago/data/di/singletonscope/RepositoryModule.kt
@@ -1,11 +1,13 @@
 package com.festago.festago.data.di.singletonscope
 
 import com.festago.festago.data.repository.DefaultArtistRepository
+import com.festago.festago.data.repository.DefaultBookmarkRepository
 import com.festago.festago.data.repository.DefaultFestivalRepository
 import com.festago.festago.data.repository.DefaultRecentSearchRepository
 import com.festago.festago.data.repository.DefaultSchoolRepository
 import com.festago.festago.data.repository.DefaultSearchRepository
 import com.festago.festago.domain.repository.ArtistRepository
+import com.festago.festago.domain.repository.BookmarkRepository
 import com.festago.festago.domain.repository.FestivalRepository
 import com.festago.festago.domain.repository.RecentSearchRepository
 import com.festago.festago.domain.repository.SchoolRepository
@@ -39,4 +41,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindsSearchRepository(searchRepository: DefaultSearchRepository): SearchRepository
+
+    @Binds
+    @Singleton
+    fun bindsBookmarkRepository(bookmarkRepository: DefaultBookmarkRepository): BookmarkRepository
 }
