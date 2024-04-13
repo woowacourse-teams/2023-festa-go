bee227e36c223619a6fcac5fafa7eb598548706d
seokjin8678 <seokjin8678@gmail.com>
2024-04-14 08:28:43 +0900

    feat: MockDataStartupInitializer 트랜잭션 적용하여 원자성 보장

--- backend/src/main/java/com/festago/mock/MockDataStartupInitializer.java
+++ backend/src/main/java/com/festago/mock/MockDataStartupInitializer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Profile({"dev"})
 @Component
@@ -17,6 +18,7 @@ public class MockDataStartupInitializer {
     private final ForMockSchoolRepository forMockSchoolRepository;
     private final MockDataService mockDataService;
 
+    @Transactional
     @EventListener(ApplicationReadyEvent.class)
     public void initialize() {
         if (forMockSchoolRepository.count() == 0) {
