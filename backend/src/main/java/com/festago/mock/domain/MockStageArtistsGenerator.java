541c54302171fb9a9ad31290baab53ea69a3c3ad
seokjin8678 <seokjin8678@gmail.com>
2024-04-14 14:31:07 +0900

    chore: MockStageArtistsGenerator Javadoc 추가

--- backend/src/main/java/com/festago/mock/domain/MockStageArtistsGenerator.java
+++ backend/src/main/java/com/festago/mock/domain/MockStageArtistsGenerator.java
@@ -14,6 +14,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class MockStageArtistsGenerator {
 
+    /**
+     * 영속되지 않은 상태의 StageArtist 목록을 생성합니다. <br/> StageArtist는 Stage와 Artist의 식별자가 필요하므로, 인자로 들어오는 Stage, Artist는 영속된 상태여야
+     * 합니다. <br/> 공연에 stagePerArtist 만큼 아티스트를 참여시키는게 불가능할 경우, 각 공연 별 최소 1명은 참가하는 것을 보장합니다. <br/> 따라서, stages.size() >
+     * artists.size() 이면 예외가 발생합니다. <br/> 생성된 StageArtist 목록에는 중복된 Artist가 존재하지 않습니다.<br/>
+     *
+     * @param stagePerArtist 각 공연에 참여될 아티스트의 수
+     * @param stages         영속 상태의 아티스트가 참여될 공연 목록
+     * @param artists        영속 상태의 공연에 참여할 아티스트 목록
+     * @return 중복된 Artist가 없는 영속되지 않은 StageArtist 엔티티 리스트
+     * @throws UnexpectedException 공연의 개수가 아티스트의 개수를 초과하면
+     */
     public List<StageArtist> generate(int stagePerArtist, List<Stage> stages, List<Artist> artists) {
         if (stages.size() > artists.size()) {
             throw new UnexpectedException("공연의 개수는 아티스트의 개수를 초과할 수 없습니다.");
