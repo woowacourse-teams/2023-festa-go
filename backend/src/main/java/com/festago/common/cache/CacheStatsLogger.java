218aad392373d16ef679e92f49fd51e0a8c33bb6
seokjin8678 <seokjin8678@gmail.com>
2024-04-17 02:54:42 +0900

    feat: CacheStatsLogger 추가
    - 어플리케이션 종료 시점에 캐시 스탯 로깅

---
+++ backend/src/main/java/com/festago/common/cache/CacheStatsLogger.java
@@ -0,0 +1,28 @@
+package com.festago.common.cache;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CacheStatsLogger {
+
+    private final CacheManager cacheManager;
+
+    @EventListener(ContextClosedEvent.class)
+    public void logCacheStats() {
+        for (String cacheName : cacheManager.getCacheNames()) {
+            Cache cache = cacheManager.getCache(cacheName);
+            if (cache instanceof CaffeineCache caffeineCache) {
+                log.info("CacheName={} CacheStats={}", cacheName, caffeineCache.getNativeCache().stats());
+            }
+        }
+    }
+}
