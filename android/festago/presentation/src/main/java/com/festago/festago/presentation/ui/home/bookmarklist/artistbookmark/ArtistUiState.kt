1250720df9aa391abc49dfc2714f6429153fb72e
re4rk <37167652+re4rk@users.noreply.github.com>
2024-04-17 14:44:37 +0900

    refactor(bookmark): 사용하지 않는 UiState 제거

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/bookmarklist/artistbookmark/ArtistUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.festago.festago.presentation.ui.home.bookmarklist.artistbookmark
-
-data class ArtistUiState(
-    val id: Long,
-    val name: String,
-    val imageUrl: String,
-    val onArtistDetail: (artistId: Long) -> Unit,
-)
