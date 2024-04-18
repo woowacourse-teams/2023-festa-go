df8dfb45b1677dff240861605f45c0fd87ee2b43
Seokjin Jeon <seokjin8678@gmail.com>
2024-04-19 03:18:43 +0900

    [BE] build: Thymeleaf 의존 제거 (#865) (#866)
    build: Thymeleaf 의존 제거

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${swaggerVersion}")
