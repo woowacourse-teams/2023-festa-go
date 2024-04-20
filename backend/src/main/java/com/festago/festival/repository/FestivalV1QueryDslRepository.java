c31dc05eadb9ef3436dba12948fe955b7949be29
seokjin8678 <seokjin8678@gmail.com>
2024-04-21 01:42:14 +0900

    feat: Slice 처리하는 Repository 공통 로직 적용

--- backend/src/main/java/com/festago/festival/repository/FestivalV1QueryDslRepository.java
+++ backend/src/main/java/com/festago/festival/repository/FestivalV1QueryDslRepository.java
@@ -4,59 +4,49 @@ import static com.festago.festival.domain.QFestival.festival;
 import static com.festago.festival.domain.QFestivalQueryInfo.festivalQueryInfo;
 import static com.festago.school.domain.QSchool.school;
 
-import com.festago.common.querydsl.QueryDslRepositorySupport;
-import com.festago.festival.domain.Festival;
+import com.festago.common.querydsl.QueryDslHelper;
 import com.festago.festival.dto.FestivalV1Response;
 import com.festago.festival.dto.QFestivalV1Response;
 import com.festago.festival.dto.QSchoolV1Response;
 import com.festago.school.domain.SchoolRegion;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
 import java.time.LocalDate;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class FestivalV1QueryDslRepository extends QueryDslRepositorySupport {
+@RequiredArgsConstructor
+public class FestivalV1QueryDslRepository {
 
-    private static final long NEXT_PAGE_TEMPORARY_COUNT = 1;
-
-    public FestivalV1QueryDslRepository() {
-        super(Festival.class);
-    }
+    private final QueryDslHelper queryDslHelper;
 
     public Slice<FestivalV1Response> findBy(FestivalSearchCondition searchCondition) {
         FestivalFilter filter = searchCondition.filter();
-        Pageable page = searchCondition.page();
-        List<FestivalV1Response> content = getSelectQuery()
-            .where(dynamicWhere(filter, searchCondition.currentTime(), searchCondition.lastFestivalId(),
-                searchCondition.lastStartDate(), searchCondition.region()))
-            .orderBy(dynamicOrderBy(filter))
-            .limit(page.getPageSize() + NEXT_PAGE_TEMPORARY_COUNT)
-            .fetch();
-        return getResponse(content, page);
-    }
-
-    private JPAQuery<FestivalV1Response> getSelectQuery() {
-        return select(new QFestivalV1Response(
-            festival.id,
-            festival.name,
-            festival.festivalDuration.startDate,
-            festival.festivalDuration.endDate,
-            festival.posterImageUrl,
-            new QSchoolV1Response(
-                school.id,
-                school.name
-            ),
-            festivalQueryInfo.artistInfo)
-        )
-            .from(festival)
-            .innerJoin(school).on(school.id.eq(festival.school.id))
-            .innerJoin(festivalQueryInfo).on(festivalQueryInfo.festivalId.eq(festival.id));
+        Pageable pageable = searchCondition.pageable();
+        return queryDslHelper.applySlice(
+            pageable,
+            query -> query.select(new QFestivalV1Response(
+                    festival.id,
+                    festival.name,
+                    festival.festivalDuration.startDate,
+                    festival.festivalDuration.endDate,
+                    festival.posterImageUrl,
+                    new QSchoolV1Response(
+                        school.id,
+                        school.name
+                    ),
+                    festivalQueryInfo.artistInfo
+                ))
+                .from(festival)
+                .innerJoin(school).on(school.id.eq(festival.school.id))
+                .innerJoin(festivalQueryInfo).on(festivalQueryInfo.festivalId.eq(festival.id))
+                .where(dynamicWhere(filter, searchCondition.currentTime(), searchCondition.lastFestivalId(),
+                    searchCondition.lastStartDate(), searchCondition.region()))
+                .orderBy(dynamicOrderBy(filter))
+        );
     }
 
     private BooleanExpression dynamicWhere(
@@ -134,21 +124,5 @@ public class FestivalV1QueryDslRepository extends QueryDslRepositorySupport {
             case PROGRESS -> new OrderSpecifier[]{festival.festivalDuration.startDate.desc(), festival.id.asc()};
             case END -> new OrderSpecifier[]{festival.festivalDuration.endDate.desc()};
         };
-    }
-
-    private Slice<FestivalV1Response> getResponse(List<FestivalV1Response> content, Pageable page) {
-        if (hasContentNextPage(content, page)) {
-            removeTemporaryContent(content);
-            return new SliceImpl<>(content, page, true);
-        }
-        return new SliceImpl<>(content, page, false);
-    }
-
-    private boolean hasContentNextPage(List<FestivalV1Response> content, Pageable page) {
-        return content.size() > page.getPageSize();
-    }
-
-    private void removeTemporaryContent(List<FestivalV1Response> content) {
-        content.remove(content.size() - 1);
     }
 }
