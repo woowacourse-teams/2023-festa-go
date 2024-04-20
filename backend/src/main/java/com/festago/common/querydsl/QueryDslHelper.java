095d01b076dca96d0108944b82180616b73c8381
seokjin8678 <seokjin8678@gmail.com>
2024-04-21 01:20:21 +0900

    feat: QueryDslHelper, QueryDslRepositorySupport applySlice 메서드 추가

--- backend/src/main/java/com/festago/common/querydsl/QueryDslHelper.java
+++ backend/src/main/java/com/festago/common/querydsl/QueryDslHelper.java
@@ -1,5 +1,6 @@
 package com.festago.common.querydsl;
 
+import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.Expression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -9,6 +10,8 @@ import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +19,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class QueryDslHelper {
 
+    private static final int NEXT_PAGE_OFFSET = 1;
     private final JPAQueryFactory queryFactory;
 
     public <T> JPAQuery<T> select(Expression<T> expr) {
         return queryFactory.select(expr);
+    }
+
+    public <T> JPAQuery<T> selectFrom(EntityPath<T> expr) {
+        return queryFactory.selectFrom(expr);
     }
 
     public <T> Optional<T> fetchOne(Function<JPAQueryFactory, JPAQuery<T>> queryFunction) {
@@ -35,5 +43,23 @@ public class QueryDslHelper {
         List<T> content = contentQueryFunction.apply(queryFactory).fetch();
         JPAQuery<Long> countQuery = countQueryFunction.apply(queryFactory);
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    public <T> Slice<T> applySlice(
+        Pageable pageable,
+        Function<JPAQueryFactory, JPAQuery<T>> contentQueryFunction
+    ) {
+        List<T> content = contentQueryFunction.apply(queryFactory)
+            .limit(pageable.getPageSize() + (long) NEXT_PAGE_OFFSET)
+            .fetch();
+        if (content.size() > pageable.getPageSize()) {
+            removeTemporaryContent(content);
+            return new SliceImpl<>(content, pageable, true);
+        }
+        return new SliceImpl<>(content, pageable, false);
+    }
+
+    private <T> void removeTemporaryContent(List<T> content) {
+        content.remove(content.size() - NEXT_PAGE_OFFSET);
     }
 }
