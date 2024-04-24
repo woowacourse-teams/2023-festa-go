9cbe7c6805410976216ad63135961a1df7aa8e51
re4rk <37167652+re4rk@users.noreply.github.com>
2024-04-24 12:02:48 +0900

    feat(my page): 마이페이지 틀 추가

--- android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/mypage/MyPageFragment.kt
+++ android/festago/presentation/src/main/java/com/festago/festago/presentation/ui/home/mypage/MyPageFragment.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import com.festago.festago.presentation.R
+import com.festago.festago.presentation.databinding.FragmentFestivalListBinding
+import com.festago.festago.presentation.databinding.FragmentMyPageBinding
+import com.festago.festago.presentation.util.setOnApplyWindowInsetsCompatListener
 
 class MyPageFragment : Fragment() {
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater)
+        binding.root.setOnApplyWindowInsetsCompatListener { view, windowInsets ->
+            val statusBarInsets = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
+            view.setPadding(0, statusBarInsets.top, 0, 0)
+            windowInsets
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
