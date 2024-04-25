7f98b46083e96e1c7c1f07727b1873b5ec54340f
seokjin8678 <seokjin8678@gmail.com>
2024-04-25 16:41:03 +0900

    feat: RefreshTokenV1Request `@NotNull` 추가

--- backend/src/main/java/com/festago/auth/dto/v1/RefreshTokenV1Request.java
+++ backend/src/main/java/com/festago/auth/dto/v1/RefreshTokenV1Request.java
@@ -1,9 +1,10 @@
 package com.festago.auth.dto.v1;
 
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.UUID;
 
 public record RefreshTokenV1Request(
-    @UUID String refreshToken
+    @NotNull @UUID String refreshToken
 ) {
 
 }
