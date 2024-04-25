69170c488e40d03462514a79652018928feb9898
seokjin8678 <seokjin8678@gmail.com>
2024-04-25 16:49:17 +0900

    fix: 학교 검색 시 발생하는 NPE 수정
    - 학교 식별자 목록으로 최근 축제 시작일 받아오는 클래스 이름 변경
    - SchoolSearchUpcomingFestivalV1QueryService -> SchoolUpcomingFestivalStartDateV1QueryService

--- backend/src/main/java/com/festago/school/application/v1/SchoolTotalSearchV1QueryService.java
+++ backend/src/main/java/com/festago/school/application/v1/SchoolTotalSearchV1QueryService.java
@@ -2,7 +2,9 @@ package com.festago.school.application.v1;
 
 import com.festago.school.dto.v1.SchoolSearchV1Response;
 import com.festago.school.dto.v1.SchoolTotalSearchV1Response;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +15,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class SchoolTotalSearchV1QueryService {
 
     private final SchoolSearchV1QueryService schoolSearchV1QueryService;
-    private final SchoolSearchUpcomingFestivalV1QueryService schoolSearchUpcomingFestivalV1QueryService;
+    private final SchoolUpcomingFestivalStartDateV1QueryService schoolUpcomingFestivalStartDateV1QueryService;
 
     public List<SchoolTotalSearchV1Response> searchSchools(String keyword) {
-        var schoolSearchResponses = schoolSearchV1QueryService.searchSchools(keyword);
+        List<SchoolSearchV1Response> schoolSearchResponses = schoolSearchV1QueryService.searchSchools(keyword);
         List<Long> schoolIds = schoolSearchResponses.stream()
             .map(SchoolSearchV1Response::id)
             .toList();
-        var schoolIdToUpcomingFestivalResponse = schoolSearchUpcomingFestivalV1QueryService.searchUpcomingFestivals(
-            schoolIds);
+        Map<Long, LocalDate> schoolIdToUpcomingFestivalStartDate = getSchoolIdToUpcomingFestivalStartDate(schoolIds);
         return schoolSearchResponses.stream()
-            .map(it -> SchoolTotalSearchV1Response.of(it, schoolIdToUpcomingFestivalResponse.get(it.id())))
+            .map(schoolSearchResponse -> new SchoolTotalSearchV1Response(
+                schoolSearchResponse.id(),
+                schoolSearchResponse.name(),
+                schoolSearchResponse.logoUrl(),
+                schoolIdToUpcomingFestivalStartDate.get(schoolSearchResponse.id())
+            ))
             .toList();
+    }
+
+    private Map<Long, LocalDate> getSchoolIdToUpcomingFestivalStartDate(List<Long> schoolIds) {
+        return schoolUpcomingFestivalStartDateV1QueryService.getSchoolIdToUpcomingFestivalStartDate(schoolIds);
     }
 }
