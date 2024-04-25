765444ddabc9dc3615a50abaa52db522765b18df
Seokjin Jeon <seokjin8678@gmail.com>
2024-04-25 19:33:13 +0900

    [BE] fix: 학교 검색 시 발생하는 NPE 수정 (#895) (#896)
    fix: 학교 검색 시 발생하는 NPE 수정
    
    - 학교 식별자 목록으로 최근 축제 시작일 받아오는 클래스 이름 변경
    - SchoolSearchUpcomingFestivalV1QueryService -> SchoolUpcomingFestivalStartDateV1QueryService

--- backend/src/main/java/com/festago/festival/repository/RecentSchoolFestivalV1QueryDslRepository.java
+++ backend/src/main/java/com/festago/festival/repository/RecentSchoolFestivalV1QueryDslRepository.java
@@ -4,8 +4,8 @@ import static com.festago.festival.domain.QFestival.festival;
 
 import com.festago.common.querydsl.QueryDslRepositorySupport;
 import com.festago.festival.domain.Festival;
-import com.festago.school.dto.v1.QSchoolSearchUpcomingFestivalV1Response;
-import com.festago.school.dto.v1.SchoolSearchUpcomingFestivalV1Response;
+import com.festago.school.dto.v1.QSchoolUpcomingFestivalStartDateV1Response;
+import com.festago.school.dto.v1.SchoolUpcomingFestivalStartDateV1Response;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Repository;
@@ -17,12 +17,12 @@ public class RecentSchoolFestivalV1QueryDslRepository extends QueryDslRepository
         super(Festival.class);
     }
 
-    public List<SchoolSearchUpcomingFestivalV1Response> findRecentSchoolFestivals(
+    public List<SchoolUpcomingFestivalStartDateV1Response> findRecentSchoolFestivals(
         List<Long> schoolIds,
         LocalDate now
     ) {
         return select(
-            new QSchoolSearchUpcomingFestivalV1Response(
+            new QSchoolUpcomingFestivalStartDateV1Response(
                 festival.school.id,
                 festival.festivalDuration.startDate.min()
             ))
--- backend/src/main/java/com/festago/school/dto/v1/SchoolUpcomingFestivalStartDateV1Response.java
+++ backend/src/main/java/com/festago/school/dto/v1/SchoolUpcomingFestivalStartDateV1Response.java
@@ -3,12 +3,12 @@ package com.festago.school.dto.v1;
 import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDate;
 
-public record SchoolSearchUpcomingFestivalV1Response(
+public record SchoolUpcomingFestivalStartDateV1Response(
     Long schoolId,
     LocalDate startDate
 ) {
 
     @QueryProjection
-    public SchoolSearchUpcomingFestivalV1Response {
+    public SchoolUpcomingFestivalStartDateV1Response {
     }
 }
