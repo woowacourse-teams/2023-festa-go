5ad3959a132f2fa7eef56139748e836c834ebcdb
seokjin8678 <seokjin8678@gmail.com>
2024-05-01 17:43:45 +0900

    feat: Member, MemberAuth 어노테이션에 스웨거 어노테이션 추가

--- backend/src/main/java/com/festago/auth/annotation/Member.java
+++ backend/src/main/java/com/festago/auth/annotation/Member.java
@@ -1,10 +1,12 @@
 package com.festago.auth.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Parameter(hidden = true)
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Member {
--- backend/src/main/java/com/festago/auth/annotation/MemberAuth.java
+++ backend/src/main/java/com/festago/auth/annotation/MemberAuth.java
@@ -1,10 +1,12 @@
 package com.festago.auth.annotation;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@SecurityRequirement(name = "bearerAuth")
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MemberAuth {
