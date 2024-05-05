9f356d1bbd3a65a2cda3c8d2b586ddf823cb3dde
vrexpert <vrexpert2302@gmail.com>
2024-05-05 18:47:38 +0900

    chore : 페스타고 버전 업 2.0.1

--- android/festago/app/build.gradle.kts
+++ android/festago/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.festago.festago"
         minSdk = 28
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.0.0"
+        versionCode = 9
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
