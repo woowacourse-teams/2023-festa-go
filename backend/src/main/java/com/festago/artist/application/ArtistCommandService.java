5ba831121f0f81a1435ca8867dda54472de2c581
Seokjin Jeon <seokjin8678@gmail.com>
2024-05-07 16:13:40 +0900

    [BE] fix: 아티스트 생성, 수정, 삭제 이벤트 발행 추가 (#948) (#949)
    fix: 아티스트 생성, 수정, 삭제 이벤트 발행 추가

--- backend/src/main/java/com/festago/artist/application/ArtistCommandService.java
+++ backend/src/main/java/com/festago/artist/application/ArtistCommandService.java
@@ -3,8 +3,12 @@ package com.festago.artist.application;
 import com.festago.artist.domain.Artist;
 import com.festago.artist.dto.command.ArtistCreateCommand;
 import com.festago.artist.dto.command.ArtistUpdateCommand;
+import com.festago.artist.dto.event.ArtistCreatedEvent;
+import com.festago.artist.dto.event.ArtistDeletedEvent;
+import com.festago.artist.dto.event.ArtistUpdatedEvent;
 import com.festago.artist.repository.ArtistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,20 +18,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArtistCommandService {
 
     private final ArtistRepository artistRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Long save(ArtistCreateCommand command) {
         Artist artist = artistRepository.save(
             new Artist(command.name(), command.profileImageUrl(), command.backgroundImageUrl())
         );
+        eventPublisher.publishEvent(new ArtistCreatedEvent(artist));
         return artist.getId();
     }
 
     public void update(ArtistUpdateCommand command, Long artistId) {
         Artist artist = artistRepository.getOrThrow(artistId);
         artist.update(command.name(), command.profileImageUrl(), command.backgroundImageUrl());
+        eventPublisher.publishEvent(new ArtistUpdatedEvent(artist));
     }
 
     public void delete(Long artistId) {
         artistRepository.deleteById(artistId);
+        eventPublisher.publishEvent(new ArtistDeletedEvent(artistId));
     }
 }
