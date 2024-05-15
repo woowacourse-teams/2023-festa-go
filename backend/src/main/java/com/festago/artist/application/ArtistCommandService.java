f0e6f46b1c69eb79deb8c8e5d2d94b716675ef9a
Seokjin Jeon <seokjin8678@gmail.com>
2024-05-15 22:37:46 +0900

    [BE] feat: Artist 생성 시 중복 이름 검증 추가 (#963) (#964)
    feat: Artist 생성 시 중복 이름 검증 추가

--- backend/src/main/java/com/festago/artist/application/ArtistCommandService.java
+++ backend/src/main/java/com/festago/artist/application/ArtistCommandService.java
@@ -7,6 +7,8 @@ import com.festago.artist.dto.event.ArtistCreatedEvent;
 import com.festago.artist.dto.event.ArtistDeletedEvent;
 import com.festago.artist.dto.event.ArtistUpdatedEvent;
 import com.festago.artist.repository.ArtistRepository;
+import com.festago.common.exception.BadRequestException;
+import com.festago.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,18 @@ public class ArtistCommandService {
     private final ApplicationEventPublisher eventPublisher;
 
     public Long save(ArtistCreateCommand command) {
+        validateSave(command);
         Artist artist = artistRepository.save(
             new Artist(command.name(), command.profileImageUrl(), command.backgroundImageUrl())
         );
         eventPublisher.publishEvent(new ArtistCreatedEvent(artist));
         return artist.getId();
+    }
+
+    private void validateSave(ArtistCreateCommand command) {
+        if (artistRepository.existsByName(command.name())) {
+            throw new BadRequestException(ErrorCode.DUPLICATE_ARTIST_NAME);
+        }
     }
 
     public void update(ArtistUpdateCommand command, Long artistId) {
