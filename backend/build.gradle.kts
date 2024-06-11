1487ba0a84c6d6d64a4dbbc05bf1ec24149da4d7
seokjin8678 <seokjin8678@gmail.com>
2024-06-11 18:53:15 +0900

    build: Redis 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${swaggerVersion}")
 
     // Spring Security
